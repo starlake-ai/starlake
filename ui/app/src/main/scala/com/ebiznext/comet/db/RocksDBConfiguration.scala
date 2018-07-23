ed34b740b83c42967456877831602e3b5ecb1c6a
Mourad DACHRAOUI <mourad.dachraoui@ebiznext.com>
2018-07-23 17:04:30 +0200

    adds rocks db persistence

---
+++ ui/app/src/main/scala/com/ebiznext/comet/db/RocksDBConfiguration.scala
@@ -0,0 +1,21 @@
+package com.ebiznext.comet.db
+
+import org.rocksdb.Options
+
+/**
+  * Created by Mourad on 23/07/2018.
+  */
+case class RocksDBConfiguration(path: String, createIfMissing: Boolean = false) {
+
+  def toOptions: Options = {
+    new Options().setCreateIfMissing(createIfMissing)
+  }
+
+  object RocksDBConfiguration {
+
+    def apply(location: String): RocksDBConfiguration = new RocksDBConfiguration(location)
+    def apply(location: String, createIfMissing: Boolean): RocksDBConfiguration =
+      new RocksDBConfiguration(location, createIfMissing)
+
+  }
+}
---
+++ ui/app/src/main/scala/com/ebiznext/comet/db/RocksDBConnection.scala
@@ -0,0 +1,14 @@
+package com.ebiznext.comet.db
+
+import org.rocksdb.RocksDB
+
+/**
+  * Created by Mourad on 23/07/2018.
+  */
+class RocksDBConnection(config: RocksDBConfiguration) {
+
+  lazy val db = RocksDB.open(config.toOptions, config.path)
+
+  RocksDB.loadLibrary()
+
+}
