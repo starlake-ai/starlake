126f6f3e6573838163ad3802ad54468865782ec6
Mourad DACHRAOUI <mourad.dachraoui@ebiznext.com>
2018-07-23 18:24:23 +0200

    test rocks db

--- ui/app/build.sbt
+++ ui/app/build.sbt
@@ -2,6 +2,7 @@ lazy val akkaHttpVersion = "10.0.11"
 lazy val akkaVersion = "2.5.11"
 lazy val rocksdbVersion = "5.7.3"
 lazy val json4sJackson = "3.5.4"
+lazy val commonsIOVersion = "2.6"
 
 lazy val root = (project in file(".")).
   settings(
@@ -17,6 +18,7 @@ lazy val root = (project in file(".")).
       "com.typesafe.akka" %% "akka-stream" % akkaVersion,
       "org.json4s" %% "json4s-jackson" % json4sJackson,
       "org.rocksdb" % "rocksdbjni" % rocksdbVersion,
+      "commons-io" % "commons-io" % commonsIOVersion,
       "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test,
       "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Test,
       "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % Test,
---
+++ ui/app/src/test/scala/com/ebiznext/comet/db/RocksDBConnectionSpec.scala
@@ -0,0 +1,36 @@
+package com.ebiznext.comet.db
+
+import java.nio.file.Files
+import java.util.UUID
+
+import org.apache.commons.io.FileUtils
+import org.scalatest.{ BeforeAndAfter, BeforeAndAfterAll, FlatSpec }
+
+/**
+ * Created by Mourad on 23/07/2018.
+ */
+class RocksDBConnectionSpec extends FlatSpec with BeforeAndAfter with BeforeAndAfterAll {
+
+  lazy val tempDir = Files.createTempDirectory("comet-test").toFile
+
+  override def afterAll(): Unit = {
+    FileUtils.deleteDirectory(tempDir.getAbsoluteFile)
+  }
+
+  def getConnection(): RocksDBConnection = {
+    val path = tempDir.getAbsolutePath + "/" + UUID.randomUUID()
+    new RocksDBConnection(RocksDBConfiguration(path))
+  }
+
+  "RocksDb" should "work" in {
+    val db = getConnection().db
+    (1 to 10).foreach { i =>
+      db.put(Array(i toByte), s"VALUE$i".toCharArray.map(_.toByte))
+    }
+    (1 to 10).foreach { i =>
+      val actual = new String(db.get(Array(i toByte)))
+      assert(actual == s"VALUE$i")
+    }
+  }
+
+}
