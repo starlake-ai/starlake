241420e2375fbdf5840fa2778ef987a0f7d21555
Mourad DACHRAOUI <mourad.dachraoui@ebiznext.com>
2018-07-31 14:58:17 +0200

    change signature

--- ui/app/src/main/scala/com/ebiznext/comet/services/ClusterService.scala
+++ ui/app/src/main/scala/com/ebiznext/comet/services/ClusterService.scala
@@ -2,29 +2,29 @@ package com.ebiznext.comet.services
 import java.nio.file.Path
 
 import com.ebiznext.comet.config.Settings
-import com.ebiznext.comet.db.{ RocksDBConfig, RocksDBConnection }
+import com.ebiznext.comet.db.{RocksDBConfig, RocksDBConnection}
 import com.ebiznext.comet.model.CometModel.Cluster
 import com.softwaremill.macwire._
 
-import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
 import scala.util.Try
 
 /**
- * Created by Mourad on 30/07/2018.
- */
-class ClusterService {
+  * Created by Mourad on 30/07/2018.
+  */
+class ClusterService(implicit executionContext: ExecutionContext) {
 
-  lazy val rocksDBConfig: RocksDBConfig = Settings.rocksDBConfig
+  lazy val rocksDBConfig: RocksDBConfig    = Settings.rocksDBConfig
   lazy val dbConnection: RocksDBConnection = wire[RocksDBConnection]
 
-  def create(userId: String, cluster: Cluster): Future[Try[String]] = ???
+  def create(userId: String, cluster: Cluster): Try[String] = ???
 
-  def delete(userId: String, clusterId: String): Future[Unit] = ???
+  def delete(userId: String, clusterId: String): Try[Unit] = ???
 
-  def update(userId: String, clusterId: String, newCluster: Cluster): Future[Try[Cluster]] = ???
+  def update(userId: String, clusterId: String, newCluster: Cluster): Try[Cluster] = ???
 
-  def clone(userId: String, clusterId: String, tagsOnly: Boolean): Future[Try[String]] = ???
+  def clone(userId: String, clusterId: String, tagsOnly: Boolean): Try[String] = ???
 
-  def buildAnsibleScript(userId: String, clusterId: String): Future[Try[Path]] = ???
+  def buildAnsibleScript(userId: String, clusterId: String): Try[Path] = ???
 
 }
