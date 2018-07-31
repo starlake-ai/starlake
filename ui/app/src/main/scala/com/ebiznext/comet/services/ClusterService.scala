48df974a5c44a202c88d923748944603de4227a0
Mourad DACHRAOUI <mourad.dachraoui@ebiznext.com>
2018-07-31 12:53:50 +0200

    db connection to the service (needs refactoring)

--- ui/app/src/main/scala/com/ebiznext/comet/services/ClusterService.scala
+++ ui/app/src/main/scala/com/ebiznext/comet/services/ClusterService.scala
@@ -1,15 +1,21 @@
 package com.ebiznext.comet.services
 import java.nio.file.Path
 
+import com.ebiznext.comet.config.Settings
+import com.ebiznext.comet.db.{ RocksDBConfig, RocksDBConnection }
 import com.ebiznext.comet.model.CometModel.Cluster
+import com.softwaremill.macwire._
 
 import scala.concurrent.Future
 import scala.util.Try
 
 /**
-  * Created by Mourad on 30/07/2018.
-  */
+ * Created by Mourad on 30/07/2018.
+ */
 class ClusterService {
+
+  lazy val rocksDBConfig: RocksDBConfig = Settings.rocksDBConfig
+  lazy val dbConnection: RocksDBConnection = wire[RocksDBConnection]
 
   def create(userId: String, cluster: Cluster): Future[Try[String]] = ???
 
