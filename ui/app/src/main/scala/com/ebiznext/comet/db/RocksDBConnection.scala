a6dca71f9de9eec1b7e9c022cb152fde634213de
Mourad DACHRAOUI <mourad.dachraoui@ebiznext.com>
2018-07-31 10:48:02 +0200

    Adds DBConnection Trait

--- ui/app/src/main/scala/com/ebiznext/comet/db/RocksDBConnection.scala
+++ ui/app/src/main/scala/com/ebiznext/comet/db/RocksDBConnection.scala
@@ -6,24 +6,24 @@ import org.rocksdb.{ RocksDB, WriteOptions }
 /**
  * Created by Mourad on 23/07/2018.
  */
-class RocksDBConnection(config: RocksDBConfig) {
+class RocksDBConnection(config: RocksDBConfig) extends DBConnection {
   import SerDeUtils._
   lazy val db = RocksDB.open(config.toOptions, config.path)
   lazy val writeOptions = new WriteOptions().setDisableWAL(false).setSync(true)
 
-  def close() = {
+  override def close(): Unit = {
     db.close()
   }
 
-  def read[V <: AnyRef](key: String)(implicit m: Manifest[V]): Option[V] = this.synchronized {
+  override def read[V <: AnyRef](key: String)(implicit m: Manifest[V]): Option[V] = this.synchronized {
     deserialize[V](db.get(serialize[String](key)))
   }
 
-  def write[V <: AnyRef](key: String, value: V)(implicit m: Manifest[V]): Unit = this.synchronized {
+  override def write[V <: AnyRef](key: String, value: V)(implicit m: Manifest[V]): Unit = this.synchronized {
     db.put(writeOptions, serialize[String](key), deserialize[V](value))
   }
 
-  def delete(key: String): Unit = this.synchronized {
+  override def delete(key: String): Unit = this.synchronized {
     db.delete(serialize[String](key))
   }
 
