2d6103cd577370c267254c2919fca7e2a771f27c
Mourad DACHRAOUI <mourad.dachraoui@ebiznext.com>
2018-08-01 17:13:44 +0200

    edit signatures for NodeService methods

--- ui/app/src/test/scala/com/ebiznext/comet/LauncherSpec.scala
+++ ui/app/src/test/scala/com/ebiznext/comet/LauncherSpec.scala
@@ -1,21 +1,21 @@
 package com.ebiznext.comet
 
-import com.ebiznext.comet.model.CometModel.{ Node, TagValue }
-import com.ebiznext.comet.services.{ NodeService, TagService }
+import com.ebiznext.comet.model.CometModel.{Node, TagValue}
+import com.ebiznext.comet.services.{NodeService, TagService}
 import com.ebiznext.comet.utils.Launcher
-import org.scalatest.{ Matchers, WordSpec }
+import org.scalatest.{Matchers, WordSpec}
 
 class LauncherSpec extends WordSpec with Matchers {
+  val path = getClass.getResource("/inventory.ini").getPath
   "Ansible" should {
     "getNodes" in {
-      val path = getClass.getResource("/inventory.ini").getPath
       val (exit, out, err) = Launcher.runCommand(s"ansible agent,master -i $path --list-hosts")
       assert(exit == 0)
-      new NodeService().getNodes().foreach(println)
+      new NodeService().getNodes(path).foreach(println)
       assert(true)
     }
     "getGroups" in {
-      val res = new NodeService().getGroups()
+      val res = new NodeService().getGroups(path)
       println(res)
     }
     "BuildTagsIni" in {
