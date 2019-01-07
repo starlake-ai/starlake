f7597b30b24fba3f45e8fb719698536c469aebae
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2019-01-07 13:54:20 +0100

    Allow user to customize area names

--- back/src/main/scala/com/ebiznext/comet/config/Settings.scala
+++ back/src/main/scala/com/ebiznext/comet/config/Settings.scala
@@ -9,12 +9,44 @@ object Settings extends StrictLogging {
 
   case class Airflow(endpoint: String)
 
-  case class Comet(datasets: String, metadata: String, archive: Boolean, launcher: String, analyze: Boolean, hive: Boolean, airflow: Airflow) {
+  case class Area(pending: String,
+                  unresolved: String,
+                  archive: String,
+                  ingesting: String,
+                  accepted: String,
+                  rejected: String,
+                  business: String
+                 )
+
+  case class Comet(datasets: String, metadata: String, archive: Boolean,
+                   launcher: String,
+                   analyze: Boolean, hive: Boolean,
+                   area: Area,
+                   airflow: Airflow) {
     def getLauncher(): LaunchHandler = launcher match {
       case "simple" => new SimpleLauncher
       case "airflow" => new AirflowLauncher
     }
   }
+
+  /*
+  area {
+  pending = "pending"
+  pending = ${?COMET_PENDING}
+  unresolved = "unresolved"
+  unresolved = ${?COMET_UNRESOLVED}
+  archive = "archive"
+  archive = ${?COMET_ARCHIVE}
+  ingesting = "ingesting"
+  ingesting = ${?COMET_INGESTING}
+  accepted = "accepted"
+  accepted = ${?COMET_ACCEPTED}
+  rejected = "rejected"
+  rejected = ${?COMET_REJECTED}
+  business = "business"
+  business = ${?COMET_BUSINESS}
+}
+   */
 
   val config: Config = ConfigFactory.load()
   val comet: Comet = config.extract[Comet].valueOrThrow { error =>
@@ -22,4 +54,6 @@ object Settings extends StrictLogging {
     throw new Exception("Failed to load config")
   }
   logger.info(s"Using Config $comet")
+
+
 }
