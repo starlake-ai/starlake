8e19f41d083c691dee057d8565dea82bb3556aff
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2019-01-08 14:13:57 +0100

    Correct acxcepted path

--- back/src/main/scala/com/ebiznext/comet/schema/model/package.scala
+++ back/src/main/scala/com/ebiznext/comet/schema/model/package.scala
@@ -3,6 +3,12 @@ package com.ebiznext.comet.schema
 import scala.collection.mutable
 
 package object model {
+  /**
+    * Find duplicates in a list of strings. Mostly used to check for attribute & schema names unicity
+    * @param values : strings
+    * @param errorMessage
+    * @return
+    */
   def duplicates(values: List[String], errorMessage: String): Either[List[String], Boolean] = {
     val errorList: mutable.MutableList[String] = mutable.MutableList.empty
     val duplicates = values.groupBy(identity).mapValues(_.size).filter {
