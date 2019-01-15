fc56a60ac5e1315dafe37e09a24914df0b5de137
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2019-01-15 01:30:27 +0100

    errors are now logged as simple strings

--- back/src/main/scala/com/ebiznext/comet/schema/model/Rejection.scala
+++ back/src/main/scala/com/ebiznext/comet/schema/model/Rejection.scala
@@ -13,28 +13,42 @@ object Rejection {
 
   /** Col information and status of parsing
     *
-    * @param colData : Column value found in row
-    * @param colName : Column source name
+    * @param colData  : Column value found in row
+    * @param colName  : Column source name
     * @param typeName : column semantic type name
-    * @param pattern : applied pattern
-    * @param success : true if column is valid, false otherwise
+    * @param pattern  : applied pattern
+    * @param success  : true if column is valid, false otherwise
     */
-  case class ColInfo(colData: String, colName: String, typeName: String, pattern: String, success: Boolean)
+  case class ColInfo(colData: String, colName: String, typeName: String, pattern: String, success: Boolean) {
+    override def toString: String = {
+      val failMsg = if (success) "success" else "failure"
+      s"$failMsg $colName:$typeName($pattern) = $colData"
+    }
+  }
 
 
   /**
     * Rejected Row information
+    *
     * @param timestamp : time of parsing for this row
-    * @param colInfos : column parsing results for this row
+    * @param colInfos  : column parsing results for this row
     */
-  case class RowInfo(timestamp: Timestamp, colInfos: List[ColInfo])
+  case class RowInfo(timestamp: Timestamp, colInfos: List[ColInfo]) {
+    override def toString: String = {
+      colInfos.map(_.toString).mkString("\n")
+
+    }
+  }
 
   /**
     * ColResult after parsing
-    * @param colInfo : Col info
+    *
+    * @param colInfo    : Col info
     * @param sparkValue : Spark Type as recognized by catalyst
     */
-  case class ColResult(colInfo: ColInfo, sparkValue: Any)
+  case class ColResult(colInfo: ColInfo, sparkValue: Any) {
+    override def toString: String = colInfo.toString
+  }
 
   /**
     *
@@ -42,8 +56,9 @@ object Rejection {
     */
   case class RowResult(colResults: List[ColResult]) {
     def isRejected: Boolean = colResults.exists(!_.colInfo.success)
-
     def isAccepted: Boolean = !isRejected
+
+    override def toString: String = colResults.map(_.toString).mkString("\n")
   }
 
 }
