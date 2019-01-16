a11c8abe60587b8f1aaaa386c6d3a3cdbceeec7f
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2019-01-16 16:04:50 +0100

    scaladoc

--- back/src/main/scala/com/ebiznext/comet/utils/Utils.scala
+++ back/src/main/scala/com/ebiznext/comet/utils/Utils.scala
@@ -3,6 +3,15 @@ package com.ebiznext.comet.utils
 import scala.util.control.NonFatal
 
 object Utils {
+  /**
+    * Handle tansparently autocloseable resources and correctly chain exceptions
+    *
+    * @param r : the resource
+    * @param f : the try bloc
+    * @tparam T : resource Type
+    * @tparam V : Try bloc return type
+    * @return : Try bloc return value
+    */
   def withResources[T <: AutoCloseable, V](r: => T)(f: T => V): V = {
     val resource: T = r
     require(resource != null, "resource is null")
