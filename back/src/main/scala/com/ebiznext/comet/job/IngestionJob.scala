559afebb1ef302f9d94b90eb1ff92345b1adf77c
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2019-01-23 17:58:51 +0100

    Merge support

--- back/src/main/scala/com/ebiznext/comet/job/IngestionJob.scala
+++ back/src/main/scala/com/ebiznext/comet/job/IngestionJob.scala
@@ -1,10 +1,11 @@
 package com.ebiznext.comet.job
 
 import com.ebiznext.comet.config.{DatasetArea, HiveArea, Settings}
+import com.ebiznext.comet.schema.handlers.StorageHandler
 import com.ebiznext.comet.schema.model._
 import org.apache.hadoop.fs.Path
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 
 /**
@@ -14,6 +15,8 @@ trait IngestionJob extends SparkJob {
   def domain: Domain
 
   def schema: Schema
+
+  def storageHandler: StorageHandler
 
   def types: List[Type]
 
@@ -37,14 +40,54 @@ trait IngestionJob extends SparkJob {
   def ingest(dataset: DataFrame): Unit
 
   def saveRejected(rejectedRDD: RDD[String]) = {
-    val writeMode = metadata.getWrite()
+    val writeMode = metadata.getWriteMode()
     val rejectedPath = new Path(DatasetArea.rejected(domain.name), schema.name)
     import session.implicits._
+    rejectedRDD.toDF.show(1000, false)
     saveRows(rejectedRDD.toDF, rejectedPath, writeMode, HiveArea.rejected)
   }
 
 
-  def saveAccepted(acceptedRDD: RDD[Row]): Unit
+  def getWriteMode(): WriteMode = schema.merge.map(_ => WriteMode.OVERWRITE).getOrElse(WriteMode.APPEND)
+
+  /**
+    * Merge new and existing dataset if required
+    * Save using overwrite / Append mode
+    *
+    * @param acceptedDF
+    */
+  def saveAccepted(acceptedDF: DataFrame): Unit = {
+    val writeMode = getWriteMode()
+    val acceptedPath = new Path(DatasetArea.accepted(domain.name), schema.name)
+    val mergedDF = schema.merge.map { mergeOptions =>
+      if (storageHandler.exist(new Path(acceptedPath, "_SUCCESS"))) {
+        val existingDF = session.read.parquet(acceptedPath.toString)
+        merge(acceptedDF, existingDF, mergeOptions)
+      }
+      else
+        acceptedDF
+    } getOrElse (acceptedDF)
+
+    saveRows(mergedDF, acceptedPath, writeMode, HiveArea.accepted, schema.merge.isDefined)
+  }
+
+  /**
+    * Merge incoming and existing dataframes using merge options
+    *
+    * @param inputDF
+    * @param existingDF
+    * @param merge
+    * @return merged dataframe
+    */
+  def merge(inputDF: DataFrame, existingDF: DataFrame, merge: MergeOptions): DataFrame = {
+    val toDeleteDF = existingDF.join(inputDF.select(merge.key.head, merge.key.tail: _*), merge.key)
+    val updatesDF = merge.delete.map(condition => inputDF.filter(s"not ($condition)")).getOrElse(inputDF)
+    logger.whenDebugEnabled {
+      logger.debug(s"Merge detected ${toDeleteDF.count()} items to update/delete")
+      logger.debug(s"Merge detected ${updatesDF.count()} items to update/insert")
+    }
+    existingDF.except(toDeleteDF).union(updatesDF)
+  }
 
   /**
     * Save typed dataset in parquet. If hive support is active, also register it as a Hive Table and if analyze is active, also compute basic statistics
@@ -54,7 +97,7 @@ trait IngestionJob extends SparkJob {
     * @param writeMode  : Append or overwrite
     * @param area       : accepted or rejected area
     */
-  def saveRows(dataset: DataFrame, targetPath: Path, writeMode: Write, area: HiveArea): Unit = {
+  def saveRows(dataset: DataFrame, targetPath: Path, writeMode: WriteMode, area: HiveArea, merge: Boolean = false): Unit = {
     if (dataset.columns.size > 0) {
       val count = dataset.count()
       val saveMode = writeMode.toSaveMode
@@ -71,9 +114,16 @@ trait IngestionJob extends SparkJob {
 
       val partitionedDF = partitionedDatasetWriter(dataset, metadata.partition.getOrElse(Nil))
 
-      val targetDataset = partitionedDF.mode(saveMode).format("parquet").option("path", targetPath.toString)
+      val mergePath = s"${targetPath.toString}.merge"
+      val targetDataset = if (merge) {
+        partitionedDF.mode(SaveMode.Overwrite).format("parquet").option("path", mergePath).save()
+        partitionedDatasetWriter(session.read.parquet(mergePath.toString), metadata.partition.getOrElse(Nil))
+      }
+      else
+        partitionedDF
+      val finalDataset = targetDataset.mode(saveMode).format("parquet").option("path", targetPath.toString)
       if (Settings.comet.hive) {
-        targetDataset.saveAsTable(fullTableName)
+        finalDataset.saveAsTable(fullTableName)
         val tableComment = schema.comment.getOrElse("")
         session.sql(s"ALTER TABLE $fullTableName SET TBLPROPERTIES ('comment' = '$tableComment')")
         if (Settings.comet.analyze) {
@@ -84,8 +134,10 @@ trait IngestionJob extends SparkJob {
         }
       }
       else {
-        targetDataset.save()
+        finalDataset.save()
       }
+      //storageHandler.delete(new Path(mergePath))
+
     } else {
       logger.warn("Empty dataset with no columns won't be saved")
     }
