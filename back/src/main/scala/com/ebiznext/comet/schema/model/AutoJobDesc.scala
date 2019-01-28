71336c2ce461bf2813647bdbaa81ced99ad58980
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2019-01-28 15:00:14 +0100

    Paritioning shoiuld be optional

--- back/src/main/scala/com/ebiznext/comet/schema/model/AutoJobDesc.scala
+++ back/src/main/scala/com/ebiznext/comet/schema/model/AutoJobDesc.scala
@@ -4,13 +4,16 @@ import com.ebiznext.comet.config.HiveArea
 
 /**
   * Task executed in teh context of a job
+  *
   * @param sql     SQL request to exexute (do not forget to prefix table names with the database name
   * @param domain  Output domain in Business Area (Will be the Database name in Hive)
   * @param dataset Dataset Name in Business Area (Will be the Table name in Hive)
   * @param write   Append to or overwrite existing data
   */
-case class AutoTask(sql: String, domain: String, dataset: String, write: WriteMode, partition: List[String],
-                    presql: Option[List[String]], postsql: Option[List[String]], area: Option[HiveArea] = None)
+case class AutoTask(sql: String, domain: String, dataset: String, write: WriteMode, partition: Option[List[String]],
+                    presql: Option[List[String]], postsql: Option[List[String]], area: Option[HiveArea] = None) {
+  def getPartitions() = partition.getOrElse(Nil)
+}
 
 /**
   *
