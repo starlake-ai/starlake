93f4111f5b4437910d91d2a8881036c4cf28c3e2
Abdelhamide EL ARIB <abdelhamide.elarib@ebiznext.com>
2019-02-05 22:43:42 +0100

    Refacto SBT

--- project/Common.scala
+++ project/Common.scala
@@ -1,3 +1,54 @@
+import sbt.{Def, _}
+import sbt.Keys._
+import com.typesafe.sbt.GitPlugin.autoImport._
+import sbtassembly.AssemblyKeys._
+
+object Common {
+
+  def enableCometAliases: Seq[Def.Setting[_]] =
+    Seq(
+      addCommandAlias("cd", "project"), // navigate the projects
+      addCommandAlias("cc", ";clean;compile"), // clean and compile
+      addCommandAlias("pl", ";clean;publishLocal"), // clean and publish locally
+      addCommandAlias("pr", ";clean;publish"), // clean and publish globally
+      addCommandAlias("pld", ";clean;local:publishLocal;dockerComposeUp") // clean and publish/launch the docker environment
+    ).flatten
+
+  def cometPlugins: Seq[AutoPlugin] = Seq(
+    com.typesafe.sbt.GitVersioning
+  )
+
+  def gitSettings = Seq(
+    git.useGitDescribe := true,
+    git.gitTagToVersionNumber := { tag: String =>
+      if (tag matches "[0-9]+\\..*") Some(tag)
+      else None
+    }
+  )
+
+  def assemlySettings = Seq(
+    test in assembly := {},
+    mainClass in Compile := Some("com.ebiznext.comet.job.Main")
+  )
+
+  def customSettings: Seq[Def.Setting[_]] =
+    Seq(
+      scalacOptions ++= Seq(
+        "-Xmacro-settings:materialize-derivations",
+      ),
+      testOptions in Test ++= Seq(
+        // show full stack traces and test case durations
+        Tests.Argument("-oDF"),
+        // -v Log "test run started" / "test started" / "test run finished" events on log level "info" instead of "debug".
+        // -a Show stack traces a nd exception class name for AssertionErrors.
+        Tests.Argument(TestFrameworks.JUnit, "-v", "-a")
+      ),
+      version := "0.1",
+      parallelExecution in Test := false
+    ) ++ gitSettings ++ assemlySettings
+
+}
+
 object Versions {
   val sparkAvro = "4.0.0"
   val hadoop = "2.7.3"
@@ -21,4 +72,12 @@ object Versions {
   val configs = "0.4.4"
 }
 
-object Common extends Definition
+object Resolvers {
+
+  val typeSafe = "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+  val allResolvers = Seq(
+    typeSafe
+  )
+
+}
