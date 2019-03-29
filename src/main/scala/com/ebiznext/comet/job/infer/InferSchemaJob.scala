06aef0ac86a624bdb28989c30ec4a0e1af85f295
Rayan <rayandinar95800@gmail.com>
2019-03-29 18:13:25 +0100

    Add parse command line for infer-schema

--- src/main/scala/com/ebiznext/comet/job/infer/InferSchemaJob.scala
+++ src/main/scala/com/ebiznext/comet/job/infer/InferSchemaJob.scala
@@ -2,13 +2,54 @@ package com.ebiznext.comet.job.infer
 
 import java.util.regex.Pattern
 
+import com.ebiznext.comet.job.infer.InferSchemaJob._
 import com.ebiznext.comet.schema.handlers.InferSchemaHandler
 import com.ebiznext.comet.schema.model.Attribute
 import com.ebiznext.comet.utils.SparkJob
 import org.apache.hadoop.fs.Path
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
-object InferSchemaJob extends SparkJob{
+
+case class InferSchema(dataPath : String,
+                       savePath: String) {
+
+  val path = new Path(dataPath)
+
+  val datasetWithoutFormat = readFile(path)
+
+  val dataframeWithFormat = createDataFrameWithFormat(datasetWithoutFormat, path)
+
+  val mode = getMode
+
+  val format = getFormatFile(datasetWithoutFormat,getExtensionFile(path))
+
+  val multiline = getMultiline
+
+  val array = if(format == "ARRAY_JSON") true else false
+
+  val withHeader = getWithHeader
+
+  val separator = getSeparator(datasetWithoutFormat)
+
+  val quote = getQuote
+
+  val escape = getEscape
+
+  val writeMode = getWriteMode
+
+  val inferSchema = new InferSchemaHandler(dataframeWithFormat)
+
+  val attributes: List[Attribute] = inferSchema.createAttributes(dataframeWithFormat.schema)
+  val metadata = inferSchema.createMetaData(mode,format,multiline,array,withHeader,separator,quote,escape,writeMode,None,false,None)
+
+  val schema = inferSchema.createSchema(getSchemaName(path),Pattern.compile(getSchemaPattern(path)),attributes,metadata)
+
+  val domain = inferSchema.createDomain(getDomainName(path),getDomainDirectoryName(path),None, List(schema))
+
+  inferSchema.generateYaml(domain, savePath)
+}
+
+object InferSchemaJob extends SparkJob {
 
   /** Read file without specifying the format
     *
@@ -20,8 +61,6 @@ object InferSchemaJob extends SparkJob{
       .textFile(path.toString)
   }
 
-
-  //TODO manage mode
   /** Get domain name
     *
     * @return the domain name
@@ -51,7 +90,7 @@ object InferSchemaJob extends SparkJob{
   def getFormatFile(datasetInit: Dataset[String], extension : String): String = {
     val firstLine = datasetInit.first()
 
-    firstLine.charAt(1).toString match {
+    firstLine.charAt(0).toString match {
       case "{"  => "JSON"
       case "[" => "ARRAY_JSON"
       case _  => if (extension.matches(".*sv$")) "DSV"
@@ -81,7 +120,7 @@ object InferSchemaJob extends SparkJob{
     * @return the domain name
     */
   def getDomainName(path: Path) : String = {
-    path.getName
+    path.getParent.getName
   }
 
   /** Get domain directory name
@@ -107,10 +146,10 @@ object InferSchemaJob extends SparkJob{
       fileName
   }
 
-  /** Get domain pattern
+  /** Get schema pattern
     *
     * @param path : file path
-    * @return the domain pattern
+    * @return the schema pattern
     */
   def getSchemaPattern(path: Path) : String = {
     path.getName
@@ -120,24 +159,30 @@ object InferSchemaJob extends SparkJob{
     *
     * @return the header option
     */
-  def getWithHeader : Boolean = {
-    false
-  }
+  def getWithHeader : Boolean = false
 
-  def getMultiline : Boolean = {
-    false
-  }
 
-  def getQuote: String = {
-    "\""
-  }
+  /** Get multiline option
+    *
+    * @return the multiline option
+    */
+  def getMultiline : Boolean = false
 
-  def getEscape: String = {
-    "\\"
-  }
+  /** Get quote option
+    *
+    * @return the quote option
+    */
+  def getQuote: String = "\""
+
+  /** Get escape option
+    *
+    * @return the escape option
+    */
+  def getEscape: String = "\\"
+
 
   def getWriteMode : String = {
-    "OVERWRITE"
+    "APPEND"
   }
 
   /**
@@ -176,37 +221,6 @@ object InferSchemaJob extends SparkJob{
     */
   override def run(): SparkSession = session
 
-  val path = new Path("/tmp/ty/toy.json")
-  val rf = readFile(path)
-  val cdf = createDataFrameWithFormat(rf, path)
 
-  val mode = getMode
-
-  val format = getFormatFile(rf,getExtensionFile(path))
-
-  val multiline = getMultiline
-
-  val array = if(format == "ARRAY_JSON") true else false
-
-  val withHeader = getWithHeader
-
-  val separator = getSeparator(rf)
-
-  val quote = getQuote
-
-  val escape = getEscape
-
-  val writeMode = getWriteMode
-
-  val inferSchema = new InferSchemaHandler(cdf)
-
-  val attributes: List[Attribute] = inferSchema.createAttributes(cdf.schema)
-  val metadata = inferSchema.createMetaData(mode,format,multiline,array,withHeader,separator,quote,escape,writeMode,None,false,None)
-
-  val schema = inferSchema.createSchema(getSchemaName(path),Pattern.compile(getSchemaPattern(path)),attributes,metadata)
-
-  val domain = inferSchema.createDomain(getDomainName(path),getDomainDirectoryName(path),None, List(schema))
-
-  inferSchema.generateYaml(domain, "/tmp/res.yml")
 
 }
