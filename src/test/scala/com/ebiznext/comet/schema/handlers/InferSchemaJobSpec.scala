46b6b6eee59ccd84c9f8a6376abe15ddb227ce86
Rayan <rayandinar95800@gmail.com>
2019-04-05 12:08:59 +0200

    Add some tests for check JsonArray and Json formats

--- src/test/scala/com/ebiznext/comet/schema/handlers/InferSchemaJobSpec.scala
+++ src/test/scala/com/ebiznext/comet/schema/handlers/InferSchemaJobSpec.scala
@@ -9,11 +9,17 @@ class InferSchemaJobSpec extends TestHelper {
   val dataset_csv: Dataset[String] = sparkSession.read
     .textFile("src/test/resources/sample/SCHEMA-VALID-NOHEADER.dsv")
 
-  val dataset_jsonArray: Dataset[String] = sparkSession.read
-    .textFile("src/test/resources/sample/simple-json-locations/locations.json")
-
   val dataset_psv: Dataset[String] = sparkSession.read
     .textFile("src/test/resources/quickstart/incoming/sales/customers-2018-01-01.psv")
+
+  val dataset_json: Dataset[String] = sparkSession.read
+    .textFile("src/test/resources/sample/json/complex.json")
+
+  val dataset_jsonArray: Dataset[String] = sparkSession.read
+    .textFile("src/test/resources/quickstart/incoming/hr/sellers-2018-01-01.json")
+
+  val dataset_jsonArrayMultiline: Dataset[String] = sparkSession.read
+    .textFile("src/test/resources/sample/simple-json-locations/locations.json")
 
   "GetSeparatorSemiColon" should "succeed" in {
 
@@ -27,9 +33,16 @@ class InferSchemaJobSpec extends TestHelper {
 
   }
 
-  "GetFormat" should "succeed" in {
+  "GetFormatCSV" should "succeed" in {
 
     InferSchemaJob.getFormatFile(dataset_csv) shouldBe "DSV"
+
+  }
+
+
+  "GetFormatJson" should "succeed" in {
+
+    InferSchemaJob.getFormatFile(dataset_json) shouldBe "JSON"
 
   }
 
@@ -38,5 +51,12 @@ class InferSchemaJobSpec extends TestHelper {
     InferSchemaJob.getFormatFile(dataset_jsonArray) shouldBe "ARRAY_JSON"
 
   }
+
+  "GetFormatArrayJsonMultiline" should "succeed" in {
+
+    InferSchemaJob.getFormatFile(dataset_jsonArrayMultiline) shouldBe "ARRAY_JSON"
+
+  }
+
 
 }
