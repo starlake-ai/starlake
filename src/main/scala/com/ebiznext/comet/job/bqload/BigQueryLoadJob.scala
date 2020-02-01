72fe0fac78be6e67b148a5bfefd5bef70e4f85c8
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2020-02-01 16:18:10 +0100

    Reformat error messages so they can be parsed as CSV

--- src/main/scala/com/ebiznext/comet/job/bqload/BigQueryLoadJob.scala
+++ src/main/scala/com/ebiznext/comet/job/bqload/BigQueryLoadJob.scala
@@ -109,11 +109,11 @@ class BigQueryLoadJob(
         .textFile(sourceJson)
         .map(text => (null, new JsonParser().parse(text).getAsJsonObject))
         .saveAsNewAPIHadoopDataset(conf)
-      Settings.storageHandler.delete(new Path(sourceJson))
       // Check the table
       val tableId = TableId.of(cliConfig.outputDataset, cliConfig.outputTable)
       val destinationTable = bigquery.getTable(tableId).getDefinition[StandardTableDefinition]
       logger.info("Loaded %d rows.\n", destinationTable.getNumRows)
+      Settings.storageHandler.delete(new Path(sourceJson))
       session
     }
   }
@@ -147,9 +147,7 @@ class BigQueryLoadJob(
     }
 
     def getOrCreateTable(): Table = {
-      import com.google.cloud.bigquery.TableId
-      import com.google.cloud.bigquery.StandardTableDefinition
-      import com.google.cloud.bigquery.TableInfo
+      import com.google.cloud.bigquery.{StandardTableDefinition, TableId, TableInfo}
 
       val tableId = TableId.of(cliConfig.outputDataset, cliConfig.outputTable)
 
@@ -187,7 +185,8 @@ class BigQueryLoadJob(
     val table = getOrCreateTable()
 
     Try {
-      val inputDataset = s"$bucket${cliConfig.sourceFile}/*.parquet"
+      val fs = Settings.comet.fileSystem.getOrElse("")
+      val inputDataset = s"$fs${cliConfig.sourceFile}/*.parquet"
       logger.info(s"source ds: $inputDataset")
       logger.info(s"TableId: ${table.getTableId}")
 
@@ -205,6 +204,95 @@ class BigQueryLoadJob(
         bigquery.getTable(table.getTableId).getDefinition.asInstanceOf[StandardTableDefinition]
       logger.info(s"Inserted ${stdTableDefinition.getNumRows} rows into BQ")
 
+      session
+    }
+  }
+
+  /**
+    * Just to force any spark job to implement its entry point using within the "run" method
+    *
+    * @return : Spark Session used for the job
+    */
+  def runSpark2(): Try[SparkSession] = {
+    val conf = session.sparkContext.hadoopConfiguration
+    logger.info(s"BigQuery Config $cliConfig")
+
+    val projectId = conf.get("fs.gs.project.id")
+    val bucket = conf.get("fs.gs.system.bucket")
+    // val outputTableId = projectId + ":wordcount_dataset.wordcount_output"
+    val outputTableId = s"$projectId:${cliConfig.outputDataset}.${cliConfig.outputTable}"
+    // Temp output bucket that is deleted upon completion of job.
+    val outputGcsPath = ("gs://" + bucket + "/tmp/" + UUID.randomUUID())
+    // Temp output bucket that is deleted upon completion of job.
+    val jsonPath = ("gs://" + bucket + "/tmp/" + UUID.randomUUID())
+    val inputPath = cliConfig.sourceFile
+    logger.info(s"Input path $inputPath")
+    logger.info(s"Json path $jsonPath")
+    BigQueryOutputConfiguration.configureWithAutoSchema(
+      conf,
+      outputTableId,
+      outputGcsPath,
+      BigQueryFileFormat.NEWLINE_DELIMITED_JSON,
+      classOf[TextOutputFormat[_, _]]
+    )
+    conf.set(
+      "mapreduce.job.outputformat.class",
+      classOf[IndirectBigQueryOutputFormat[_, _]].getName
+    )
+    conf.set(BigQueryConfiguration.OUTPUT_TABLE_WRITE_DISPOSITION_KEY, cliConfig.writeDisposition)
+    conf.set(BigQueryConfiguration.OUTPUT_TABLE_CREATE_DISPOSITION_KEY, cliConfig.createDisposition)
+    cliConfig.outputPartition.foreach { outputPartition =>
+      import com.google.api.services.bigquery.model.TimePartitioning
+      val timeField =
+        if (List("_PARTITIONDATE", "_PARTITIONTIME").contains(outputPartition))
+          new TimePartitioning().setType("DAY").setRequirePartitionFilter(true)
+        else
+          new TimePartitioning()
+            .setType("DAY")
+            .setRequirePartitionFilter(true)
+            .setField(outputPartition)
+      val timePartitioning =
+        new BigQueryTimePartitioning(
+          timeField
+        );
+      conf.set(BigQueryConfiguration.OUTPUT_TABLE_PARTITIONING_KEY, timePartitioning.getAsJson)
+    }
+
+    Try {
+      val bigqueryHelper = RemoteBigQueryHelper.create
+      val bigquery = bigqueryHelper.getOptions().getService();
+      val datasetId = DatasetId.of(projectId, cliConfig.outputDataset)
+      val dataset = scala.Option(bigquery.getDataset(datasetId))
+      dataset.getOrElse {
+        val datasetInfo = DatasetInfo
+          .newBuilder(cliConfig.outputDataset)
+          .setLocation(cliConfig.getLocation())
+          .build
+        bigquery.create(datasetInfo)
+      }
+      logger.info(s"dataset read")
+      val sourceJson = if (cliConfig.sourceFormat.equalsIgnoreCase("parquet")) {
+        val parquetDF = session.read.parquet(inputPath)
+        logger.info("Read parquet File")
+        parquetDF.write.json(jsonPath)
+        logger.info(s"Written to $jsonPath")
+        jsonPath
+      } else if (cliConfig.sourceFormat.equalsIgnoreCase("json")) {
+        inputPath
+      } else {
+        throw new Exception(s"Unknown format ${cliConfig.sourceFormat}")
+      }
+      logger.info(s"Source Json $sourceJson")
+
+      session.sparkContext
+        .textFile(sourceJson)
+        .map(text => (null, new JsonParser().parse(text).getAsJsonObject))
+        .saveAsNewAPIHadoopDataset(conf)
+      // Check the table
+      val tableId = TableId.of(cliConfig.outputDataset, cliConfig.outputTable)
+      val destinationTable = bigquery.getTable(tableId).getDefinition[StandardTableDefinition]
+      logger.info("Loaded %d rows.\n", destinationTable.getNumRows)
+      Settings.storageHandler.delete(new Path(sourceJson))
       session
     }
   }
