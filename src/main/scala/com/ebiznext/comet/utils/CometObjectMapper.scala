e8f1e7d2f7a3a3efcda847ba8c97297b09036706
Cyrille Chépélov <cyrille.chepelov@ebiznext.com>
2020-02-27 16:49:21 +0100

    chore: deduplicate the ObjectMapper settings & construction

---
+++ src/main/scala/com/ebiznext/comet/utils/CometObjectMapper.scala
@@ -0,0 +1,42 @@
+package com.ebiznext.comet.utils
+
+import com.ebiznext.comet.config.Settings
+import com.fasterxml.jackson.annotation.JsonIgnoreType
+import com.fasterxml.jackson.core.JsonFactory
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.databind.{InjectableValues, ObjectMapper}
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+
+object CometObjectMapper {
+
+  // https://github.com/FasterXML/jackson-databind/issues/962
+  @JsonIgnoreType
+  private class MixinsForObjectMapper
+
+}
+
+class CometObjectMapper(
+  jf: JsonFactory = null,
+  injectables: scala.collection.immutable.Seq[(Class[_], AnyRef)] = Nil
+) extends ObjectMapper(jf)
+    with ScalaObjectMapper {
+
+  this.registerModule(DefaultScalaModule)
+  this.registerModule(CometJacksonModule)
+
+  this.registerModule(
+    /* this thing ensures we'll never attempt to serialize the ObjectMapper itself even if it appears in a class
+    hierarchy. This happens, in particular, within [[Settings]].
+     */
+    new SimpleModule()
+      .setMixInAnnotation(classOf[ObjectMapper], classOf[CometObjectMapper.MixinsForObjectMapper])
+  )
+
+  if (injectables.nonEmpty) {
+    val iv = new InjectableValues.Std()
+    injectables.foreach { case (klass, value) => iv.addValue(klass, value) }
+    this.setInjectableValues(iv: InjectableValues)
+  }
+
+}
