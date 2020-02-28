31b438b291016749339461ebf1833d26c0a96894
Scala Steward <me@scala-steward.org>
2020-02-28 18:18:22 +0100

    Update elasticsearch-hadoop to 7.5.2

--- project/Common.scala
+++ project/Common.scala
@@ -110,7 +110,7 @@ object Versions {
   val jackson211 = "2.7.9"
   val jackson212 = "2.9.5"  
   val configs = "0.4.4"
-  val esHadoop = "7.5.1"
+  val esHadoop = "7.5.2"
   val scopt = "4.0.0-RC2"
   val sttp = "1.5.11"
 //  val gcs = "hadoop2-2.0.0"
