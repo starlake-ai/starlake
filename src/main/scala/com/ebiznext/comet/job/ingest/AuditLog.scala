scala-steward/comet-data-pipeline

WIP : JDBC Support

src/main/scala/com/ebiznext/comet/job/ingest/AuditLog.scala
@@ -24,7 +24,9 @@ import java.sql.Timestamp
 
 import com.ebiznext.comet.config.Settings
 import com.ebiznext.comet.job.bqload.{BigQueryLoadConfig, BigQueryLoadJob}
+import com.ebiznext.comet.job.jdbcload.JdbcLoadConfig
 import com.ebiznext.comet.utils.FileLock
+import com.google.cloud.bigquery.JobInfo.{CreateDisposition, WriteDisposition}
 import com.google.cloud.bigquery.{Field, LegacySQLTypeName}
 import org.apache.hadoop.fs.Path
 import org.apache.spark.rdd.RDD
@@ -121,7 +123,19 @@ object SparkAuditLogWriter {
       )
       .toDF(auditCols.map(_._1): _*)
 
-    if (Settings.comet.audit.index == "BQ") {
+    if (Settings.comet.audit.index == "JDBC") {
+      val jdbcConfig = JdbcLoadConfig(
+        Right(auditDF),
+        "audit",
+        CreateDisposition.CREATE_IF_NEEDED,
+        WriteDisposition.WRITE_APPEND,
+        Settings.comet.audit.options.get("jdbc-driver"),
+        Settings.comet.audit.options.get("jdbc-uri"),
+        Settings.comet.audit.options.get("jdbc-user"),
+        Settings.comet.audit.options.get("jdbc-password")
+      )
+    }
+    else if (Settings.comet.audit.index == "BQ") {
       val bqConfig = BigQueryLoadConfig(
         Right(auditDF),
         Settings.comet.audit.options.getOrDefault("bq-dataset", "audit"),

starlake-ai/starlake

src/main/scala/com/ebiznext/comet/job/ingest/AuditLog.scala
@@ -24,7 +24,9 @@ import java.sql.Timestamp
 
 import com.ebiznext.comet.config.Settings
 import com.ebiznext.comet.job.bqload.{BigQueryLoadConfig, BigQueryLoadJob}
+import com.ebiznext.comet.job.jdbcload.JdbcLoadConfig
 import com.ebiznext.comet.utils.FileLock
+import com.google.cloud.bigquery.JobInfo.{CreateDisposition, WriteDisposition}
 import com.google.cloud.bigquery.{Field, LegacySQLTypeName}
 import org.apache.hadoop.fs.Path
 import org.apache.spark.rdd.RDD
@@ -117,7 +119,19 @@ object SparkAuditLogWriter {
       )
       .toDF(auditCols.map(_._1): _*)
 
-    if (settings.comet.audit.index == "BQ") {
+    if (settings.comet.audit.index == "JDBC") {
+      val jdbcConfig = JdbcLoadConfig(
+        Right(auditDF),
+        "audit",
+        CreateDisposition.CREATE_IF_NEEDED,
+        WriteDisposition.WRITE_APPEND,
+        settings.comet.audit.options.get("jdbc-driver"),
+        settings.comet.audit.options.get("jdbc-uri"),
+        settings.comet.audit.options.get("jdbc-user"),
+        settings.comet.audit.options.get("jdbc-password")
+      )
+    }
+    else if (settings.comet.audit.index == "BQ") {
       val bqConfig = BigQueryLoadConfig(
         Right(auditDF),
         settings.comet.audit.options.getOrDefault("bq-dataset", "audit"),