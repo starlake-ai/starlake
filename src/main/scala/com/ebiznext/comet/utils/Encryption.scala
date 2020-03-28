1e7c97934ade438f2ec0139d4c5bf22b052be7cc
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2020-03-28 16:26:50 +0100

    Put back Encryption in its original folder.

---
+++ src/main/scala/com/ebiznext/comet/utils/Encryption.scala
@@ -0,0 +1,61 @@
+package com.ebiznext.comet.utils
+
+/**
+  * Several encryption methods used in privacy management
+  */
+object Encryption {
+
+  def algo(alg: String, data: String): String = {
+    val m = java.security.MessageDigest.getInstance(alg)
+    val b = data.getBytes("UTF-8")
+    m.update(b, 0, b.length)
+    new java.math.BigInteger(1, m.digest())
+      .toString(16)
+      .reverse
+      .padTo(32, '0')
+      .reverse
+      .mkString
+  }
+}
+
+trait Encryption {
+  def encrypt(s: String): String
+}
+
+object Md5 extends Encryption {
+  def encrypt(s: String): String = Encryption.algo("MD5", s)
+}
+
+object Sha1 extends Encryption {
+  def encrypt(s: String): String = Encryption.algo("SHA-1", s)
+}
+
+object Sha256 extends Encryption {
+  def encrypt(s: String): String = Encryption.algo("SHA-256", s)
+}
+
+object Sha512 extends Encryption {
+  def encrypt(s: String): String = Encryption.algo("SHA-512", s)
+}
+
+object Hide extends Encryption {
+  def encrypt(s: String): String = ""
+}
+
+object No extends Encryption {
+  def encrypt(s: String): String = s
+}
+
+object Initials extends Encryption {
+  def encrypt(s: String): String = {
+    s.split("\\s+").map(_.substring(0,1)).mkString(".")
+  }
+}
+
+object EmailMD5 extends Encryption {
+  def encrypt(s: String): String = {
+    val split = s.split('@')
+    Md5.encrypt(split(0)) + "@" + split(1)
+  }
+}
+
