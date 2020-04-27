4ee60f1b6c0015a2d1cc586febec2c51ce90f54f
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2020-04-27 12:34:54 +0200

    Feature/205 pre encrypt privacy (#206)
    * Use SCOPT to parse SchemaGen CLI arguments
    
    * Privacy is disabled when SchemaGen is generated with --privacy None.
    
    * Typo fix
    
    * Update Tests
    
    * Update deprecated FlatSpec to AnyFlatSpec
    
    * Update ShemaGen Config
    
    * Add Tests
    
    * No connection with this PR. Simply reformat AutoJob.
    Added new Issue

---
+++ src/main/scala/com/ebiznext/comet/schema/generator/SchemaGenConfig.scala
@@ -0,0 +1,69 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  * contributor license agreements.  See the NOTICE file distributed with
+ *  * this work for additional information regarding copyright ownership.
+ *  * The ASF licenses this file to You under the Apache License, Version 2.0
+ *  * (the "License"); you may not use this file except in compliance with
+ *  * the License.  You may obtain a copy of the License at
+ *  *
+ *  *    http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ *
+ */
+package com.ebiznext.comet.schema.generator
+
+import buildinfo.BuildInfo
+import com.ebiznext.comet.utils.CliConfig
+import scopt.OParser
+
+/**
+  *
+  * @param files List of Excel files
+  * @param encryption Should pre & post encryption YAML be generated ?
+  * @param privacy What privacy policies are to be applied at the pre-encrypt step ? All by default.
+  */
+case class SchemaGenConfig(
+  files: Seq[String] = Nil,
+  encryption: Boolean = true,
+  privacy: Seq[String] = Nil
+)
+
+object SchemaGenConfig extends CliConfig[SchemaGenConfig] {
+
+  val parser: OParser[Unit, SchemaGenConfig] = {
+    val builder = OParser.builder[SchemaGenConfig]
+    import builder._
+    OParser.sequence(
+      programName("comet"),
+      head("comet", BuildInfo.version),
+      opt[Seq[String]]("files")
+        .action((x, c) => c.copy(files = x))
+        .required()
+        .text("List of Excel files describing Domains & Schemas"),
+      opt[Boolean]("encryption")
+        .action((x, c) => c.copy(encryption = x))
+        .required()
+        .text("If true generate pre and post encryption YML"),
+      opt[Seq[String]]("privacy")
+        .action((x, c) => c.copy(privacy = x))
+        .optional()
+        .text("""What privacy policies should be applied in the pre-encryption phase ? 
+                | All privacy policies are applied by default.""".stripMargin)
+    )
+  }
+
+  /**
+    *
+    * @param args args list passed from command line
+    * @return Option of case class SchemaGenConfig.
+    */
+  def parse(args: Seq[String]): Option[SchemaGenConfig] =
+    OParser.parse(parser, args, SchemaGenConfig())
+}
