b51a135b25ecc04e64c2b05ce226c35673fed2a4
Scala Steward <me@scala-steward.org>
2020-05-15 20:14:23 +0200

    Update gcs-connector to hadoop3-2.1.3

--- project/Common.scala
+++ project/Common.scala
@@ -121,7 +121,7 @@ object Versions {
   val esHadoop = "7.6.2"
   val scopt = "4.0.0-RC2"
   val sttp = "1.7.2"
-  val gcs = "hadoop3-2.0.1"
+  val gcs = "hadoop3-2.1.3"
   val hadoopbq = "hadoop3-1.0.0"
   val bq = "1.114.0"
   val hadoop = "3.2.0"
