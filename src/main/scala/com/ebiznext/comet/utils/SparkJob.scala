92d4c2c5972ceb8ec02f8de7040c075cbfae7593
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2020-05-30 00:46:52 +0200

    Spark Job may now rertturn a Dataframe thatt we need to pipe directly without storing it in parquet

--- src/main/scala/com/ebiznext/comet/utils/SparkJob.scala
+++ src/main/scala/com/ebiznext/comet/utils/SparkJob.scala
@@ -8,6 +8,8 @@ import org.apache.spark.sql.{DataFrame, DataFrameWriter, Row, SparkSession}
 
 import scala.util.Try
 
+case class SparkJobResult(session: SparkSession, df: Option[DataFrame] = None)
+
 /**
   * All Spark Job extend this trait.
   * Build Spark session using spark variables from applciation.conf.
@@ -24,7 +26,7 @@ trait SparkJob extends StrictLogging {
     *
     * @return : Spark Session used for the job
     */
-  def run(): Try[SparkSession]
+  def run(): Try[SparkJobResult]
 
   /**
     * Partition a dataset using dataset columns.
