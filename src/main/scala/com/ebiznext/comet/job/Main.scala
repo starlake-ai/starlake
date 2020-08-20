85b70c770108bbc3df7b84601bb716e3b278a105
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2020-08-20 12:49:03 +0200

    Update Main and add xls2yml & extract as options

--- src/main/scala/com/ebiznext/comet/job/Main.scala
+++ src/main/scala/com/ebiznext/comet/job/Main.scala
@@ -22,17 +22,19 @@ package com.ebiznext.comet.job
 
 import buildinfo.BuildInfo
 import com.ebiznext.comet.config.{DatasetArea, Settings}
+import com.ebiznext.comet.extractor.ScriptGen
 import com.ebiznext.comet.job.atlas.AtlasConfig
-import com.ebiznext.comet.job.index.bqload.BigQueryLoadConfig
 import com.ebiznext.comet.job.convert.{Parquet2CSV, Parquet2CSVConfig}
+import com.ebiznext.comet.job.index.bqload.BigQueryLoadConfig
 import com.ebiznext.comet.job.index.esload.ESLoadConfig
-import com.ebiznext.comet.job.infer.InferSchemaConfig
-import com.ebiznext.comet.job.ingest.IngestConfig
 import com.ebiznext.comet.job.index.jdbcload.JdbcLoadConfig
+import com.ebiznext.comet.job.infer.InferSchemaConfig
+import com.ebiznext.comet.job.ingest.LoadConfig
 import com.ebiznext.comet.job.metrics.MetricsConfig
+import com.ebiznext.comet.schema.generator.Xls2Yml
 import com.ebiznext.comet.schema.handlers.SchemaHandler
 import com.ebiznext.comet.utils.{CometObjectMapper, FileLock}
-import com.ebiznext.comet.workflow.IngestionWorkflow
+import com.ebiznext.comet.workflow.{IngestionWorkflow, TransformConfig, WatchConfig}
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.typesafe.config.ConfigFactory
@@ -60,7 +62,7 @@ object Main extends StrictLogging {
         |comet job jobname
         |comet watch [+/-DOMAIN1,DOMAIN2,...]
         |comet import
-        |${IngestConfig.usage()}
+        |${LoadConfig.usage()}
         |${ESLoadConfig.usage()}
         |${BigQueryLoadConfig.usage()}
         |${InferSchemaConfig.usage()}
@@ -107,26 +109,24 @@ object Main extends StrictLogging {
     val arglist = args.toList
     logger.info(s"Running Comet $arglist")
     arglist.head match {
-      case "job" =>
-        if (arglist.length == 2)
-          workflow.autoJobRun(arglist(1))
-        else if (arglist.length == 3)
-          workflow.autoJobRun(arglist(1), Some(arglist(2)))
-        else logger.error(s"Number of arguments not respected: Please check your input: $arglist")
+      case "job" | "transform" =>
+        TransformConfig.parse(args.drop(1)) match {
+          case Some(config) =>
+            // do something
+            workflow.autoJob(config)
+          case _ =>
+            println(TransformConfig.usage())
+        }
       case "import" => workflow.loadLanding()
       case "watch" =>
-        if (arglist.length == 2) {
-          val param = arglist(1)
-          if (param.startsWith("-"))
-            workflow.loadPending(Nil, param.substring(1).split(',').toList)
-          else if (param.startsWith("+"))
-            workflow.loadPending(param.substring(1).split(',').toList, Nil)
-          else
-            workflow.loadPending(param.split(',').toList, Nil)
-        } else
-          workflow.loadPending()
-      case "ingest" =>
-        IngestConfig.parse(args.drop(1)) match {
+        WatchConfig.parse(args.drop(1)) match {
+          case Some(config) =>
+            workflow.loadPending(config)
+          case _ =>
+            println(WatchConfig.usage())
+        }
+      case "ingest" | "load" =>
+        LoadConfig.parse(args.drop(1)) match {
           case Some(config) =>
             val lockPath =
               new Path(settings.comet.lock.path, s"${config.domain}_${config.schema}.lock")
@@ -138,10 +138,10 @@ object Main extends StrictLogging {
             }
 
           case _ =>
-            println(IngestConfig.usage())
+            println(LoadConfig.usage())
 
         }
-      case "index" =>
+      case "index" | "esload" =>
         ESLoadConfig.parse(args.drop(1)) match {
           case Some(config) =>
             // do something
@@ -197,6 +197,11 @@ object Main extends StrictLogging {
             println(Parquet2CSVConfig.usage())
         }
 
+      case "xls2yml" =>
+        Xls2Yml.main(args.drop(1))
+
+      case "extract" =>
+        ScriptGen.main(args.drop(1))
       case _ => printUsage()
     }
   }
