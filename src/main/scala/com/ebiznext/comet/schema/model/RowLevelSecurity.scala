b2101d8ef92577540f489903632df2e97035e204
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2020-09-17 06:12:55 +0200

    row level security doc

--- src/main/scala/com/ebiznext/comet/schema/model/RowLevelSecurity.scala
+++ src/main/scala/com/ebiznext/comet/schema/model/RowLevelSecurity.scala
@@ -2,9 +2,10 @@ package com.ebiznext.comet.schema.model
 
 /**
   * User / Group and Service accounts rights on a subset of the table.
-  * @param name
-  * @param predicate
-  * @param grants
+  * @param name : This Row Level Security unique name
+  * @param predicate : The condition that goes to the WHERE clause and limitt the visible rows.
+  * @param grants : user / groups / service accounts to which this security level is applied.
+  *               ex : user:me@mycompany.com,group:group@mycompany.com,serviceAccount:mysa@google-accounts.com
   */
 case class RowLevelSecurity(
   name: String,
