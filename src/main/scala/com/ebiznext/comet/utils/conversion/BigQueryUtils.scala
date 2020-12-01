d0fbb8644361169e283a092db06720eac4c00025
Abdelhamide EL ARIB <me@elarib.com>
2020-12-01 12:19:29 +0100

    add test

--- src/main/scala/com/ebiznext/comet/utils/conversion/BigQueryUtils.scala
+++ src/main/scala/com/ebiznext/comet/utils/conversion/BigQueryUtils.scala
@@ -1,6 +1,7 @@
 package com.ebiznext.comet.utils.conversion
 
-import com.google.cloud.bigquery.{Field, FieldList, StandardSQLTypeName, Schema => BQSchema}
+import com.google.cloud.bigquery.{StandardSQLTypeName, Schema => BQSchema}
+import com.google.cloud.spark.bigquery.SchemaConverters
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types._
 
@@ -8,6 +9,8 @@ import org.apache.spark.sql.types._
   * Conversion between [X] Schema and BigQuery Schema
   */
 object BigQueryUtils {
+
+  val schemaConverter = new SchemaConverters
 
   implicit val sparkToBq: Convertible[DataFrame, BQSchema] = new Convertible[DataFrame, BQSchema] {
     override def apply(v1: DataFrame): BQSchema = bqSchema(v1.schema)
@@ -21,15 +24,16 @@ object BigQueryUtils {
       DataTypes.createDecimalType(BQ_NUMERIC_PRECISION, BQ_NUMERIC_SCALE)
 
     sparkType match {
-      case BooleanType                                     => StandardSQLTypeName.BOOL
-      case ByteType | LongType | IntegerType               => StandardSQLTypeName.INT64
-      case DoubleType | FloatType                          => StandardSQLTypeName.FLOAT64
-      case StringType                                      => StandardSQLTypeName.STRING
-      case BinaryType                                      => StandardSQLTypeName.BYTES
-      case DateType                                        => StandardSQLTypeName.DATE
-      case TimestampType                                   => StandardSQLTypeName.TIMESTAMP
-      case DecimalType.SYSTEM_DEFAULT | NUMERIC_SPARK_TYPE => StandardSQLTypeName.NUMERIC
-      case _                                               => throw new IllegalArgumentException(s"Unsupported type:$sparkType")
+      case BooleanType                                        => StandardSQLTypeName.BOOL
+      case ByteType | LongType | IntegerType                  => StandardSQLTypeName.INT64
+      case DoubleType | FloatType                             => StandardSQLTypeName.FLOAT64
+      case StringType                                         => StandardSQLTypeName.STRING
+      case BinaryType                                         => StandardSQLTypeName.BYTES
+      case DateType                                           => StandardSQLTypeName.DATE
+      case TimestampType                                      => StandardSQLTypeName.TIMESTAMP
+      case DecimalType.SYSTEM_DEFAULT | NUMERIC_SPARK_TYPE    => StandardSQLTypeName.NUMERIC
+      case StructType(_) | ArrayType(_, _) | MapType(_, _, _) => StandardSQLTypeName.STRUCT
+      case _                                                  => throw new IllegalArgumentException(s"Unsupported type:$sparkType")
     }
   }
 
@@ -39,62 +43,76 @@ object BigQueryUtils {
     */
 
   def bqSchema(schema: DataType): BQSchema = {
+//
+//    import scala.collection.JavaConverters._
+//    def inferBqSchema(field: String, dataType: DataType): Field = {
+//      (field, dataType) match {
+//        case (field: String, dataType: ArrayType) =>
+//          val builder: Field.Builder = convert(dataType.elementType) match {
+//            case StandardSQLTypeName.STRUCT =>
+//              val elementTypes: Seq[(String, DataType)] = fieldsSchemaAsMap(dataType.elementType)
+//              val arrayFields = elementTypes.map { case (name, dataType) =>
+//                Field
+//                  .newBuilder(
+//                    name,
+//                    convert(dataType)
+//                  )
+//                  .setMode(Field.Mode.NULLABLE)
+//                  .build()
+//              }.asJava
+//              Field
+//                .newBuilder(
+//                  field,
+//                  convert(dataType.elementType),
+//                  FieldList.of(arrayFields)
+//                )
+//            case bqDatatype =>
+//              Field
+//                .newBuilder(
+//                  field,
+//                  bqDatatype
+//                )
+//
+//          }
+//
+//          builder
+//            .setMode(Field.Mode.REPEATED)
+//            .setDescription("")
+//            .build()
+//        case (field: String, dataType: StructType) =>
+//          val converted = dataType.fields.map { attr =>
+//            inferBqSchema(attr.name, attr.dataType)
+//          }
+//          Field
+//            .newBuilder(
+//              field,
+//              StandardSQLTypeName.STRUCT,
+//              FieldList.of(converted.toList.asJava)
+//            )
+//            .setMode(Field.Mode.NULLABLE)
+//            .setDescription("")
+//            .build()
+//        case (field: String, dataType: DataType) =>
+//          Field
+//            .newBuilder(
+//              field,
+//              convert(dataType)
+//            )
+//            .setMode(Field.Mode.NULLABLE)
+//            .setDescription("")
+//            .build()
+//      }
+//    }
+//
+//    val fields = fieldsSchemaAsMap(schema)
+//      .map { case (field, dataType) =>
+//        inferBqSchema(field, dataType)
+//
+//      }
+//    BQSchema.of(fields: _*)
 
-    import scala.collection.JavaConverters._
-    def inferBqSchema(field: String, dataType: DataType): Field = {
-      (field, dataType) match {
-        case (field: String, dataType: ArrayType) =>
-          val elementTypes: Seq[(String, DataType)] = fieldsSchemaAsMap(dataType.elementType)
-          val arrayFields = elementTypes.map { case (name, dataType) =>
-            Field
-              .newBuilder(
-                name,
-                convert(dataType)
-              )
-              .setMode(Field.Mode.NULLABLE)
-              .setDescription("")
-              .build()
-          }.asJava
-          Field
-            .newBuilder(
-              field,
-              StandardSQLTypeName.STRUCT,
-              FieldList.of(arrayFields)
-            )
-            .setMode(Field.Mode.REPEATED)
-            .setDescription("")
-            .build()
-        case (field: String, dataType: StructType) =>
-          val converted = dataType.fields.map { attr =>
-            inferBqSchema(attr.name, attr.dataType)
-          }
-          Field
-            .newBuilder(
-              field,
-              StandardSQLTypeName.STRUCT,
-              FieldList.of(converted.toList.asJava)
-            )
-            .setMode(Field.Mode.NULLABLE)
-            .setDescription("")
-            .build()
-        case (field: String, dataType: DataType) =>
-          Field
-            .newBuilder(
-              field,
-              convert(dataType)
-            )
-            .setMode(Field.Mode.NULLABLE)
-            .setDescription("")
-            .build()
-      }
-    }
+    SchemaConverters.toBigQuerySchema(schema.asInstanceOf[StructType])
 
-    val fields = fieldsSchemaAsMap(schema)
-      .map { case (field, dataType) =>
-        inferBqSchema(field, dataType)
-
-      }
-    BQSchema.of(fields: _*)
   }
 
   /** The aim of this function is to retrieve columns and nested columns
