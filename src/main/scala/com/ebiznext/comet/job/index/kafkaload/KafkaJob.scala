b663a0eb231ffe0b7b882db87a1d8f1d6b27c2ff
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2021-02-01 09:46:38 +0100

    MAke KafkaClient a autocloseable resource

--- src/main/scala/com/ebiznext/comet/job/index/kafkaload/KafkaJob.scala
+++ src/main/scala/com/ebiznext/comet/job/index/kafkaload/KafkaJob.scala
@@ -1,8 +1,8 @@
 package com.ebiznext.comet.job.index.kafkaload
 
 import com.ebiznext.comet.config.Settings
-import com.ebiznext.comet.utils.kafka.KafkaTopicUtils
-import com.ebiznext.comet.utils.{JobResult, SparkJob, SparkJobResult}
+import com.ebiznext.comet.utils.kafka.KafkaClient
+import com.ebiznext.comet.utils.{JobResult, SparkJob, SparkJobResult, Utils}
 import org.apache.spark.sql.DataFrame
 
 import scala.util.Try
@@ -17,52 +17,54 @@ class KafkaJob(
 
   def offload(): Try[SparkJobResult] = {
     Try {
-      val kafkaUtils = new KafkaTopicUtils(settings.comet.kafka)
-      val (df, offsets) = kafkaUtils
-        .consumeTopic(
-          kafkaJobConfig.topic,
-          session,
-          topicConfig
-        )
-
-      val transformedDF = transfom(df)
-
-      val res = kafkaJobConfig.path match {
-        case Some(path) =>
-          transformedDF.write
-            .mode(kafkaJobConfig.mode)
-            .format(kafkaJobConfig.format)
-            .save(path)
-          kafkaUtils.topicSaveOffsets(
+      Utils.withResources(new KafkaClient(settings.comet.kafka)) { kafkaUtils =>
+        val (df, offsets) = kafkaUtils
+          .consumeTopic(
             kafkaJobConfig.topic,
-            topicConfig.accessOptions,
-            offsets
+            session,
+            topicConfig
           )
-          transformedDF
-        case None =>
-          logger.warn("Offsets not updated when no sink provided")
-          transformedDF
+
+        val transformedDF = transfom(df)
+
+        val res = kafkaJobConfig.path match {
+          case Some(path) =>
+            transformedDF.write
+              .mode(kafkaJobConfig.mode)
+              .format(kafkaJobConfig.format)
+              .save(path)
+            kafkaUtils.topicSaveOffsets(
+              kafkaJobConfig.topic,
+              topicConfig.accessOptions,
+              offsets
+            )
+            transformedDF
+          case None =>
+            logger.warn("Offsets not updated when no sink provided")
+            transformedDF
+        }
+        SparkJobResult(Some(res))
       }
-      SparkJobResult(Some(res))
     }
   }
 
   def load(): Try[SparkJobResult] = {
     Try {
-      val kafkaUtils = new KafkaTopicUtils(settings.comet.kafka)
-      kafkaJobConfig.path match {
-        case Some(path) =>
-          val df = session.read.format(kafkaJobConfig.format).load(path.split(','): _*)
-          val transformedDF = transfom(df)
+      Utils.withResources(new KafkaClient(settings.comet.kafka)) { kafkaUtils =>
+        kafkaJobConfig.path match {
+          case Some(path) =>
+            val df = session.read.format(kafkaJobConfig.format).load(path.split(','): _*)
+            val transformedDF = transfom(df)
 
-          kafkaUtils.sinkToTopic(
-            kafkaJobConfig.topic,
-            topicConfig,
-            transformedDF
-          )
-          SparkJobResult(Some(transformedDF))
-        case None =>
-          throw new Exception("No path provided !")
+            kafkaUtils.sinkToTopic(
+              kafkaJobConfig.topic,
+              topicConfig,
+              transformedDF
+            )
+            SparkJobResult(Some(transformedDF))
+          case None =>
+            throw new Exception("No path provided !")
+        }
       }
     }
   }
