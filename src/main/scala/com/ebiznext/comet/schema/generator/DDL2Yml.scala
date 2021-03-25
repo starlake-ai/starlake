095fe830b50a2e510b5ecd9fde8bcbd5e2c73836
Hayssam Saleh <hayssam@saleh.fr>
2021-03-25 22:58:56 +0100

    scalafmt

--- src/main/scala/com/ebiznext/comet/schema/generator/DDL2Yml.scala
+++ src/main/scala/com/ebiznext/comet/schema/generator/DDL2Yml.scala
@@ -46,8 +46,7 @@ object DDL2Yml extends LazyLogging {
   // The other part of the biMap
   val reverseSqlTypes = sqlTypes map (_.swap)
 
-  /**
-    * Generate YML file from JDBC Schema stoerd in a YML file
+  /** Generate YML file from JDBC Schema stoerd in a YML file
     * @param jdbcMapFile : Yaml File containing the JDBC Schema to extract
     * @param ymlOutputDir : Where to output the YML file. The generated filename
     *                     will be in the for TABLE_SCHEMA_NAME.yml
@@ -58,8 +57,7 @@ object DDL2Yml extends LazyLogging {
     run(jdbcSchema: JDBCSchema, ymlOutputDir)
   }
 
-  /**
-    * Generate YML file from the JDBCSchema
+  /** Generate YML file from the JDBCSchema
     * @param jdbcSchema : the JDBC Schema to extract
     * @param ymlOutputDir : Where to output the YML file. The generated filename
     *                     will be in the for TABLE_SCHEMA_NAME.yml
@@ -72,9 +70,8 @@ object DDL2Yml extends LazyLogging {
     val url = jdbcOptions.options("url")
     import java.sql.DriverManager
     val properties = new Properties()
-    (jdbcOptions.options - "url").foreach {
-      case (key, value) =>
-        properties.setProperty(key, value)
+    (jdbcOptions.options - "url").foreach { case (key, value) =>
+      properties.setProperty(key, value)
     }
     val connection = DriverManager.getConnection(url, properties)
     val databaseMetaData = connection.getMetaData()
@@ -82,8 +79,7 @@ object DDL2Yml extends LazyLogging {
       jdbcSchema.tables.map(tblSchema => tblSchema.table.toUpperCase -> tblSchema).toMap
     val tableNames = jdbcTableMap.keys.toList
 
-    /**
-      * Extract all tables from the database
+    /** Extract all tables from the database
       * @return Map of tablename -> tableDescription
       */
     def extractTables(): Map[String, String] = {
@@ -113,47 +109,46 @@ object DDL2Yml extends LazyLogging {
     }
 
     // Extract the Comet Schema
-    val cometSchema = selectedTables.map {
-      case (tableName, tableRemarks) =>
-        val resultSet = databaseMetaData.getColumns(
-          jdbcSchema.catalog.orNull,
-          jdbcSchema.schema,
-          tableName,
-          null
+    val cometSchema = selectedTables.map { case (tableName, tableRemarks) =>
+      val resultSet = databaseMetaData.getColumns(
+        jdbcSchema.catalog.orNull,
+        jdbcSchema.schema,
+        tableName,
+        null
+      )
+      val columns = ListBuffer.empty[Attribute]
+      while (resultSet.next()) {
+        val colName = resultSet.getString("COLUMN_NAME")
+        val colType = resultSet.getInt("DATA_TYPE")
+        val colRemarks = resultSet.getString("REMARKS")
+        val colRequired = resultSet.getString("IS_NULLABLE").equals("NO")
+        columns += Attribute(
+          name = colName,
+          `type` = sparkType(colType),
+          required = colRequired,
+          comment = Option(colRemarks)
         )
-        val columns = ListBuffer.empty[Attribute]
-        while (resultSet.next()) {
-          val colName = resultSet.getString("COLUMN_NAME")
-          val colType = resultSet.getInt("DATA_TYPE")
-          val colRemarks = resultSet.getString("REMARKS")
-          val colRequired = resultSet.getString("IS_NULLABLE").equals("NO")
-          columns += Attribute(
-            name = colName,
-            `type` = sparkType(colType),
-            required = colRequired,
-            comment = Option(colRemarks)
+      }
+      // Limit to the columns specified by the user if any
+      val currentTableRequestedColumns =
+        jdbcTableMap.get(tableName).map(_.columns.map(_.toUpperCase)).getOrElse(Nil)
+      val selectedColumns =
+        if (currentTableRequestedColumns.isEmpty)
+          columns.toList
+        else
+          columns.toList.filter(col =>
+            currentTableRequestedColumns.contains(col.name.toUpperCase())
           )
-        }
-        // Limit to the columns specified by the user if any
-        val currentTableRequestedColumns =
-          jdbcTableMap.get(tableName).map(_.columns.map(_.toUpperCase)).getOrElse(Nil)
-        val selectedColumns =
-          if (currentTableRequestedColumns.isEmpty)
-            columns.toList
-          else
-            columns.toList.filter(
-              col => currentTableRequestedColumns.contains(col.name.toUpperCase())
-            )
-        Schema(
-          tableName,
-          Pattern.compile(s"$tableName.*"),
-          selectedColumns,
-          None,
-          None,
-          Option(tableRemarks),
-          None,
-          None
-        )
+      Schema(
+        tableName,
+        Pattern.compile(s"$tableName.*"),
+        selectedColumns,
+        None,
+        None,
+        Option(tableRemarks),
+        None,
+        None
+      )
     }
     // Generate the domain with a dummy watch directory
     val domain =
