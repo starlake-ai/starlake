4c4ab9ec4c58c54c79b614b5724bf2b1de1be30f
Hayssam Saleh <hayssam@saleh.fr>
2021-04-05 16:19:06 +0200

    Update CLI docs

--- src/test/scala/com/ebiznext/comet/utils/CliConfigSpec.scala
+++ src/test/scala/com/ebiznext/comet/utils/CliConfigSpec.scala
@@ -4,12 +4,13 @@ import com.ebiznext.comet.TestHelper
 import com.ebiznext.comet.extractor.ExtractScriptGenConfig
 import com.ebiznext.comet.job.convert.Parquet2CSVConfig
 import com.ebiznext.comet.job.index.bqload.BigQueryLoadConfig
-import com.ebiznext.comet.job.index.esload.ESLoadConfig
 import com.ebiznext.comet.job.index.connectionload.ConnectionLoadConfig
+import com.ebiznext.comet.job.index.esload.ESLoadConfig
+import com.ebiznext.comet.job.index.kafkaload.KafkaJobConfig
 import com.ebiznext.comet.job.infer.InferSchemaConfig
 import com.ebiznext.comet.job.ingest.LoadConfig
 import com.ebiznext.comet.job.metrics.MetricsConfig
-import com.ebiznext.comet.schema.generator.Xls2YmlConfig
+import com.ebiznext.comet.schema.generator.{Xls2YmlConfig, Yml2XlsConfig}
 import com.ebiznext.comet.workflow.{ImportConfig, TransformConfig, WatchConfig}
 
 class CliConfigSpec extends TestHelper {
@@ -27,7 +28,9 @@ class CliConfigSpec extends TestHelper {
         "xls2yml"      -> Xls2YmlConfig.sphinx(),
         "extract"      -> ExtractScriptGenConfig.sphinx(),
         "transform"    -> TransformConfig.sphinx(),
-        "watch"        -> WatchConfig.sphinx()
+        "watch"        -> WatchConfig.sphinx(),
+        "kafkaload"    -> KafkaJobConfig.sphinx(),
+        "yml2xls"      -> Yml2XlsConfig.sphinx()
       )
       val rstPath = getClass.getResource("/").getPath + "../../../docs/user/cli"
       rstMap.foreach { case (k, v) =>
