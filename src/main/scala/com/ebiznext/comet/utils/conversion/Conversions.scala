db3134247bce74007f99d5997a1fd530c39571d5
Olivier Schultz <schultz.olivier83@gmail.com>
2021-04-22 19:36:38 +0200

    Move convertToScalaIterator to Utils package

---
+++ src/main/scala/com/ebiznext/comet/utils/conversion/Conversions.scala
@@ -0,0 +1,23 @@
+package com.ebiznext.comet.utils.conversion
+
+import org.apache.hadoop.fs.RemoteIterator
+
+import scala.language.implicitConversions
+
+object Conversions {
+
+  /** Make us able to use a java Hadoop Iterator as a scala Iterator
+    * Making us able to use filter, map ...
+    * @param underlying the java Hadoop Iterator
+    * @tparam T
+    * @return the augmented iterator
+    */
+  implicit def convertToScalaIterator[T](underlying: RemoteIterator[T]): Iterator[T] = {
+    case class wrapper(underlying: RemoteIterator[T]) extends Iterator[T] {
+      override def hasNext = underlying.hasNext
+
+      override def next = underlying.next
+    }
+    wrapper(underlying)
+  }
+}
