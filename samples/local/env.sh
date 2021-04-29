f32afa7ac03c1a304c181eecf2257ca3041bb3c5
Hayssam Saleh <hayssam@saleh.fr>
2021-04-29 18:01:30 +0200

    Add BQ Sample

--- samples/local/env.sh
+++ samples/local/env.sh
@@ -11,5 +11,5 @@ export COMET_HIVE=false
 export COMET_GROUPED=false
 export COMET_METRICS_PATH="/tmp/metrics/{domain}"
 export SPARK_CONF_OPTIONS="--conf spark.driver.extraJavaOptions=-Dlog4j.configuration=file://$SPARK_DIR/conf/log4j.properties.template"
-COMET_JAR_FULL_NAME="/Users/hayssams/git/public/comet-app/target/scala-2.12/comet-spark3_2.12-0.1.37-SNAPSHOT-assembly.jar"
+
 export COMET_SCRIPT="$SPARK_SUBMIT $SPARK_CONF_OPTIONS --class com.ebiznext.comet.job.Main $COMET_JAR_FULL_NAME"
