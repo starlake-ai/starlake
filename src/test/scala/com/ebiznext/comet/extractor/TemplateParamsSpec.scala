5009d49f6d0da96f649f1bcd8a0ec2afc64608b5
Hayssam Saleh <hayssam@saleh.fr>
2021-04-29 10:32:40 +0200

    Pattern to use is now in teh reference.conf file.

--- src/test/scala/com/ebiznext/comet/extractor/TemplateParamsSpec.scala
+++ src/test/scala/com/ebiznext/comet/extractor/TemplateParamsSpec.scala
@@ -1,78 +1,77 @@
 package com.ebiznext.comet.extractor
 
 import java.util.regex.Pattern
-
 import better.files.File
+import com.ebiznext.comet.TestHelper
 import com.ebiznext.comet.schema.model._
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
 
-class TemplateParamsSpec extends AnyFlatSpec with Matchers {
+class TemplateParamsSpec extends TestHelper {
   val scriptOutputFolder: File = File("/tmp")
+  new WithSettings() {
+    "fromSchema" should "generate the correct TemplateParams for a given Schema" in {
+      val schema: Schema = Schema(
+        name = "table1",
+        pattern = Pattern.compile("output_file.*.csv"),
+        List(
+          Attribute(name = "col1"),
+          Attribute(name = "col2", `type` = "long"),
+          Attribute(name = "col3", script = Some("script"))
+        ),
+        metadata = Option(Metadata(write = Some(WriteMode.APPEND))),
+        merge = Some(MergeOptions(List("col1", "col2"), None, timestamp = Some("updateCol"))),
+        comment = None,
+        presql = None,
+        postsql = None
+      )
 
-  "fromSchema" should "generate the correct TemplateParams for a given Schema" in {
-    val schema: Schema = Schema(
-      name = "table1",
-      pattern = Pattern.compile("output_file.*.csv"),
-      List(
-        Attribute(name = "col1"),
-        Attribute(name = "col2", `type` = "long"),
-        Attribute(name = "col3", script = Some("script"))
-      ),
-      metadata = Option(Metadata(write = Some(WriteMode.APPEND))),
-      merge = Some(MergeOptions(List("col1", "col2"), None, timestamp = Some("updateCol"))),
-      comment = None,
-      presql = None,
-      postsql = None
-    )
+      val expectedTemplateParams = TemplateParams(
+        domainToExport = "AnyDomain",
+        tableToExport = "table1",
+        columnsToExport = List("col1" -> "string", "col2" -> "long"),
+        fullExport = false,
+        dsvDelimiter = ",",
+        deltaColumn = Some("updateCol"),
+        exportOutputFileBase = "output_file",
+        scriptOutputFile = scriptOutputFolder / "EXTRACT_table1.sql"
+      )
+      TemplateParams.fromSchema(
+        "AnyDomain",
+        schema,
+        scriptOutputFolder,
+        None,
+        Some("updateCol")
+      ) shouldBe expectedTemplateParams
+    }
 
-    val expectedTemplateParams = TemplateParams(
-      domainToExport = "AnyDomain",
-      tableToExport = "table1",
-      columnsToExport = List("col1" -> "string", "col2" -> "long"),
-      fullExport = false,
-      dsvDelimiter = ",",
-      deltaColumn = Some("updateCol"),
-      exportOutputFileBase = "output_file",
-      scriptOutputFile = scriptOutputFolder / "EXTRACT_table1.sql"
-    )
-    TemplateParams.fromSchema(
-      "AnyDomain",
-      schema,
-      scriptOutputFolder,
-      None,
-      Some("updateCol")
-    ) shouldBe expectedTemplateParams
-  }
+    it should "generate the correct TemplateParams for an other Schema" in {
+      val schema: Schema = Schema(
+        name = "table1",
+        pattern = Pattern.compile("output_file.*.csv"),
+        List(Attribute(name = "col1"), Attribute(name = "col2", `type` = "long")),
+        metadata = Option(Metadata(write = Some(WriteMode.OVERWRITE), separator = Some("|"))),
+        merge = Some(MergeOptions(List("col1", "col2"), None, timestamp = Some("updateCol"))),
+        comment = None,
+        presql = None,
+        postsql = None
+      )
 
-  it should "generate the correct TemplateParams for an other Schema" in {
-    val schema: Schema = Schema(
-      name = "table1",
-      pattern = Pattern.compile("output_file.*.csv"),
-      List(Attribute(name = "col1"), Attribute(name = "col2", `type` = "long")),
-      metadata = Option(Metadata(write = Some(WriteMode.OVERWRITE), separator = Some("|"))),
-      merge = Some(MergeOptions(List("col1", "col2"), None, timestamp = Some("updateCol"))),
-      comment = None,
-      presql = None,
-      postsql = None
-    )
-
-    val expectedTemplateParams = TemplateParams(
-      domainToExport = "AnyDomain",
-      tableToExport = "table1",
-      columnsToExport = List("col1" -> "string", "col2" -> "long"),
-      fullExport = true,
-      dsvDelimiter = "|",
-      deltaColumn = None,
-      exportOutputFileBase = "output_file",
-      scriptOutputFile = scriptOutputFolder / "EXTRACT_table1.sql"
-    )
-    TemplateParams.fromSchema(
-      "AnyDomain",
-      schema,
-      scriptOutputFolder,
-      None,
-      None
-    ) shouldBe expectedTemplateParams
+      val expectedTemplateParams = TemplateParams(
+        domainToExport = "AnyDomain",
+        tableToExport = "table1",
+        columnsToExport = List("col1" -> "string", "col2" -> "long"),
+        fullExport = true,
+        dsvDelimiter = "|",
+        deltaColumn = None,
+        exportOutputFileBase = "output_file",
+        scriptOutputFile = scriptOutputFolder / "EXTRACT_table1.sql"
+      )
+      TemplateParams.fromSchema(
+        "AnyDomain",
+        schema,
+        scriptOutputFolder,
+        None,
+        None
+      ) shouldBe expectedTemplateParams
+    }
   }
 }
