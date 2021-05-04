b70edc52625c01fa708621ceb86c78de89304849
Hayssam Saleh <hayssam@saleh.fr>
2021-05-04 09:19:53 +0200

    Update baseline

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-    title: "Build Optimized Big & Fast Data Pipelines",
+    title: "Quickly Build Optimized Big & Fast Data Pipelines",
     tagline: 'So your data just keep moving',
     url: 'https://ebiznext.github.io',
     baseUrl: '/comet-data-pipeline/',
