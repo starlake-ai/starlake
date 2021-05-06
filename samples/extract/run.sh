601122b9ee50327c7a5da4cfa42461d5bd2b60ef
Hayssam Saleh <hayssam@saleh.fr>
2021-05-06 18:27:57 +0200

    Improve docs

--- samples/extract/run.sh
+++ samples/extract/run.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
+PROG_DIR=$(cd `dirname $0` && pwd)
+
+if [ "$PWD" != "$PROG_DIR" ]; then
+  echo "run command from local folder in the form ./0.init.sh"
+  exit 1
+fi
 
 if ls drivers/*.jar 1>/dev/null 2>&1; then
     echo "JAR in JDBC driver folfer found"
@@ -20,7 +25,7 @@ export COMET_SINK_TO_FILE=true
 
 
 
-COMET_JAR="comet-spark3_2.12-0.1.36-assembly.jar"
+COMET_JAR="comet-spark3_2.12-0.2.0.jar"
 COMET_OPTIONS="--conf spark.driver.extraJavaOptions=-Dconfig.file=$PWD/application.conf"
 COMET_LIBS="--jars drivers/postgresql-42.2.19.jar"
 COMET_SCRIPT="spark-3.1.1-bin-hadoop2.7/bin/spark-submit $COMET_LIBS $COMET_OPTIONS --class com.ebiznext.comet.schema.generator.DDL2Yml ${COMET_JAR}"
