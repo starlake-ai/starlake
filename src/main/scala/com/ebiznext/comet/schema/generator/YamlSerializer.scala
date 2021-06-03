c2c4fa0cdf6b41d3abd47cbe903cf68701bf5d01
Frédéric TAPIN <frederic.tapin@dalenys.com>
2021-06-03 16:52:57 +0200

    Handle multiple schemas during extraction

--- src/main/scala/com/ebiznext/comet/schema/generator/YamlSerializer.scala
+++ src/main/scala/com/ebiznext/comet/schema/generator/YamlSerializer.scala
@@ -29,11 +29,11 @@ object YamlSerializer extends LazyLogging {
     mapper.readValue(jsonContent, classOf[Map[String, Any]])
   }
 
-  def serialize(jdbcSchema: JDBCSchema): String = mapper.writeValueAsString(jdbcSchema)
+  def serialize(jdbcSchemas: JDBCSchemas): String = mapper.writeValueAsString(jdbcSchemas)
 
-  def deserializeJDBCSchema(file: File) = {
+  def deserializeJDBCSchemas(file: File): JDBCSchemas = {
     val rootNode = mapper.readTree(file.newInputStream)
-    val extractNode = rootNode.path("jdbc-schema")
+    val extractNode = rootNode.path("extract")
     val jdbcNode =
       if (extractNode.isNull() || extractNode.isMissingNode) {
         logger.warn(
@@ -42,7 +42,7 @@ object YamlSerializer extends LazyLogging {
         rootNode
       } else
         extractNode
-    mapper.treeToValue(jdbcNode, classOf[JDBCSchema])
+    mapper.treeToValue(jdbcNode, classOf[JDBCSchemas])
   }
 
   def deserializeDomain(file: File): Try[Domain] = {
