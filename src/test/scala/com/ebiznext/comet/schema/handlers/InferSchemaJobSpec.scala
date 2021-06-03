da40e5cdbacaf6714e100195472c5936ec857bd4
abdelhakimbendjabeur <abdelhakim.bendjabeur@gmail.com>
2021-06-03 17:18:47 +0200

    Improve InferSchemaJob

--- src/test/scala/com/ebiznext/comet/schema/handlers/InferSchemaJobSpec.scala
+++ src/test/scala/com/ebiznext/comet/schema/handlers/InferSchemaJobSpec.scala
@@ -2,62 +2,70 @@ package com.ebiznext.comet.schema.handlers
 
 import com.ebiznext.comet.TestHelper
 import com.ebiznext.comet.job.infer.InferSchemaJob
-import org.apache.spark.sql.Dataset
+
+import scala.io.Source
 
 class InferSchemaJobSpec extends TestHelper {
 
   new WithSettings() {
 
-    lazy val dataset_csv: Dataset[String] = sparkSession.read
-      .textFile("src/test/resources/sample/SCHEMA-VALID-NOHEADER.dsv")
+    lazy val csvLines =
+      Source.fromFile("src/test/resources/sample/SCHEMA-VALID-NOHEADER.dsv").getLines().toList
 
-    lazy val dataset_psv: Dataset[String] = sparkSession.read
-      .textFile("src/test/resources/quickstart/incoming/sales/customers-2018-01-01.psv")
+    lazy val psvLines = Source
+      .fromFile("src/test/resources/quickstart/incoming/sales/customers-2018-01-01.psv")
+      .getLines()
+      .toList
 
-    lazy val dataset_json: Dataset[String] = sparkSession.read
-      .textFile("src/test/resources/sample/json/complex.json")
+    lazy val jsonLines =
+      Source.fromFile("src/test/resources/sample/json/complex.json").getLines().toList
 
-    lazy val dataset_jsonArray: Dataset[String] = sparkSession.read
-      .textFile("src/test/resources/quickstart/incoming/hr/sellers-2018-01-01.json")
+    lazy val jsonArrayLines = Source
+      .fromFile("src/test/resources/quickstart/incoming/hr/sellers-2018-01-01.json")
+      .getLines()
+      .toList
 
-    lazy val dataset_jsonArrayMultiline: Dataset[String] = sparkSession.read
-      .textFile("src/test/resources/sample/simple-json-locations/locations.json")
+    lazy val jsonArrayMultilinesLines =
+      Source
+        .fromFile("src/test/resources/sample/simple-json-locations/locations.json")
+        .getLines()
+        .toList
 
     lazy val inferSchemaJob: InferSchemaJob = new InferSchemaJob()
 
     "GetSeparatorSemiColon" should "succeed" in {
 
-      inferSchemaJob.getSeparator(dataset_csv) shouldBe ";"
+      inferSchemaJob.getSeparator(csvLines) shouldBe ";"
 
     }
 
     "GetSeparatorPipe" should "succeed" in {
 
-      inferSchemaJob.getSeparator(dataset_psv) shouldBe "|"
+      inferSchemaJob.getSeparator(psvLines) shouldBe "|"
 
     }
 
     "GetFormatCSV" should "succeed" in {
 
-      inferSchemaJob.getFormatFile(dataset_csv) shouldBe "DSV"
+      inferSchemaJob.getFormatFile(csvLines) shouldBe "DSV"
 
     }
 
     "GetFormatJson" should "succeed" in {
 
-      inferSchemaJob.getFormatFile(dataset_json) shouldBe "JSON"
+      inferSchemaJob.getFormatFile(jsonLines) shouldBe "JSON"
 
     }
 
     "GetFormatArrayJson" should "succeed" in {
 
-      inferSchemaJob.getFormatFile(dataset_jsonArray) shouldBe "ARRAY_JSON"
+      inferSchemaJob.getFormatFile(jsonArrayLines) shouldBe "ARRAY_JSON"
 
     }
 
     "GetFormatArrayJsonMultiline" should "succeed" in {
 
-      inferSchemaJob.getFormatFile(dataset_jsonArrayMultiline) shouldBe "ARRAY_JSON"
+      inferSchemaJob.getFormatFile(jsonArrayMultilinesLines) shouldBe "ARRAY_JSON"
 
     }
   }
