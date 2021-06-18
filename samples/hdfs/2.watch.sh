b373fb5b9e781564071b365498efa52aa24843a5
Hayssam Saleh <hayssam@saleh.fr>
2021-06-18 11:14:16 +0200

    Update docusaurus & correct extrajavadirs in samples

--- samples/hdfs/2.watch.sh
+++ samples/hdfs/2.watch.sh
@@ -3,4 +3,4 @@ set -x
 
 awk -v hdfs="$HDFS_URI" '{gsub("HDFS_URI", hdfs)}1' hdfs-site.xml >../bin/$SPARK_DIR_NAME/conf/hdfs-site.xml
 
-$COMET_SCRIPT watch
+$SPARK_SUBMIT --driver-java-options "$SPARK_DRIVER_OPTIONS" $SPARK_CONF_OPTIONS --class $COMET_MAIN $COMET_JAR_FULL_NAME watch
--- samples/hdfs/3.transform.sh
+++ samples/hdfs/3.transform.sh
@@ -1,3 +1,5 @@
 source ./env.sh
 set -x
-$COMET_SCRIPT transform --name kpi
+
+$SPARK_SUBMIT --driver-java-options "$SPARK_DRIVER_OPTIONS" $SPARK_CONF_OPTIONS --class $COMET_MAIN $COMET_JAR_FULL_NAME transform --name kpi
+
