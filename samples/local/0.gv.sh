364ab83b7df100962ed4a20d26e01380d89bae4e
Hayssam Saleh <hayssam@saleh.fr>
2021-06-18 11:14:16 +0200

    Update docusaurus & correct extrajavadirs in samples

--- samples/local/0.gv.sh
+++ samples/local/0.gv.sh
@@ -1,4 +1,4 @@
 source ./env.sh
 initEnv
 set -x
-$COMET_SCRIPT yml2gv
+$SPARK_SUBMIT --driver-java-options "$SPARK_DRIVER_OPTIONS" $SPARK_CONF_OPTIONS --class $COMET_MAIN $COMET_JAR_FULL_NAME yml2gv
