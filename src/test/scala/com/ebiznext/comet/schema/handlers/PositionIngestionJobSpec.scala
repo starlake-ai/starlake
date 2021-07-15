0aeda1dceb4b437b0b143826c95ffc1153143bf0
Mohamad Kassir <mbkassir@gmail.com>
2021-07-16 00:28:34 +0200

    add tests for empty files

--- src/test/scala/com/ebiznext/comet/schema/handlers/PositionIngestionJobSpec.scala
+++ src/test/scala/com/ebiznext/comet/schema/handlers/PositionIngestionJobSpec.scala
@@ -70,6 +70,21 @@ class PositionIngestionJobSpec extends TestHelper {
 
     }
   }
+  "Ingestion of empty Position file" should "run without errors" in {
+    new WithSettings() {
+      new SpecTrait(
+        domainFilename = "position.comet.yml",
+        sourceDomainPathname = "/sample/position/position.comet.yml",
+        datasetDomainName = "position",
+        sourceDatasetPathName = "/sample/position/empty_position"
+      ) {
+        cleanMetadata
+        cleanDatasets
+        logger.info(settings.comet.datasets)
+        loadPending shouldBe true
+      }
+    }
+  }
   "Ingest Position File" should "use encoding when loading files" in {
     new WithSettings() {
       new SpecTrait(
