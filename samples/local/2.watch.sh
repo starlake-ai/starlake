cbbaad8605801c661d499beb42aa8a88bb042909
Hayssam Saleh <hayssam@saleh.fr>
2021-08-26 16:38:36 +0200

    improve samples

--- samples/local/2.watch.sh
+++ samples/local/2.watch.sh
@@ -1,5 +1,8 @@
 source ./env.sh
 set -x
-#SPARK_DRIVER_OPTIONS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -Dlog4j.configuration=file://$SPARK_DIR/conf/log4j.properties.template"
 
+export COMET_SPARK_SQL_SOURCES_PARTITION_OVERWRITE_MODE=dynamic
+export COMET_MERGE_OPTIMIZE_PARTITION_WRITE=true
+
+SPARK_DRIVER_OPTIONS="-Dconfig.file=$PWD/application.conf  -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -Dlog4j.configuration=file://$SPARK_DIR/conf/log4j.properties.template"
 $SPARK_SUBMIT --driver-java-options "$SPARK_DRIVER_OPTIONS" $SPARK_CONF_OPTIONS --class $COMET_MAIN $COMET_JAR_FULL_NAME watch
