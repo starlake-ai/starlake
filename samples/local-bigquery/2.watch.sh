d5f58a9c8deb4badb9eb6c50a699366bba57fbe5
Hayssam Saleh <hayssam@saleh.fr>
2021-08-26 16:38:36 +0200

    improve samples

--- samples/local-bigquery/2.watch.sh
+++ samples/local-bigquery/2.watch.sh
@@ -3,5 +3,9 @@ set -x
 
 awk -v bucket="$GCP_BUCKET_NAME" -v project="$GCP_PROJECT_ID" -v json="$GCP_SA_JSON_PATH" '{gsub("GCP_BUCKET_NAME", bucket);gsub("GCP_PROJECT_ID", project);gsub("GCP_SA_JSON_PATH", json)}1' core-site.xml >../bin/$SPARK_DIR_NAME/conf/core-site.xml
 
+export COMET_SPARK_SQL_SOURCES_PARTITION_OVERWRITE_MODE=dynamic
+export COMET_MERGE_OPTIMIZE_PARTITION_WRITE=true
+
+#SPARK_DRIVER_OPTIONS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -Dlog4j.configuration=file://$SPARK_DIR/conf/log4j.properties.template"
 
 $SPARK_SUBMIT --driver-java-options "$SPARK_DRIVER_OPTIONS" $SPARK_CONF_OPTIONS  --class $COMET_MAIN $COMET_JAR_FULL_NAME watch
