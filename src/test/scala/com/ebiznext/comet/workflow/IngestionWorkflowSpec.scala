0d9a925351e5cc51bf82e569be8110a4e9b0ed7e
Nicolas Boussuge <nicolas.boussuge@dalenys.com>
2021-09-01 13:17:25 +0200

    add tests with Domain.ack defined, rework loadLanding to accept only expected extensions

--- src/test/scala/com/ebiznext/comet/workflow/IngestionWorkflowSpec.scala
+++ src/test/scala/com/ebiznext/comet/workflow/IngestionWorkflowSpec.scala
@@ -10,25 +10,39 @@ class IngestionWorkflowSpec extends TestHelper {
     private def loadLandingFile(landingFile: String, pendingFile: String) = {
       new SpecTrait(
         domainOrJobFilename = "DOMAIN.comet.yml",
-        sourceDomainOrJobPathname = s"/sample/DOMAIN.comet.yml",
+        sourceDomainOrJobPathname = "/sample/DOMAIN.comet.yml",
         datasetDomainName = "DOMAIN",
         sourceDatasetPathName = landingFile
       ) {
         cleanMetadata
         cleanDatasets
 
-        loadLanding
-        val destFolder = DatasetArea.pending(datasetDomainName)
-        val targetFile = new Path(
-          destFolder,
-          pendingFile
-        )
-        assert(storageHandler.exists(targetFile))
-      }
-    }
+        storageHandler.delete(new Path(landingPath))
+        // Make sure unrelated files, even without extensions, are not imported
+        withSettings.deliverTestFile("/sample/_SUCCESS", new Path(landingPath, "_SUCCESS"))
 
-    "Loading files without extension in Landing area" should "produce file in pending area" in {
-      loadLandingFile("/sample/_SUCCESS", "_SUCCESS")
+        loadLanding
+        val destFolder: Path = DatasetArea.pending(datasetDomainName)
+        assert(storageHandler.exists(new Path(destFolder, pendingFile)), "Landing file directly imported")
+        assert(!storageHandler.exists(new Path(destFolder, "_SUCCESS")), "Unrelated file ignored")
+      }
+
+      // Test again, but with Domain.ack defined
+      new SpecTrait(
+        domainOrJobFilename = "DOMAIN.comet.yml",
+        sourceDomainOrJobPathname = "/sample/DOMAIN-ACK.comet.yml",
+        datasetDomainName = "DOMAIN",
+        sourceDatasetPathName = landingFile
+      ) {
+        cleanMetadata
+        cleanDatasets
+
+        storageHandler.delete(new Path(landingPath))
+
+        loadLanding
+        val destFolder: Path = DatasetArea.pending(datasetDomainName)
+        assert(storageHandler.exists(new Path(destFolder, pendingFile)), "Landing file based on extension imported")
+      }
     }
 
     "Loading files in Landing area" should "produce file in pending area" in {
