38e5566418f395774542c8e3085989319c3e21c0
Hayssam Saleh <hayssam@saleh.fr>
2021-12-05 10:19:19 +0100

    Activate test only when COMET_GCP_TEST is defined

--- src/test/scala/ai/starlake/gcp/DataCatalogTest.scala
+++ src/test/scala/ai/starlake/gcp/DataCatalogTest.scala
@@ -33,41 +33,46 @@ class DataCatalogTest
     with StrictLogging
     with DatasetLogging {
   "Taxonomy" should "list files by modification_time and name" in {
-    val client = PolicyTagManagerClient.create()
-    val taxonomyListRequest =
-      ListTaxonomiesRequest.newBuilder().setParent("projects/starlake-325712/locations/eu").build()
-    val taxonomyList = client.listTaxonomies(taxonomyListRequest)
-    val taxonomyRef =
-      taxonomyList.iterateAll().asScala.filter(_.getDisplayName() == "RGPD").map(_.getName).head
-    val policyTagsRequest = ListPolicyTagsRequest.newBuilder().setParent(taxonomyRef).build()
-    val policyTags = client.listPolicyTags(policyTagsRequest)
-    val policyTagRef =
-      policyTags.iterateAll().asScala.filter(_.getDisplayName() == "PHI").map(_.getName).head
+    if (sys.env.getOrElse("COMET_GCP_TEST", "false").toBoolean) {
+      val client = PolicyTagManagerClient.create()
+      val taxonomyListRequest =
+        ListTaxonomiesRequest
+          .newBuilder()
+          .setParent("projects/starlake-325712/locations/eu")
+          .build()
+      val taxonomyList = client.listTaxonomies(taxonomyListRequest)
+      val taxonomyRef =
+        taxonomyList.iterateAll().asScala.filter(_.getDisplayName() == "RGPD").map(_.getName).head
+      val policyTagsRequest = ListPolicyTagsRequest.newBuilder().setParent(taxonomyRef).build()
+      val policyTags = client.listPolicyTags(policyTagsRequest)
+      val policyTagRef =
+        policyTags.iterateAll().asScala.filter(_.getDisplayName() == "PHI").map(_.getName).head
 
-    val getPolicyTagRequest =
-      GetPolicyTagRequest.newBuilder().setName(policyTagRef.toString).build()
-    val policyTag = client.getPolicyTag(getPolicyTagRequest)
-    println(policyTag)
+      val getPolicyTagRequest =
+        GetPolicyTagRequest.newBuilder().setName(policyTagRef.toString).build()
+      val policyTag = client.getPolicyTag(getPolicyTagRequest)
+      println(policyTag)
 
-    val bigquery: BigQuery = BigQueryOptions.getDefaultInstance.getService
-    val tableId = TableId.of("audit", "audit")
-    val table: Table = bigquery.getTable(tableId)
-    val tableDefinition = table.getDefinition().asInstanceOf[StandardTableDefinition]
-    val bqSchema = tableDefinition.getSchema()
-    val bqFields = bqSchema.getFields.asScala.toList
-    val head = bqFields.head
-    val tail = bqFields.tail
-    val newField = Field
-      .newBuilder(head.getName, head.getType, head.getSubFields)
-      .setPolicyTags(
-        PolicyTags.newBuilder().setNames(List(policyTagRef).asJava).build()
-      )
-      .build()
+      val bigquery: BigQuery = BigQueryOptions.getDefaultInstance.getService
+      val tableId = TableId.of("audit", "audit")
+      val table: Table = bigquery.getTable(tableId)
+      val tableDefinition = table.getDefinition().asInstanceOf[StandardTableDefinition]
+      val bqSchema = tableDefinition.getSchema()
+      val bqFields = bqSchema.getFields.asScala.toList
+      val head = bqFields.head
+      val tail = bqFields.tail
+      val newField = Field
+        .newBuilder(head.getName, head.getType, head.getSubFields)
+        .setPolicyTags(
+          PolicyTags.newBuilder().setNames(List(policyTagRef).asJava).build()
+        )
+        .build()
 
-    val updatedFields = newField :: tail
-    table.toBuilder
-      .setDefinition(StandardTableDefinition.of(Schema.of(updatedFields: _*)))
-      .build()
-      .update()
+      val updatedFields = newField :: tail
+      table.toBuilder
+        .setDefinition(StandardTableDefinition.of(Schema.of(updatedFields: _*)))
+        .build()
+        .update()
+    }
   }
 }
