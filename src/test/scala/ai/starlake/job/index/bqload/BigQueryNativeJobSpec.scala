3f5743186a3d13e43467c3ed88013dfbcf6341e7
Hayssam Saleh <hayssam@saleh.fr>
2021-12-07 14:53:13 +0100

    Support SQL in views folder

---
+++ src/test/scala/ai/starlake/job/index/bqload/BigQueryNativeJobSpec.scala
@@ -0,0 +1,74 @@
+package ai.starlake.job.index.bqload
+
+import ai.starlake.TestHelper
+import ai.starlake.config.Settings
+import ai.starlake.schema.handlers.{SchemaHandler, SimpleLauncher}
+import ai.starlake.schema.model._
+import ai.starlake.workflow.{IngestionWorkflow, TransformConfig}
+import com.google.cloud.bigquery.TableId
+import org.apache.hadoop.fs.Path
+import org.scalatest.BeforeAndAfterAll
+
+class BigQueryNativeJobSpec extends TestHelper with BeforeAndAfterAll {
+  override def beforeAll(): Unit = {
+    BigQueryJobBase.bigquery.delete(TableId.of("bqtest", "account"))
+  }
+  override def afterAll(): Unit = {
+    BigQueryJobBase.bigquery.delete(TableId.of("bqtest", "account"))
+  }
+
+  new WithSettings() {
+    "Ingest to BigQuery" should "should be ingest and store table in BigQuery" in {
+      if (sys.env.getOrElse("COMET_GCP_TEST", "false").toBoolean) {
+        import org.slf4j.impl.StaticLoggerBinder
+        val binder = StaticLoggerBinder.getSingleton
+        logger.debug(binder.getLoggerFactory.toString)
+        logger.debug(binder.getLoggerFactoryClassStr)
+
+        new WithSettings() {
+          new SpecTrait(
+            domainOrJobFilename = "bqtest.comet.yml",
+            sourceDomainOrJobPathname = "/sample/position/bqtest.comet.yml",
+            datasetDomainName = "bqtest",
+            sourceDatasetPathName = "/sample/position/XPOSTBL"
+          ) {
+            cleanMetadata
+            cleanDatasets
+
+            logger.info(settings.comet.datasets)
+            loadPending
+          }
+        }
+        val tableFound =
+          Option(BigQueryJobBase.bigquery.getTable(TableId.of("bqtest", "account"))).isDefined
+        tableFound should be(true)
+
+      }
+    }
+    "Native BigQuery AutoJob" should "succeed" in {
+      val businessTask1 = AutoTaskDesc(
+        None,
+        Some("select * from bqtest.account"),
+        "bqtest",
+        "jobresult",
+        WriteMode.OVERWRITE,
+        sink = Some(BigQuerySink(name = Some("sinktest"), location = Some("EU"))),
+        engine = Some(Engine.BQ)
+      )
+      val businessJob =
+        AutoJobDesc("user", List(businessTask1), None, None, None, engine = Some(Engine.BQ))
+      val schemaHandler = new SchemaHandler(metadataStorageHandler)
+
+      val businessJobDef = mapper
+        .writer()
+        .withAttribute(classOf[Settings], settings)
+        .writeValueAsString(businessJob)
+      lazy val pathBusiness = new Path(cometMetadataPath + "/jobs/bqjobtest.comet.yml")
+
+      val workflow =
+        new IngestionWorkflow(storageHandler, schemaHandler, new SimpleLauncher())
+      storageHandler.write(businessJobDef, pathBusiness)
+      workflow.autoJob(TransformConfig("bqjobtest")) should be(true)
+    }
+  }
+}
