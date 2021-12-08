31b9142454cce15a3037f99e15f1ad5ed7d75128
Hayssam Saleh <hayssam@saleh.fr>
2021-12-08 22:52:23 +0100

    remove main from class

--- src/main/scala/ai/starlake/utils/ViewExtractor.scala
+++ src/main/scala/ai/starlake/utils/ViewExtractor.scala
@@ -3,17 +3,6 @@ package ai.starlake.utils
 object ViewExtractor {
   val viewPattern = "ref\\((\\s*[0-9a-zA-Z_]*\\s*)\\)".r
 
-  def main(argds: Array[String]): Unit = {
-
-    val input =
-      """SELECT * 
-        |FROM ref( myview), ref(yourview)
-        |union
-        |select ref(herview )
-        |""".stripMargin
-    println(parse(input))
-  }
-
   def parse(sqlContent: String): (String, List[String]) = {
     var result = sqlContent
     val patterns = viewPattern
