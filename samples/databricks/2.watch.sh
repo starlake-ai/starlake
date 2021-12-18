4fca9e7e119e9cc54f03b219fffebeca22e91d00
Hayssam Saleh <hayssam@saleh.fr>
2021-12-19 00:12:43 +0100

    New Databricks SAMPLES

---
+++ samples/databricks/2.watch.sh
@@ -0,0 +1,16 @@
+source ./env.sh
+set -x
+
+awk -v bucket="$GCP_BUCKET_NAME" -v project="$GCP_PROJECT_ID" -v json="$GCP_SA_JSON_PATH" '{gsub("GCP_BUCKET_NAME", bucket);gsub("GCP_PROJECT_ID", project);gsub("GCP_SA_JSON_PATH", json)}1' core-site.xml >../bin/$SPARK_DIR_NAME/conf/core-site.xml
+
+export COMET_SPARK_SQL_SOURCES_PARTITION_OVERWRITE_MODE=dynamic
+export COMET_MERGE_OPTIMIZE_PARTITION_WRITE=true
+export COMET_ACCESS_POLICIES_APPLY=true
+export COMET_ACCESS_POLICIES_LOCATION=eu
+export COMET_ACCESS_POLICIES_PROJECT_ID=starlake-325712
+export COMET_ACCESS_POLICIES_TAXONOMY=RGPD
+
+SPARK_DRIVER_OPTIONS="-Dconfig.file=$PWD/application.conf -Dlog4j.configuration=file://$SPARK_DIR/conf/log4j.properties.template"
+#SPARK_DRIVER_OPTIONS="-Dconfig.file=$PWD/application.conf -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -Dlog4j.configuration=file://$SPARK_DIR/conf/log4j.properties.template"
+
+$SPARK_SUBMIT --driver-java-options "$SPARK_DRIVER_OPTIONS" $SPARK_CONF_OPTIONS  --class $COMET_MAIN $COMET_JAR_FULL_NAME watch
