dad9ad5bdbe706c97c67a20697c9c3de3d2c1f79
Hayssam Saleh <hayssam@saleh.fr>
2021-12-29 15:31:55 +0200

    Update samples

--- samples/local/3.transform.sh
+++ samples/local/3.transform.sh
@@ -1,4 +1,17 @@
-source ./env.sh
+if [[ -z "$STARLAKE_ENV" ]]; then
+    echo "Must provide STARLAKE_ENV in environment" 1>&2
+    exit 1
+fi
+
+if (["LOCAL", "HDFS", "GCP"].indexOf(foo) > -1)
+
+case $STARLAKE_ENV in
+    LOCAL|HDFS|GCP) echo "Running  in $STARLAKE_ENV env";;
+    *)             echo "$STARLAKE_ENV for STARLAKE_ENV unknown"; exit 1;;
+esac
+
+source ./env.${STARLAKE_ENV}.sh
+
 set -x
 
 #SPARK_DRIVER_OPTIONS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -Dlog4j.configuration=file://$SPARK_DIR/conf/log4j.properties.template"
