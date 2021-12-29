45f2852f82ffbf9c4caa6fcf74a50411ecd7f694
Hayssam Saleh <hayssam@saleh.fr>
2021-12-29 15:31:55 +0200

    Update samples

--- samples/hdfs/airflow/run.sh
+++
@@ -1,6 +0,0 @@
-curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.0.2/docker-compose.yaml'
-mkdir -p ./dags ./logs ./plugins
-echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
-docker compose down
-docker compose up airflow-init
-docker compose up
