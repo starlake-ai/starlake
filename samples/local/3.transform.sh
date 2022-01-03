f3b8818f65e0f93b07e83ca00fd13f88c00cec1e
Hayssam Saleh <hayssam@saleh.fr>
2022-01-03 11:33:25 +0100

    improve sample

--- samples/local/3.transform.sh
+++ samples/local/3.transform.sh
@@ -3,14 +3,13 @@ if [[ -z "$STARLAKE_ENV" ]]; then
     exit 1
 fi
 
-if (["LOCAL", "HDFS", "GCP"].indexOf(foo) > -1)
-
 case $STARLAKE_ENV in
     LOCAL|HDFS|GCP) echo "Running  in $STARLAKE_ENV env";;
     *)             echo "$STARLAKE_ENV for STARLAKE_ENV unknown"; exit 1;;
 esac
 
-source ./env.${STARLAKE_ENV}.sh
+# shellcheck disable=SC1090
+source ./env."${STARLAKE_ENV}".sh
 
 set -x
 
