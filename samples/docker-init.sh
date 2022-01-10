7703679cbc7c626911918bed8f1c9487e51e22c4
Hayssam Saleh <hayssam@saleh.fr>
2022-01-10 05:31:05 +0100

    improve sample

--- samples/docker-init.sh
+++ samples/docker-init.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Setting up samples ..."
-rm -rf /samples/bin
-mkdir -p /samples/bin
-cp /app/starlake-spark3_2.12-0.2.8-SNAPSHOT-assembly.jar /samples/bin/
-cp -r /app/spark-3.1.2-bin-hadoop3.2 /samples/bin/
 HOSTNAM=`cat /etc/hostname`
-echo "setup finished"
 echo "to login run: docker exec -it $HOSTNAM bash"
 
 mainmenu () {
