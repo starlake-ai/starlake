32ce8f793527c20820f17a0b922429ebf1f1d393
Hayssam Saleh <hayssam@saleh.fr>
2022-01-30 11:33:03 +0100

    Spark Fair Scheduler support

--- src/main/scala/ai/starlake/config/SparkEnv.scala
+++ src/main/scala/ai/starlake/config/SparkEnv.scala
@@ -20,14 +20,9 @@
 
 package ai.starlake.config
 
-import better.files.File
 import com.typesafe.scalalogging.StrictLogging
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
-
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import scala.collection.JavaConverters._
 
 /** Any Spark Job will inherit from this class. All properties defined in application conf file and
   * prefixed by the "spark" key will be loaded into the Spark Job
@@ -40,59 +35,9 @@ class SparkEnv(name: String, confTransformer: SparkConf => SparkConf = identity)
   settings: Settings
 ) extends StrictLogging {
 
-  private def initSchedulingConfig(): File = {
-    import settings.comet.scheduling._
-
-    val schedulingConfig =
-      if (allocations.isEmpty)
-        s"""<?xml version="1.0"?>
-                     |<allocations>
-                     |  <pool name="starlake">
-                     |    <schedulingMode>$mode</schedulingMode>
-                     |    <weight>$weight}</weight>
-                     |    <minShare>$minShare</minShare>
-                     |  </pool>
-                     |</allocations>""".stripMargin
-      else
-        allocations
-
-    val tempFile = File.temp / "starlake-spark-scheduling.xml"
-    tempFile.overwrite(schedulingConfig)
-  }
-
   /** Load spark.* properties from the loaded application conf file
     */
-  val config: SparkConf = {
-    val schedulingConfig = initSchedulingConfig()
-    val now = LocalDateTime
-      .now()
-      .format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss.SSS"))
-    val appName = s"$name-$now"
-
-    // When using local Spark with remote BigQuery (useful for testing)
-    val initialConf =
-      sys.env.get("TEMPORARY_GCS_BUCKET") match {
-        case Some(value) => new SparkConf().set("temporaryGcsBucket", value)
-        case None        => new SparkConf()
-      }
-
-    val thisConf = settings.sparkConfig
-      .entrySet()
-      .asScala
-      .to[Vector]
-      .map(x => (x.getKey, x.getValue.unwrapped().toString))
-      .foldLeft(initialConf) { case (conf, (key, value)) => conf.set("spark." + key, value) }
-      .setAppName(appName)
-      .set("spark.app.id", appName)
-      .set("spark.scheduler.allocation.file", schedulingConfig.uri.toString)
-
-    val withExtraConf = confTransformer(thisConf)
-
-    logger.whenDebugEnabled {
-      logger.debug(withExtraConf.toDebugString)
-    }
-    withExtraConf
-  }
+  val config: SparkConf = confTransformer(settings.jobConf)
 
   /** Creates a Spark Session with the spark.* keys defined the application conf file.
     */
@@ -107,5 +52,4 @@ class SparkEnv(name: String, confTransformer: SparkConf => SparkConf = identity)
     logger.info(session.conf.getAll.mkString("\n"))
     session
   }
-
 }
