b7c725616bab4ff221603cf9400938108a8bace8
Hayssam Saleh <hayssam@saleh.fr>
2022-04-27 11:31:20 +0200

    Code improvement

--- src/test/scala/ai/starlake/TestHelper.scala
+++ src/test/scala/ai/starlake/TestHelper.scala
@@ -20,15 +20,13 @@
 
 package ai.starlake
 
-import ai.starlake.schema.handlers.StorageHandler
-import ai.starlake.schema.model.AutoJobDesc
-import com.dimafeng.testcontainers.{ElasticsearchContainer, KafkaContainer}
 import ai.starlake.config.{DatasetArea, Settings}
 import ai.starlake.job.ingest.LoadConfig
 import ai.starlake.schema.handlers.{SchemaHandler, SimpleLauncher, StorageHandler}
 import ai.starlake.schema.model.AutoJobDesc
 import ai.starlake.utils.{CometObjectMapper, Utils}
 import ai.starlake.workflow.{ImportConfig, IngestionWorkflow, WatchConfig}
+import com.dimafeng.testcontainers.{ElasticsearchContainer, KafkaContainer}
 import com.fasterxml.jackson.annotation.JsonInclude.Include
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
@@ -45,12 +43,12 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.{Assertion, BeforeAndAfterAll}
 import org.testcontainers.utility.DockerImageName
+
 import java.io.{File, InputStream}
 import java.nio.file.Files
 import java.time.LocalDate
 import java.util.UUID
 import java.util.concurrent.atomic.AtomicBoolean
-
 import scala.collection.JavaConverters._
 import scala.io.{Codec, Source}
 import scala.util.Try
