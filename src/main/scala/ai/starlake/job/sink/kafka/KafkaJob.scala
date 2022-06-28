d6a9d9c675aa855d3bc11769bcdf58bb9e141dbe
Hayssam Saleh <hayssam@saleh.fr>
2022-06-28 16:32:52 +0200

    Support stream source provider

--- src/main/scala/ai/starlake/job/sink/kafka/KafkaJob.scala
+++ src/main/scala/ai/starlake/job/sink/kafka/KafkaJob.scala
@@ -2,6 +2,7 @@ package ai.starlake.job.sink.kafka
 
 import ai.starlake.config.{DatasetArea, Settings}
 import ai.starlake.schema.handlers.SchemaHandler
+import ai.starlake.utils.Formatter._
 import ai.starlake.utils.kafka.KafkaClient
 import ai.starlake.utils.{JobResult, SparkJob, SparkJobResult, Utils}
 import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient
@@ -13,7 +14,6 @@ import org.apache.spark.sql.streaming.Trigger
 
 import scala.collection.JavaConverters._
 import scala.util.Try
-import ai.starlake.utils.Formatter._
 
 object CustomDeserializer {
   var userDefinedDeserializer: Deserializer[Any] = _
@@ -49,14 +49,17 @@ class KafkaJob(
     Map("config" -> kafkaJobConfig.topicConfigName, "topic" -> topicConfig.topicName)
   )
 
-  val schemaRegistryUrl = settings.comet.kafka.serverOptions.get("schema.registry.url")
-  val schemaRegistryClient = schemaRegistryUrl.map(schemaRegistryUrl =>
-    new CachedSchemaRegistryClient(
-      schemaRegistryUrl,
-      128,
-      settings.comet.kafka.serverOptions.asJava
+  val schemaRegistryUrl: Option[JdbcConfigName] =
+    settings.comet.kafka.serverOptions.get("schema.registry.url")
+
+  val schemaRegistryClient: Option[CachedSchemaRegistryClient] =
+    schemaRegistryUrl.map(schemaRegistryUrl =>
+      new CachedSchemaRegistryClient(
+        schemaRegistryUrl,
+        128,
+        settings.comet.kafka.serverOptions.asJava
+      )
     )
-  )
 
   def lookupTopicSchema(topic: String, isKey: Boolean = false): Option[JdbcConfigName] = {
     schemaRegistryClient.map(
@@ -69,9 +72,33 @@ class KafkaJob(
     SchemaConverters.toSqlType(parser.parse(avroSchema))
   }
 
-  val dfValueSchema: Option[SchemaConverters.SchemaType] = {
+  private val dfValueSchema: Option[SchemaConverters.SchemaType] = {
     val rawSchema = lookupTopicSchema(topicConfig.topicName)
     rawSchema.map(rawSchema => avroSchemaToSparkSchema(rawSchema))
+  }
+
+  private val writeOptions = kafkaJobConfig.writeOptions.get("config") match {
+    case Some(configValue) =>
+      loadOptionsFromConfig(configValue)
+    case None =>
+      kafkaJobConfig.writeOptions
+  }
+
+  private val options = kafkaJobConfig.options.get("config") match {
+    case Some(configValue) =>
+      loadOptionsFromConfig(configValue)
+    case None =>
+      kafkaJobConfig.options
+  }
+
+  private def loadOptionsFromConfig(configValue: String): Map[String, String] = {
+    settings.extraConf
+      .getConfig(configValue)
+      .entrySet()
+      .asScala
+      .to[Vector]
+      .map(x => (x.getKey, x.getValue.unwrapped().toString))
+      .toMap
   }
 
   def offload(): Try[SparkJobResult] = {
@@ -85,39 +112,7 @@ class KafkaJob(
               topicConfig
             )
 
-          val transformedDF = transfom(df)
-          val finalDF =
-            kafkaJobConfig.coalesce match {
-              case None    => transformedDF
-              case Some(x) => transformedDF.repartition(x)
-            }
-
-          logger.info(s"Saving to $kafkaJobConfig")
-          finalDF.write
-            .mode(kafkaJobConfig.mode)
-            .format(kafkaJobConfig.format)
-            .options(kafkaJobConfig.writeOptions)
-            .save(finalPath)
-          logger.info(s"Kafka saved messages to offload -> ${finalPath}")
-
-          kafkaJobConfig.coalesce match {
-            case Some(1) =>
-              val extension = kafkaJobConfig.format
-              val targetPath = new Path(finalPath)
-              val singleFile = settings.storageHandler
-                .list(
-                  targetPath,
-                  recursive = false
-                )
-                .filter(_.getName.startsWith("part-"))
-                .head
-              val tmpPath = new Path(targetPath.toString + ".tmp")
-              if (settings.storageHandler.move(singleFile, tmpPath)) {
-                settings.storageHandler.delete(targetPath)
-                settings.storageHandler.move(tmpPath, targetPath)
-              }
-            case _ =>
-          }
+          val transformedDF: DataFrame = batchSave(df)
 
           kafkaClient.topicSaveOffsets(
             kafkaJobConfig.topicConfigName,
@@ -133,41 +128,7 @@ class KafkaJob(
               session,
               topicConfig
             )
-          val transformedDF = transfom(df)
-
-          val writer = transformedDF.writeStream
-            .outputMode(kafkaJobConfig.streamingWriteMode)
-            .format(kafkaJobConfig.streamingWriteFormat)
-            .options(kafkaJobConfig.writeOptions)
-
-          val trigger = kafkaJobConfig.streamingTrigger.map(_.toLowerCase).map {
-            case "once"           => Trigger.Once()
-            case "processingtime" => Trigger.ProcessingTime(kafkaJobConfig.streamingTriggerOption)
-            case "continuous"     => Trigger.Continuous(kafkaJobConfig.streamingTriggerOption)
-          }
-
-          val triggerWriter = trigger match {
-            case Some(trigger) => writer.trigger(trigger)
-            case None          => writer
-          }
-
-          val partitionedWriter = kafkaJobConfig.streamingWritePartitionBy match {
-            case Nil =>
-              triggerWriter
-            case list =>
-              triggerWriter.partitionBy(list: _*)
-          }
-          val streamingQuery =
-            if (
-              kafkaJobConfig.streamingWriteToTable
-            ) // partitionedWriter.toTable(kafkaJobConfig.path)
-              throw new Exception("streamingWriteToTable Not Supported")
-            else
-              partitionedWriter
-                .start(finalPath)
-
-          streamingQuery
-            .awaitTermination()
+          streamToKafka(df)
           SparkJobResult(None)
         }
       }
@@ -177,16 +138,101 @@ class KafkaJob(
   def load(): Try[SparkJobResult] = {
     Try {
       Utils.withResources(new KafkaClient(settings.comet.kafka)) { kafkaClient =>
-        val df = session.read.format(kafkaJobConfig.format).load(finalPath.split(','): _*)
-        val transformedDF = transfom(df)
+        // DataSource.lookupDataSource(kafkaJobConfig.format, session.sessionState.conf)
 
-        kafkaClient.sinkToTopic(
-          topicConfig,
-          transformedDF
-        )
-        SparkJobResult(Some(transformedDF))
+        kafkaJobConfig.streaming match {
+          case true =>
+            val df = session.readStream
+              .format(kafkaJobConfig.format)
+              .options(options)
+              .load()
+              .selectExpr(topicConfig.fields: _*)
+            streamToKafka(df)
+            SparkJobResult(None)
+          case false =>
+            val df = session.read.format(kafkaJobConfig.format).load(finalPath.split(','): _*)
+            val transformedDF = transfom(df)
+
+            kafkaClient.sinkToTopic(
+              topicConfig,
+              transformedDF
+            )
+            SparkJobResult(Some(transformedDF))
+        }
       }
     }
+  }
+
+  private def batchSave(df: DataFrame) = {
+    val transformedDF = transfom(df)
+    val finalDF =
+      kafkaJobConfig.coalesce match {
+        case None    => transformedDF
+        case Some(x) => transformedDF.repartition(x)
+      }
+
+    logger.info(s"Saving to $kafkaJobConfig")
+    finalDF.write
+      .mode(kafkaJobConfig.mode)
+      .format(kafkaJobConfig.format)
+      .options(writeOptions)
+      .save(finalPath)
+    logger.info(s"Kafka saved messages to offload -> ${finalPath}")
+
+    kafkaJobConfig.coalesce match {
+      case Some(1) =>
+        val targetPath = new Path(finalPath)
+        val singleFile = settings.storageHandler
+          .list(
+            targetPath,
+            recursive = false
+          )
+          .filter(_.getName.startsWith("part-"))
+          .head
+        val tmpPath = new Path(targetPath.toString + ".tmp")
+        if (settings.storageHandler.move(singleFile, tmpPath)) {
+          settings.storageHandler.delete(targetPath)
+          settings.storageHandler.move(tmpPath, targetPath)
+        }
+      case _ =>
+    }
+    transformedDF
+  }
+
+  private def streamToKafka(df: DataFrame) = {
+    val transformedDF = transfom(df)
+
+    val writer = transformedDF.writeStream
+      .outputMode(kafkaJobConfig.streamingWriteMode)
+      .format(kafkaJobConfig.streamingWriteFormat)
+      .options(writeOptions)
+
+    val trigger = kafkaJobConfig.streamingTrigger.map(_.toLowerCase).map {
+      case "once"           => Trigger.Once()
+      case "processingtime" => Trigger.ProcessingTime(kafkaJobConfig.streamingTriggerOption)
+      case "continuous"     => Trigger.Continuous(kafkaJobConfig.streamingTriggerOption)
+    }
+
+    val triggerWriter = trigger match {
+      case Some(trigger) => writer.trigger(trigger)
+      case None          => writer
+    }
+
+    val partitionedWriter = kafkaJobConfig.streamingWritePartitionBy match {
+      case Nil =>
+        triggerWriter
+      case list =>
+        triggerWriter.partitionBy(list: _*)
+    }
+    val streamingQuery =
+      if (kafkaJobConfig.streamingWriteToTable) // partitionedWriter.toTable(kafkaJobConfig.path)
+        throw new Exception("streamingWriteToTable Not Supported")
+      else
+        partitionedWriter
+          .start(finalPath)
+
+    streamingQuery
+      .awaitTermination()
   }
 
   private val transformInstance: Option[DataFrameTransform] = {
