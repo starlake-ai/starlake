e01558dde886524ce494e4a8d1446abd9c8a13d0
Hayssam Saleh <hayssam@saleh.fr>
2022-07-07 18:16:43 +0200

    reduce assembly size

--- src/main/scala/ai/starlake/schema/generator/Xls2YmlConfig.scala
+++ src/main/scala/ai/starlake/schema/generator/Xls2YmlConfig.scala
@@ -33,7 +33,7 @@ import scopt.OParser
   */
 case class Xls2YmlConfig(
   files: Seq[String] = Nil,
-  encryption: Boolean = true,
+  encryption: Boolean = false,
   delimiter: Option[String] = None,
   privacy: Seq[String] = Nil,
   outputPath: Option[String] = None
@@ -54,7 +54,7 @@ object Xls2YmlConfig extends CliConfig[Xls2YmlConfig] {
         .text("List of Excel files describing Domains & Schemas"),
       opt[Boolean]("encryption")
         .action((x, c) => c.copy(encryption = x))
-        .required()
+        .optional()
         .text("If true generate pre and post encryption YML"),
       opt[String]("delimiter")
         .action((x, c) => c.copy(delimiter = Some(x)))
