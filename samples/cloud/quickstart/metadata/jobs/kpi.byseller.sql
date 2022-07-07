7ca6d0e5fb2a9e2769dcfb97f11cba2cfee4450c
Hayssam Saleh <hayssam@saleh.fr>
2022-07-07 22:09:22 +0200

    fix test

--- samples/cloud/quickstart/metadata/jobs/kpi.byseller.sql
+++ samples/cloud/quickstart/metadata/jobs/kpi.byseller.sql
@@ -1,6 +1,6 @@
 with mycte as (
     select seller_email, amount
-    from sellers hrs, orders sos where hrs.id = sos.seller_id
+    from hr.sellers hrs, sales.orders sos where hrs.id = sos.seller_id
 )
 select seller_email, sum(amount) as sum from mycte
 group by mycte.seller_email
