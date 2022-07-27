6791cd67a38de50074726af56eada122c9e94e69
Hayssam Saleh <hayssam@saleh.fr>
2022-07-27 09:36:54 +0300

    Update to 2.12.16

--- sbt-gcp.sh
+++ sbt-gcp.sh
@@ -2,4 +2,4 @@
 ## sbt-gcp.sh publish
 export GOOGLE_APPLICATION_CREDENTIALS="/Users/me/.gcloud/keys/starlake-$USER.json"
 export GCS_BUCKET_ARTEFACTS="starlake-dev-artefacts"
-COMET_SPARK_VERSION=3.0.0 sbt ++2.12.15 $*
+COMET_SPARK_VERSION=3.0.0 sbt ++2.12.16 $*
