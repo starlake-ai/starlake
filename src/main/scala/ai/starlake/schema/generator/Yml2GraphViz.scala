335025ea81db11b0ea6cb99ca259885fbe3a34da
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2022-08-12 08:51:00 +0200

    Improve GraphViz generation

--- src/main/scala/ai/starlake/schema/generator/Yml2GraphViz.scala
+++ src/main/scala/ai/starlake/schema/generator/Yml2GraphViz.scala
@@ -21,11 +21,17 @@ class Yml2GraphViz(schemaHandler: SchemaHandler) extends LazyLogging {
                      |}
                      |""".stripMargin
 
+  private def relatedTables(): List[String] = {
+    schemaHandler.domains.flatMap(_.relatedTables())
+  }
+
   def run(args: Array[String]): Unit = {
     implicit val settings: Settings = Settings(ConfigFactory.load())
     Yml2GraphVizConfig.parse(args) match {
       case Some(config) =>
-        val dots = schemaHandler.domains.map(_.asDot(config.includeAllAttributes.getOrElse(true)))
+        val fkTables = relatedTables().map(_.toLowerCase).toSet
+        val dots =
+          schemaHandler.domains.map(_.asDot(config.includeAllAttributes.getOrElse(true), fkTables))
         val result = prefix + dots.mkString("\n") + suffix
         config.output match {
           case None => println(result)
