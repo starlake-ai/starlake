0f2c6e6a7086f1133da3edbdcc16585c7d3c31b2
Scala Steward <scala_steward@virtuslab.com>
2022-12-18 17:59:39 +0000

    Revert commit(s) 351374eb

--- project/Versions.scala
+++ project/Versions.scala
@@ -18,7 +18,7 @@ object Versions {
   val esSpark212 = "8.5.3"
   val scopt = "4.1.0"
   val gcsConnector = "hadoop3-2.2.9"
-  val bigquery = "2.20.0"
+  val bigquery = "2.19.1"
   val hadoop = "3.3.4"
   val sparkBigqueryWithDependencies = "0.27.1"
   val bigqueryConnector = "hadoop3-1.2.0"
