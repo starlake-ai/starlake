15249613dec931fea03b8558f9269365fc859006
Hayssam Saleh <Hayssam.saleh@starlake.ai>
2023-01-07 15:43:04 +0100

    Fix GCP Project in tests

--- src/main/scala/ai/starlake/config/SettingsManager.scala
+++ src/main/scala/ai/starlake/config/SettingsManager.scala
@@ -55,7 +55,7 @@ object SettingsManager {
     outFile.createDirectoryIfNotExists()
 
     gcpProject.foreach { gcpProject =>
-      sysProps.setProperty("GOOGLE_CLOUD_PROJECT", gcpProject)
+      sysProps.setProperty("GCP_PROJECT", gcpProject)
     }
 
     val currentSettings = settingsMap.getOrElse(
