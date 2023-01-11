6ceeb34f36c57dd8f67271847aea8e0879b0d2af
Scala Steward <scala_steward@virtuslab.com>
2023-01-11 18:57:37 +0000

    Update elasticsearch-spark-30 to 8.6.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object Versions {
   val betterFiles = "3.9.1"
   val jackson212ForSpark3 = "2.13.3"
   val pureConfig212ForSpark3 = "0.14.0"
-  val esSpark212 = "8.5.3"
+  val esSpark212 = "8.6.0"
   val scopt = "4.1.0"
   val gcsConnector = "hadoop3-2.2.10"
   val bigquery = "2.20.1"
