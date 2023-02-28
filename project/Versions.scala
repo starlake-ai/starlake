287400f33f54a5de278146d13d0c64e3c90e2ada
Scala Steward <scala_steward@virtuslab.com>
2023-02-28 20:00:11 +0000

    Update spark-bigquery-connector-common, ... to 0.28.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -19,8 +19,8 @@ object Versions {
   val gcsConnector = "hadoop3-2.2.11"
   val bigquery = "2.23.0"
   val hadoop = "3.3.4"
-  val sparkBigqueryWithDependencies = "0.28.0"
-  val sparkBigqueryConnectorCommon = "0.28.0"
+  val sparkBigqueryWithDependencies = "0.28.1"
+  val sparkBigqueryConnectorCommon = "0.28.1"
   val bigqueryConnector = "hadoop3-1.2.0"
   val h2 = "2.1.214" // Test only
   val poi = "4.1.2"
