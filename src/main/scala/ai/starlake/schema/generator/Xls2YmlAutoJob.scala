9fd9dea9a8b0323f4d11dcb9023dad54172ae90a
Mohamad Kassir <mbkassir@gmail.com>
2023-03-23 13:32:25 +0100

    simplify generation  of Job yaml

--- src/main/scala/ai/starlake/schema/generator/Xls2YmlAutoJob.scala
+++ src/main/scala/ai/starlake/schema/generator/Xls2YmlAutoJob.scala
@@ -1,13 +1,11 @@
 package ai.starlake.schema.generator
 
-import better.files.File
 import ai.starlake.config.{DatasetArea, Settings}
 import ai.starlake.schema.model._
-import ai.starlake.utils.CliConfig
+import ai.starlake.utils.YamlSerializer._
+import better.files.File
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
-import ai.starlake.utils.YamlSerializer._
-import scopt.OParser
 
 object Xls2YmlAutoJob extends LazyLogging {
 
@@ -40,12 +38,12 @@ object Xls2YmlAutoJob extends LazyLogging {
 
   def run(args: Array[String]): Boolean = {
     implicit val settings: Settings = Settings(ConfigFactory.load())
-    Xls2YmlAutoJobConfig.parse(args) match {
+    Xls2YmlConfig.parse(args) match {
       case Some(config) =>
         config.files.foreach(generateSchema(_, config.policyFile, config.outputPath))
         true
       case _ =>
-        println(Xls2YmlAutoJobConfig.usage())
+        println(Xls2YmlConfig.usage())
         false
     }
   }
@@ -54,49 +52,4 @@ object Xls2YmlAutoJob extends LazyLogging {
     val result = Xls2YmlAutoJob.run(args)
     System.exit(if (result) 0 else 1)
   }
-}
-
-case class Xls2YmlAutoJobConfig(
-  files: Seq[String] = Nil,
-  policyFile: Option[String] = None,
-  outputPath: Option[String] = None
-)
-
-object Xls2YmlAutoJobConfig extends CliConfig[Xls2YmlAutoJobConfig] {
-  val command = "xls2ymljob"
-
-  val parser: OParser[Unit, Xls2YmlAutoJobConfig] = {
-    val builder = OParser.builder[Xls2YmlAutoJobConfig]
-    import builder._
-    OParser.sequence(
-      programName("starlake xls2ymljob"),
-      head("starlake", "xls2ymljob", "[options]"),
-      note(""),
-      opt[Seq[String]]("files")
-        .action((x, c) => c.copy(files = x))
-        .required()
-        .text("List of Excel files describing Domains & Schemas"),
-      opt[Option[String]]("policyFile")
-        .action((x, c) => c.copy(policyFile = x))
-        .optional()
-        .text(
-          """File contains policies""".stripMargin
-        ),
-      opt[Option[String]]("outputPath")
-        .action((x, c) => c.copy(outputPath = x))
-        .optional()
-        .text(
-          """Path for saving the resulting YAML file(s). Comet domains path is used by default.""".stripMargin
-        )
-    )
-  }
-
-  /** @param args
-    *   args list passed from command line
-    * @return
-    *   Option of case class SchemaGenConfig.
-    */
-  def parse(args: Seq[String]): Option[Xls2YmlAutoJobConfig] =
-    OParser.parse(parser, args, Xls2YmlAutoJobConfig())
-
 }
