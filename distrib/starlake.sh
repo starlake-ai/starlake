ae3d96cfda0b07dc9f8aabe3742c8c0a8115fa55
Hayssam Saleh <Hayssam.saleh@starlake.ai>
2023-04-15 22:16:33 +0200

    Run starlake as a docker image on any cloud

--- distrib/starlake.sh
+++ distrib/starlake.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )"
-COMET_ROOT="${COMET_ROOT:-`pwd`}"
+SL_ROOT="${SL_ROOT:-`pwd`}"
+SCALA_VERSION=2.12
 SPARK_VERSION="${SPARK_VERSION:-3.3.2}"
 HADOOP_VERSION="${HADOOP_VERSION:-3}"
-SPARK_BQ_VERSION="${SPARK_BQ_VERSION:-0.29.0}"
-SCALA_VERSION=2.12
+SPARK_BQ_VERSION="${SPARK_BQ_VERSION:-0.30.0}"
 STARLAKE_ARTIFACT_NAME=starlake-spark3_$SCALA_VERSION
 SPARK_DIR_NAME=spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION
 SPARK_TARGET_FOLDER=$SCRIPT_DIR/bin/spark
 SPARK_BQ_ARTIFACT_NAME=spark-bigquery-with-dependencies_$SCALA_VERSION
 SPARK_BQ_JAR_NAME=$SPARK_BQ_ARTIFACT_NAME-$SPARK_BQ_VERSION.jar
 
-SKIP_INSTALL=0
-export COMET_ENV="${COMET_ENV:-FS}"
+SKIP_INSTALL=1
+export SL_ENV="${SL_ENV:-FS}"
 export SPARK_DRIVER_MEMORY="${SPARK_DRIVER_MEMORY:-4G}"
-export COMET_FS="${COMET_FS:-file://}"
-export COMET_MAIN=ai.starlake.job.Main
-export COMET_VALIDATE_ON_LOAD=false
+export SL_FS="${SL_FS:-file://}"
+export SL_MAIN=ai.starlake.job.Main
+export SL_VALIDATE_ON_LOAD=false
 
 SPARK_TGZ_NAME=$SPARK_DIR_NAME.tgz
 SPARK_TGZ_URL=https://archive.apache.org/dist/spark/spark-$SPARK_VERSION/$SPARK_TGZ_NAME
 SPARK_BQ_URL=https://repo1.maven.org/maven2/com/google/cloud/spark/$SPARK_BQ_ARTIFACT_NAME/$SPARK_BQ_VERSION/$SPARK_BQ_ARTIFACT_NAME-$SPARK_BQ_VERSION.jar
 
 initStarlakeInstallVariables() {
-  if [[ -z "$COMET_VERSION" ]]; then
-      COMET_VERSION=$(curl -s --fail "https://search.maven.org/solrsearch/select?q=g:ai.starlake+AND+a:$STARLAKE_ARTIFACT_NAME&core=gav&start=0&rows=42&wt=json" | jq -r '.response.docs[].v' | sed '#-#!{s/$/_/}' | sort -Vr | sed 's/_$//' | head -n 1)
+  if [[ -z "$SL_VERSION" ]]; then
+      SL_VERSION=$(curl -s --fail "https://search.maven.org/solrsearch/select?q=g:ai.starlake+AND+a:$STARLAKE_ARTIFACT_NAME&core=gav&start=0&rows=42&wt=json" | jq -r '.response.docs[].v' | sed '#-#!{s/$/_/}' | sort -Vr | sed 's/_$//' | head -n 1)
   fi
-  STARLAKE_JAR_NAME=$STARLAKE_ARTIFACT_NAME-$COMET_VERSION-assembly.jar
-  if [[ "$COMET_VERSION" == *"SNAPSHOT"* ]]
+  STARLAKE_JAR_NAME=$STARLAKE_ARTIFACT_NAME-$SL_VERSION-assembly.jar
+  if [[ "$SL_VERSION" == *"SNAPSHOT"* ]]
   then
-    STARLAKE_JAR_URL=https://s01.oss.sonatype.org/content/repositories/snapshots/ai/starlake/starlake-spark3_$SCALA_VERSION/$COMET_VERSION/$STARLAKE_JAR_NAME
+    STARLAKE_JAR_URL=https://s01.oss.sonatype.org/content/repositories/snapshots/ai/starlake/starlake-spark3_$SCALA_VERSION/$SL_VERSION/$STARLAKE_JAR_NAME
   else
-    STARLAKE_JAR_URL=https://s01.oss.sonatype.org/content/repositories/releases/ai/starlake/starlake-spark3_$SCALA_VERSION/$COMET_VERSION/$STARLAKE_JAR_NAME
+    STARLAKE_JAR_URL=https://s01.oss.sonatype.org/content/repositories/releases/ai/starlake/starlake-spark3_$SCALA_VERSION/$SL_VERSION/$STARLAKE_JAR_NAME
   fi
 }
 
@@ -104,7 +104,7 @@ initEnv() {
   then
     echo "- starlake: downloading from $STARLAKE_JAR_URL"
     curl --fail --output "$SPARK_TARGET_FOLDER/jars/$STARLAKE_JAR_NAME" "$STARLAKE_JAR_URL"
-    echo "Starlake Version: $COMET_VERSION" >> "$SCRIPT_DIR/version.info"
+    echo "Starlake Version: $SL_VERSION" >> "$SCRIPT_DIR/version.info"
     echo "- starlake: OK"
   else
     echo "- starlake: skipped"
@@ -122,6 +122,7 @@ initEnv() {
 }
 
 checkCurrentState
+
 if [[ $SKIP_INSTALL -eq 1 ]]
 then
   initEnv
@@ -130,46 +131,59 @@ fi
 echo
 echo Launching starlake.
 echo "- JAVA_HOME=$JAVA_HOME"
-echo "- COMET_ROOT=$COMET_ROOT"
-echo "- COMET_ENV=$COMET_ENV"
-echo "- COMET_FS=$COMET_FS"
-echo "- COMET_MAIN=$COMET_MAIN"
-echo "- COMET_VALIDATE_ON_LOAD=$COMET_VALIDATE_ON_LOAD"
+echo "- SL_ROOT=$SL_ROOT"
+echo "- SL_ENV=$SL_ENV"
+echo "- SL_FS=$SL_FS"
+echo "- SL_MAIN=$SL_MAIN"
+echo "- SL_VALIDATE_ON_LOAD=$SL_VALIDATE_ON_LOAD"
 echo "- SPARK_DRIVER_MEMORY=$SPARK_DRIVER_MEMORY"
 echo Make sure your java home path does not contain space
 
 #SPARK_DRIVER_OPTIONS="-Dlog4j.configuration=file://$SCRIPT_DIR/bin/spark/conf/log4j2.properties"
 #export SPARK_DRIVER_OPTIONS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -Dlog4j.configuration=file://$SPARK_DIR/conf/log4j.properties"
 
-  if [[ -z "$COMET_NATIVE" ]]
-  then
-    SPARK_SUBMIT="$SPARK_TARGET_FOLDER/bin/spark-submit"
-    COMET_ROOT=$COMET_ROOT $SPARK_SUBMIT --driver-java-options "$SPARK_DRIVER_OPTIONS" $SPARK_CONF_OPTIONS --class $COMET_MAIN $SPARK_TARGET_FOLDER/jars/$STARLAKE_JAR_NAME $@
-  else
-    case $COMET_AUDIT_SINK_TYPE in
-        BigQuerySink|NoneSink)
-          COMET_ROOT=$COMET_ROOT java \
-                              --add-opens=java.base/java.lang=ALL-UNNAMED \
-                              --add-opens=java.base/java.lang.invoke=ALL-UNNAMED \
-                              --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
-                              --add-opens=java.base/java.io=ALL-UNNAMED \
-                              --add-opens=java.base/java.net=ALL-UNNAMED \
-                              --add-opens=java.base/java.nio=ALL-UNNAMED \
-                              --add-opens=java.base/java.util=ALL-UNNAMED \
-                              --add-opens=java.base/java.util.concurrent=ALL-UNNAMED \
-                              --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED \
-                              --add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
-                              --add-opens=java.base/sun.nio.cs=ALL-UNNAMED \
-                              --add-opens=java.base/sun.security.action=ALL-UNNAMED \
-                              --add-opens=java.base/sun.util.calendar=ALL-UNNAMED \
-                              --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED \
-                              -Dlog4j.configuration=file://$SPARK_TARGET_FOLDER/conf/log4j2.properties \
-                              -cp "$SPARK_TARGET_FOLDER/jars/*" $COMET_MAIN $@
-        ;;
-        *)
-          echo "COMET_AUDIT_SINK_TYPE env var must be set ot BigQuerySink or NoneSink"
-        ;;
-    esac
-  fi
+if [[ -z "$SL_BQ_NATIVE" ]]
+then
+  SPARK_SUBMIT="$SPARK_TARGET_FOLDER/bin/spark-submit"
+  SL_ROOT=$SL_ROOT $SPARK_SUBMIT --driver-java-options "$SPARK_DRIVER_OPTIONS" $SPARK_CONF_OPTIONS --class $SL_MAIN $SPARK_TARGET_FOLDER/jars/$STARLAKE_JAR_NAME $@
+else
+  case $SL_AUDIT_SINK_TYPE in
+      BigQuerySink|NoneSink)
+      if [[ -z "$SL_GCP_SA_JSON_FILE" ]]
+      then
+        echo "SL_GCP_SA_JSON_FILE should reference the service account json file"
+      else
+      export SL_STORAGE_CONF="fs.AbstractFileSystem.gs.impl=com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS,
+                      google.cloud.auth.type=SERVICE_ACCOUNT_JSON_KEYFILE,
+                      google.cloud.auth.service.account.json.keyfile=$SL_GCP_SA_JSON_FILE,
+                      fs.gs.impl=com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem,
+                      google.cloud.auth.service.account.enable=true,
+                      fs.default.name=$SL_FS,
+                      fs.defaultFS=$SL_FS"
+      fi
+
+        SL_ROOT=$SL_ROOT java \
+                            --add-opens=java.base/java.lang=ALL-UNNAMED \
+                            --add-opens=java.base/java.lang.invoke=ALL-UNNAMED \
+                            --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
+                            --add-opens=java.base/java.io=ALL-UNNAMED \
+                            --add-opens=java.base/java.net=ALL-UNNAMED \
+                            --add-opens=java.base/java.nio=ALL-UNNAMED \
+                            --add-opens=java.base/java.util=ALL-UNNAMED \
+                            --add-opens=java.base/java.util.concurrent=ALL-UNNAMED \
+                            --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED \
+                            --add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
+                            --add-opens=java.base/sun.nio.cs=ALL-UNNAMED \
+                            --add-opens=java.base/sun.security.action=ALL-UNNAMED \
+                            --add-opens=java.base/sun.util.calendar=ALL-UNNAMED \
+                            --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED \
+                            -Dlog4j.configuration=file://$SPARK_TARGET_FOLDER/conf/log4j2.properties \
+                            -cp "$SPARK_TARGET_FOLDER/jars/*" $SL_MAIN $@
+      ;;
+      *)
+        echo "SL_AUDIT_SINK_TYPE env var must be set ot BigQuerySink or NoneSink"
+      ;;
+  esac
+fi
 
 
