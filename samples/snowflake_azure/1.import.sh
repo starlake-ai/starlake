34c5c8ea04de88882a293216257c93d52e145f41
Hayssam Saleh <Hayssam.saleh@starlake.ai>
2023-04-26 00:41:41 +0200

    Improve sample Upgrade Azure support

--- samples/snowflake_azure/1.import.sh
+++ samples/snowflake_azure/1.import.sh
@@ -1,16 +1,3 @@
-if [[ -z "$SL_ENV" ]]; then
-    echo "SL_ENV not provided using default value LOCAL" 1>&2
-fi
-
-export SL_ENV="${SL_ENV:-LOCAL}"
-
-
-case $SL_ENV in
-    LOCAL|SNOWFLAKE) echo "Running  in $SL_ENV env";;
-    *)             echo "$SL_ENV for SL_ENV unknown"; exit 1;;
-esac
-
-# shellcheck disable=SC1090
-source ./env."${SL_ENV}".sh
+source ./env.sh
 
 $SL_BIN_DIR/starlake.sh import
