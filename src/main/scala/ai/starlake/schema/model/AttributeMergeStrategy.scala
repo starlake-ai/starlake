1c2bf78403ce3e9415f82d46490d6a7729dbe496
Hayssam Saleh <hayssam@saleh.fr>
2023-06-26 14:04:14 +0200

    Update AttributeMergeStrategy.scala
    Add comment

--- src/main/scala/ai/starlake/schema/model/AttributeMergeStrategy.scala
+++ src/main/scala/ai/starlake/schema/model/AttributeMergeStrategy.scala
@@ -8,14 +8,22 @@ final case class AttributeMergeStrategy(
 )
 
 sealed trait KeepSourceDiffAttributesStrategy
+// All fields present in the existing yml schema are kept regardless of their presence in the incoming schema.
 final case object KeepAllDiff extends KeepSourceDiffAttributesStrategy
 
+// All scripted fields present in the existing yml schema are kept which means that the incoming schema override the existing one.
+// If a incoming column has the same name as an existing scripted field. The latter is overriden
 final case object KeepOnlyScriptDiff extends KeepSourceDiffAttributesStrategy
 
+// Incoming schema override existing schema.
 final case object DropAll extends KeepSourceDiffAttributesStrategy
 
 /** Define which information has higher priority to merge in ref attribute property.
   */
 sealed trait AttributeRefPropertiesMergeStrategy
+// Attributes properties are based on the following priorities:
+// RefFirst: the incoming schema overrides the existing properties
 final case object RefFirst extends AttributeRefPropertiesMergeStrategy
+
+// SourceFirst: The existing shcema in the yml file are kept intact.
 final case object SourceFirst extends AttributeRefPropertiesMergeStrategy
