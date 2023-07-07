73d216685ebacea03c6e8c16ba9a13d5d480a648
Hayssam Saleh <Hayssam.saleh@starlake.ai>
2023-07-07 09:49:16 +0200

    Fix tests

--- src/main/scala/ai/starlake/schema/model/Env.scala
+++ src/main/scala/ai/starlake/schema/model/Env.scala
@@ -1,7 +1,7 @@
 package ai.starlake.schema.model
 
 import ai.starlake.config.Settings
-import ai.starlake.utils.Formatter._
+import ai.starlake.schema.model.Ref.anyRefPattern
 import com.fasterxml.jackson.annotation.JsonCreator
 
 import java.util.regex.Pattern
@@ -11,37 +11,19 @@ object Ref {
 }
 
 case class InputRef(
-  table: String = "",
-  domain: Option[String] = None,
-  database: Option[String] = None
+  table: Pattern = anyRefPattern,
+  domain: Option[Pattern] = None,
+  database: Option[Pattern] = None
 ) {
   @JsonCreator
-  def this() = this("", None, None) // Should never be called. Here for Jackson deserialization only
-
-  def richFormat(activeEnv: Map[String, String], extraEnv: Map[String, String])(implicit
-    settings: Settings
-  ): InputRef = {
-    InputRef(
-      table.richFormat(activeEnv, extraEnv),
-      domain.map(_.richFormat(activeEnv, extraEnv)),
-      database.map(_.richFormat(activeEnv, extraEnv))
-    )
-  }
+  def this() =
+    this(anyRefPattern, None, None) // Should never be called. Here for Jackson deserialization only
 }
 
 case class OutputRef(table: String = "", domain: String = "", database: String = "") {
   @JsonCreator
   def this() = this("", "", "") // Should never be called. Here for Jackson deserialization only
 
-  def richFormat(activeEnv: Map[String, String], extraEnv: Map[String, String])(implicit
-    settings: Settings
-  ): OutputRef = {
-    OutputRef(
-      table.richFormat(activeEnv, extraEnv),
-      domain.richFormat(activeEnv, extraEnv),
-      database.richFormat(activeEnv, extraEnv)
-    )
-  }
   def asTuple(): (String, String, String) = (database, domain, table)
   def toBigQueryString() = s"`$database.$domain.$table`"
 }
@@ -53,29 +35,19 @@ case class Ref(
   @JsonCreator
   def this() =
     this(InputRef(), OutputRef()) // Should never be called. Here for Jackson deserialization only
-
-  def richFormat(activeEnv: Map[String, String], extraEnv: Map[String, String])(implicit
-    settings: Settings
-  ): Ref = {
-    Ref(
-      input.richFormat(activeEnv, extraEnv),
-      output.richFormat(activeEnv, extraEnv)
-    )
-  }
 }
 
 case class EnvRefs(refs: List[Ref])(implicit settngs: Settings) {
-
-  private def richFormat(
+  private def replace(
     ref: OutputRef,
     thisDatabase: String,
     thisDomain: String,
     thisTable: String
   ): OutputRef = {
     ref.copy(
-      database = ref.database.richFormat(Map("SL_THIS_DATABASE" -> thisDatabase), Map.empty),
-      domain = ref.domain.richFormat(Map("SL_THIS_DOMAIN" -> thisDomain), Map.empty),
-      table = ref.table.richFormat(Map("SL_THIS_TABLE" -> thisTable), Map.empty)
+      database = ref.database.replaceAll("SL_THIS_DATABASE", thisDatabase),
+      domain = ref.domain.replaceAll("SL_THIS_DOMAIN", thisDomain),
+      table = ref.table.replaceAll("SL_THIS_TABLE", thisTable)
     )
   }
   def getOutputRef(database: String, domain: String, table: String): Option[OutputRef] = {
@@ -83,15 +55,15 @@ case class EnvRefs(refs: List[Ref])(implicit settngs: Settings) {
       .find { ref =>
         (ref.input.database, ref.input.domain) match {
           case (Some(inputDatabase), Some(inputDomain)) =>
-            inputDatabase.equalsIgnoreCase(database) &&
-            inputDomain.equalsIgnoreCase(domain) &&
-            ref.input.table.equalsIgnoreCase(table)
+            inputDatabase.matcher(database).matches() &&
+            inputDomain.matcher(domain).matches() &&
+            ref.input.table.matcher(table).matches()
           case _ =>
             false
         }
       }
       .map(_.output)
-    result.map(richFormat(_, database, domain, table))
+    result.map(replace(_, database, domain, table))
   }
 
   def getOutputRef(domain: String, table: String): Option[OutputRef] = {
@@ -99,14 +71,14 @@ case class EnvRefs(refs: List[Ref])(implicit settngs: Settings) {
       .find { ref =>
         ref.input.domain match {
           case Some(inputDomain) =>
-            inputDomain.equalsIgnoreCase(domain) &&
-            ref.input.table.equalsIgnoreCase(table)
+            inputDomain.matcher(domain).matches() &&
+            ref.input.table.matcher(table).matches()
           case None =>
             false
         }
       }
       .map(_.output)
-    result.map(richFormat(_, "", domain, table))
+    result.map(replace(_, "", domain, table))
   }
 
   def getOutputRef(table: String): Option[OutputRef] = {
@@ -114,13 +86,13 @@ case class EnvRefs(refs: List[Ref])(implicit settngs: Settings) {
       .find { ref =>
         ref.input.domain match {
           case None =>
-            ref.input.table.equalsIgnoreCase(table)
+            ref.input.table.matcher(table).matches()
           case _ =>
             false
         }
       }
       .map(_.output)
-    result.map(richFormat(_, "", "", table))
+    result.map(replace(_, "", "", table))
 
   }
 
