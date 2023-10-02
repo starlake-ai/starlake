bb5f74e107ef7d18612d23becd1d17c336bacfa4
Hayssam Saleh <Hayssam.saleh@starlake.ai>
2023-10-02 09:01:57 +0200

    Inherit settings from context and generate  schema with new format

--- src/test/scala/ai/starlake/integration/ExtractIntegrationSpec.scala
+++ src/test/scala/ai/starlake/integration/ExtractIntegrationSpec.scala
@@ -1,7 +1,7 @@
 package ai.starlake.integration
 
 import ai.starlake.TestHelper
-import ai.starlake.extract.ExtractData
+import ai.starlake.extract.{ExtractData, ExtractJDBCSchema}
 import ai.starlake.schema.handlers.SchemaHandler
 import better.files.File
 
@@ -10,7 +10,7 @@ import java.sql.DriverManager
 class ExtractIntegrationSpec extends TestHelper {
 
   new WithSettings() {
-    "All Dag generation" should "succeed" in {
+    "Extract Data" should "succeed" in {
       val jdbcOptions = settings.appConfig.connections("test-h2")
       val conn = DriverManager.getConnection(
         jdbcOptions.options("url"),
@@ -76,5 +76,77 @@ class ExtractIntegrationSpec extends TestHelper {
       val files = tmpDir.listRecursively.filter(_.name.endsWith(".csv")).toList
       assert(files.size == 2)
     }
+    "Extract Schema" should "succeed" in {
+      val jdbcOptions = settings.appConfig.connections("test-h2")
+      val conn = DriverManager.getConnection(
+        jdbcOptions.options("url"),
+        jdbcOptions.options("user"),
+        jdbcOptions.options("password")
+      )
+      val sql: String =
+        """
+          |drop table if exists test_view1;
+          |drop table if exists test_table1;
+          |create table test_table1(ID INT PRIMARY KEY,NAME VARCHAR(500));
+          |create view test_view1 AS SELECT NAME FROM test_table1;
+          |insert into test_table1 values (1,'A');
+          |CREATE TABLE IF NOT EXISTS audit.SL_LAST_EXPORT (
+          |                              domain VARCHAR(255) not NULL,
+          |                              schema VARCHAR(255) not NULL,
+          |                              last_ts TIMESTAMP,
+          |                              last_date DATE,
+          |                              last_long INTEGER,
+          |                              last_decimal DECIMAL,
+          |                              start_ts TIMESTAMP not NULL,
+          |                              end_ts TIMESTAMP not NULL,
+          |                              duration INTEGER not NULL,
+          |                              mode VARCHAR(255) not NULL,
+          |                              count BIGINT not NULL,
+          |                              success BOOLEAN not NULL,
+          |                              message VARCHAR(255) not NULL,
+          |                              step VARCHAR(255) not NULL
+          |                             );""".stripMargin
+      val st = conn.createStatement()
+      st.execute(sql)
+
+      val config =
+        """
+          |extract:
+          |  connectionRef: "test-h2" # Connection name as defined in the connections section of the application.conf file
+          |  jdbcSchemas:
+          |    - schema: "PUBLIC" # Database schema where tables are located
+          |      tables:
+          |        - name: "*" # Takes all tables
+          |      tableTypes: # One or many of the types below
+          |        - "TABLE"
+          |        - "VIEW"
+          |        - "SYSTEM TABLE"
+          |        - "GLOBAL TEMPORARY"
+          |        - "LOCAL TEMPORARY"
+          |        - "ALIAS"
+          |        - "SYNONYM"
+          |""".stripMargin
+      val tmpYmlFile = File.newTemporaryFile("extract", ".sl.yml")
+      val tmpDir = File.newTemporaryDirectory("extract")
+      println(tmpDir.pathAsString)
+      tmpYmlFile.write(config)
+      val schemaHandler = new SchemaHandler(storageHandler)
+      new ExtractJDBCSchema(schemaHandler).run(
+        Array(
+          "--config",
+          tmpYmlFile.pathAsString,
+          "--output-dir",
+          tmpDir.pathAsString
+        )
+      )
+      val files =
+        tmpDir.listRecursively.filter(_.name.endsWith(".sl.yml")).map(_.name.toLowerCase()).toList
+      files should contain theSameElementsAs List(
+        "test_table1.sl.yml",
+        "test_view1.sl.yml",
+        "_config.sl.yml"
+      )
+    }
+
   }
 }
