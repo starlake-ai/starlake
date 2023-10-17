9718fca8dabe29041cbb46725348da21c1af1a38
Scala Steward <scala_steward@virtuslab.com>
2023-10-17 03:55:24 +0000

    Update google-cloud-bigquery to 2.33.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -18,7 +18,7 @@ object Versions {
   val esSpark212 = "8.10.2"
   val scopt = "4.1.0"
   val gcsConnector = "hadoop3-2.2.17"
-  val bigquery = "2.33.1"
+  val bigquery = "2.33.2"
   val hadoop = "3.3.6"
   val sparkBigqueryWithDependencies = "0.32.2"
   val bigqueryConnector = "hadoop3-1.2.0"
