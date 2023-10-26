d2343b139450a239522de54c66da64bfbdb1f6f5
Hayssam Saleh <Hayssam.saleh@starlake.ai>
2023-10-26 17:35:43 +0200

    Add local test

--- src/test/scala/ai/starlake/integration/TransformIntegration2Spec.scala
+++ src/test/scala/ai/starlake/integration/TransformIntegration2Spec.scala
@@ -3,7 +3,11 @@ package ai.starlake.integration
 import ai.starlake.job.Main
 
 class TransformIntegration2Spec extends BigQueryIntegrationSpecBase {
-  if (sys.env.getOrElse("SL_GCP_TEST", "false").toBoolean) {
+  if (
+    sys.env.getOrElse("SL_LOCAL_TEST", "false").toBoolean && sys.env
+      .getOrElse("SL_GCP_TEST", "false")
+      .toBoolean
+  ) {
 
     "Import / Load / Transform BQ" should "succeed" in {
       withEnvs(
