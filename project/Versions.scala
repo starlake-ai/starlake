e5b2b6d87419945b94cfad965e8408ec72c1097b
Scala Steward <scala_steward@virtuslab.com>
2023-11-08 07:10:21 +0000

    Update kafka-clients to 7.5.2-ce

--- project/Versions.scala
+++ project/Versions.scala
@@ -25,7 +25,7 @@ object Versions {
   val h2 = "2.2.224" // Test only
   val poi = "4.1.2"
   val scalate = "1.9.8"
-  val kafkaClients = "7.5.1-ce"
+  val kafkaClients = "7.5.2-ce"
   val confluentVersion = "7.5.1"
   val testContainers = "0.41.0"
   val gcpDataCatalog = "1.36.0"
