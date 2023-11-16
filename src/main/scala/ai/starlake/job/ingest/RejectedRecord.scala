4533ba39416e8eca779f33e51b73aa80a452daab
Hayssam Saleh <Hayssam.saleh@starlake.ai>
2023-11-16 21:31:36 +0100

    Improve autocomplete

--- src/main/scala/ai/starlake/job/ingest/RejectedRecord.scala
+++ src/main/scala/ai/starlake/job/ingest/RejectedRecord.scala
@@ -1,6 +1,9 @@
 package ai.starlake.job.ingest
 
+import ai.starlake.schema.model.{Attribute, Schema}
+
 import java.sql.Timestamp
+import java.util.regex.Pattern
 
 case class RejectedRecord(
   jobid: String,
@@ -10,3 +13,21 @@ case class RejectedRecord(
   error: String,
   path: String
 )
+
+object RejectedRecord {
+  val starlakeSchema = Schema(
+    name = "rejected",
+    pattern = Pattern.compile("ignore"),
+    attributes = List(
+      Attribute("jobid", "string"),
+      Attribute("timestamp", "timestamp"),
+      Attribute("domain", "string"),
+      Attribute("schema", "string"),
+      Attribute("error", "string"),
+      Attribute("path", "string")
+    ),
+    None,
+    None,
+    None
+  )
+}
