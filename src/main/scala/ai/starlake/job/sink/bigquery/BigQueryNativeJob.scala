4573daeb3144feedd7d148bf61ff43aeed3e3c36
Hayssam Saleh <Hayssam.saleh@starlake.ai>
2023-12-18 18:32:57 +0100

    Prepare redshift / snowflake native merge on load

--- src/main/scala/ai/starlake/job/sink/bigquery/BigQueryNativeJob.scala
+++ src/main/scala/ai/starlake/job/sink/bigquery/BigQueryNativeJob.scala
@@ -416,6 +416,7 @@ class BigQueryNativeJob(
               queryConfig.setTimePartitioning(partitioning)
           case None =>
             queryConfig
+
         }
 
         logger.info("Computing clustering")
