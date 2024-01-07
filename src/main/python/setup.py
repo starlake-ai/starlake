76dd1b317496575320ed14ee7f2852e833678d59
Stéphane Manciot <stephane.manciot@gmail.com>
2024-01-07 20:05:29 +0100

    v0.0.1

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='starlake-airflow',
-      version='0.0.1rc3',
+      version='0.0.1',
       description='Starlake Python Distribution For Airflow',
       long_description=long_description,
       long_description_content_type="text/markdown",
