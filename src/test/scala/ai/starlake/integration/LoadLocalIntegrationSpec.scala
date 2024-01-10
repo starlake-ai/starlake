bbb717f87344aff3f7e68775829334b051315051
Hayssam Saleh <Hayssam.saleh@starlake.ai>
2024-01-10 20:37:19 +0100

    No need to keep LocalStorageHandler

--- src/test/scala/ai/starlake/integration/LoadLocalIntegrationSpec.scala
+++ src/test/scala/ai/starlake/integration/LoadLocalIntegrationSpec.scala
@@ -2,7 +2,7 @@ package ai.starlake.integration
 
 import ai.starlake.job.Main
 
-class LoadLocalIntegrationSpec extends JDBCIntegrationSpecBase {
+class LoadLocalIntegrationSpec extends IntegrationTestBase {
   override def templates = starlakeDir / "samples"
   override def localDir = templates / "spark"
   override val incomingDir = localDir / "incoming"
@@ -11,15 +11,12 @@ class LoadLocalIntegrationSpec extends JDBCIntegrationSpecBase {
     withEnvs(
       "SL_ROOT"                                       -> localDir.pathAsString,
       "SL_INTERNAL_SUBSTITUTE_VARS"                   -> "true",
+      "SL_ENV"                                        -> "LOCAL",
       "SL_SPARK_SQL_SOURCES_PARTITION_OVERWRITE_MODE" -> "DYNAMIC",
       "SL_MERGE_OPTIMIZE_PARTITION_WRITE"             -> "true"
     ) {
       cleanup()
       copyFilesToIncomingDir(sampleDataDir)
-      val hrDir = incomingDir / "hr"
-      hrDir.list(_.isRegularFile).foreach(_.delete())
-      val salesDir = incomingDir / "sales"
-      salesDir.list(_.isRegularFile).foreach(_.delete())
       Main.main(
         Array("import")
       )
