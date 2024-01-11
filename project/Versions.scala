ba6352220b6ed02b0499c23bb853960fc29137ab
Hayssam Saleh <Hayssam.saleh@starlake.ai>
2024-01-11 11:30:38 +0100

    Upgrade sql parser version

--- project/Versions.scala
+++ project/Versions.scala
@@ -33,6 +33,6 @@ object Versions {
   val jinja = "2.7.1"
   val snowflakeJDBC = "3.13.30"
   val snowflakeSpark: String = "2.11.3-spark_3.3"
-  val sqlParser = "4.7"
+  val jSqlParser = "4.8"
   val bigQueue = "0.7.0"
 }
