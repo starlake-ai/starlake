597c87e2ba329a787c5fb5a2df3c163d40ca5eec
Scala Steward <scala_steward@virtuslab.com>
2024-01-13 11:39:46 +0000

    Update kafka-avro-serializer, ... to 7.5.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -26,7 +26,7 @@ object Versions {
   val poi = "4.1.2"
   val scalate = "1.9.8"
   val kafkaClients = "7.5.1-ce"
-  val confluentVersion = "7.5.2"
+  val confluentVersion = "7.5.3"
   val testContainers = "0.41.0"
   val gcpDataCatalog = "1.37.0"
   val gcpCloudLogging = "3.15.14"
