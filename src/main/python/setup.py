601874df2829d3514e8e696ec44030eaa71d1706
Stéphane Manciot <stephane.manciot@gmail.com>
2024-01-17 08:21:51 +0100

    to fix cloud run async value, add retry delay in seconds, update bash command when retry on failure

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='starlake-airflow',
-      version='0.0.10',
+      version='0.0.11',
       description='Starlake Python Distribution For Airflow',
       long_description=long_description,
       long_description_content_type="text/markdown",
