e1a756155089a3613d776da9ea0c8dc66c4d480d
Stéphane Manciot <stephane.manciot@gmail.com>
2024-01-25 05:00:46 +0100

    add library for stralake-orchestration, update package ai.starlake.job.airflow to ai.starlake.airflow

--- src/main/python/starlake-orchestration/ai/starlake/job/starlake_options.py
+++ src/main/python/starlake-orchestration/ai/starlake/job/starlake_options.py
@@ -11,7 +11,21 @@ class StarlakeOptions:
 
     @classmethod
     def get_context_var(cls, var_name: str, default_value: any=None, options: dict = None, **kwargs):
-        """Get context variable."""
+        """
+        Get the value of the specified variable from the context.
+        The value is searched in the following order:
+        - options
+        - default_value
+        - Environment variable
+        Args:
+            var_name (str): The variable name.
+            default_value (any, optional): The optional default value. Defaults to None.
+            options (dict, optional): The optional options dictionary. Defaults to None.
+            Raises:
+                MissingEnvironmentVariable: If the variable does not exist.
+            Returns:
+                any: The variable value.
+        """
         if options and options.get(var_name):
             return options.get(var_name)
         elif default_value is not None:
