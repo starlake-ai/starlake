130f8d8e58c1fab8fe27b43bbbdde8576003ffd2
Stéphane Manciot <stephane.manciot@gmail.com>
2024-02-07 18:39:45 +0100

    add dagster op factories for dataproc and cloud run, add dagster load and transform templates for cloud run and dataproc

---
+++ src/main/python/starlake-dagster/ai/starlake/dagster/gcp/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['starlake_dagster_cloud_run_job', 'starlake_dagster_dataproc_job']
+
+from .starlake_dagster_cloud_run_job import StarlakeDagsterCloudRunJob
+from .starlake_dagster_dataproc_job import StarlakeDagsterDataprocJob
