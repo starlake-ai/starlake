28242842fdc546979b2bd7d7ce6341f3aec97992
Scala Steward <scala_steward@virtuslab.com>
2024-02-22 04:47:57 +0000

    Update postgres-socket-factory to 1.15.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -148,7 +148,7 @@ object Dependencies {
   )
 
   val pgGcp = Seq(
-    "com.google.cloud.sql" % "postgres-socket-factory" % "1.15.0" % Test,
+    "com.google.cloud.sql" % "postgres-socket-factory" % "1.15.2" % Test,
     "com.dimafeng" %% "testcontainers-scala-postgresql" % Versions.testContainers % Test excludeAll (jnaExclusions: _*),
     "org.postgresql" % "postgresql" % "42.7.1" % Test
   )
