aa60eaaa5509a999626cb4f13749f61f51ccf4ae
Scala Steward <scala_steward@virtuslab.com>
2024-02-22 04:47:16 +0000

    Update google-cloud-datacatalog to 1.42.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -28,7 +28,7 @@ object Versions {
   val kafkaClients = "7.6.0-ce"
   val confluentVersion = "7.6.0"
   val testContainers = "0.41.3"
-  val gcpDataCatalog = "1.41.0"
+  val gcpDataCatalog = "1.42.0"
   val gcpCloudLogging = "3.15.17"
   val jinja = "2.7.2"
   val snowflakeJDBC = "3.13.30"
