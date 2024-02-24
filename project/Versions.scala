57f3ac5446894ab561cdaa0a4f0310c0863f9b09
Scala Steward <scala_steward@virtuslab.com>
2024-02-24 01:07:38 +0000

    Update spark-avro, spark-core, spark-hive, ... to 3.5.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val curator = "2.6.0"
-  val spark3d0 = "3.5.0"
+  val spark3d0 = "3.5.1"
   val deltaSpark3d0 = "3.1.0"
   val scalatest = "3.2.18"
   val sparkXML2d0 = "0.13.0"
