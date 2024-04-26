8f1de58e17c814daf7ee68c69b048e2e5470ddc4
tiboun <bounkong@gmail.com>
2024-04-26 22:58:00 +0200

    improvement: add log during long extraction around every 30 seconds

--- src/main/scala/ai/starlake/extract/SLObjectRowWriterProcessor.scala
+++ src/main/scala/ai/starlake/extract/SLObjectRowWriterProcessor.scala
@@ -1,11 +1,17 @@
 package ai.starlake.extract
 
+import com.typesafe.scalalogging.StrictLogging
 import com.univocity.parsers.common.NormalizedString
 import com.univocity.parsers.common.processor.ObjectRowWriterProcessor
 
-class SLObjectRowWriterProcessor extends ObjectRowWriterProcessor {
+import java.time.{Duration, Instant}
+
+class SLObjectRowWriterProcessor(context: String)
+    extends ObjectRowWriterProcessor
+    with StrictLogging {
 
   private var recordsCount: Long = 0
+  private var lastNotifiedTime: Instant = Instant.now()
 
   override def write(
     input: Array[AnyRef],
@@ -13,6 +19,12 @@ class SLObjectRowWriterProcessor extends ObjectRowWriterProcessor {
     indexesToWrite: Array[Int]
   ): Array[AnyRef] = {
     recordsCount += 1
+    if (Duration.between(lastNotifiedTime, Instant.now()).toSeconds > 30) {
+      lastNotifiedTime = Instant.now()
+      logger.info(
+        s"$context Already extracted ${this.getRecordsCount()} rows"
+      )
+    }
     super.write(input, headers, indexesToWrite)
   }
 
