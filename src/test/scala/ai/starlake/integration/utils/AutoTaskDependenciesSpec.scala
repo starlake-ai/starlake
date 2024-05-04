8be239906f147e307a90fe4b0c4ec2da9b51f1e4
Hayssam Saleh <Hayssam.saleh@starlake.ai>
2024-05-04 11:18:27 +0200

    Windows install

--- src/test/scala/ai/starlake/integration/utils/AutoTaskDependenciesSpec.scala
+++ src/test/scala/ai/starlake/integration/utils/AutoTaskDependenciesSpec.scala
@@ -34,7 +34,7 @@ class AutoTaskDependenciesSpec extends IntegrationTestBase {
     }
   }
 
-  "sample test" should "succeed" in {
+  "Dependencies" should "succeed" in {
     if (sys.env.getOrElse("SL_LOCAL_TEST", "true").toBoolean) {
       withEnvs("SL_ROOT" -> localDir.pathAsString, "SL_ENV" -> "DUCKDB") {
         assert(
@@ -45,7 +45,7 @@ class AutoTaskDependenciesSpec extends IntegrationTestBase {
       }
     }
   }
-  "Dependency Generation" should "succeed" in {
+  "Lineage" should "succeed" in {
     if (sys.env.getOrElse("SL_LOCAL_TEST", "true").toBoolean) {
       withEnvs(
         "SL_ROOT" -> localDir.pathAsString,
