8f7155c19108b82b48e9c9f0ee57f45a1ce344bb
Scala Steward <scala_steward@virtuslab.com>
2024-05-06 02:27:16 +0000

    Update google-cloud-datacatalog to 1.48.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -30,7 +30,7 @@ object Versions {
   val confluentVersion = "7.6.1"
   val kafkaClients = "7.6.1-ce"
   val testContainers = "0.41.3"
-  val gcpDataCatalog = "1.47.0"
+  val gcpDataCatalog = "1.48.0"
   val gcpCloudLogging = "3.16.3"
   val jinja = "2.7.2" // forces dependency override on guava
   val snowflakeJDBC = "3.15.1"
