6989c2fe844fd5e35852ab3d1719bfe58d184be1
Scala Steward <scala_steward@virtuslab.com>
2024-05-06 02:28:22 +0000

    Update spark-bigquery-with-dependencies to 0.38.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -22,7 +22,7 @@ object Versions {
   val gcsConnector = "hadoop3-2.2.21"
   val bigquery = "2.38.2"
   val hadoop = "3.3.6"
-  val sparkBigqueryWithDependencies = "0.37.0"
+  val sparkBigqueryWithDependencies = "0.38.0"
   val bigqueryConnector = "hadoop3-1.2.0"
   val h2 = "2.2.224" // Test only
   val poi = "4.1.2"
