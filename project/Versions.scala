973ab53ca7e3dc825749dbfe85eaf28a102f4aca
Scala Steward <scala_steward@virtuslab.com>
2024-05-25 20:31:20 +0000

    Update duckdb_jdbc to 0.10.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -35,7 +35,7 @@ object Versions {
   val jinja = "2.7.2" // forces dependency override on guava
   val snowflakeJDBC = "3.16.0"
   val snowflakeSpark: String = "2.15.0-spark_3.4"
-  val duckdb = "0.10.2"
+  val duckdb = "0.10.3"
   val jSqlParser = "4.10-SNAPSHOT"
   val jSqlTranspiler = "0.2-SNAPSHOT"
   val bigQueue = "0.7.0"
