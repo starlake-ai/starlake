aae9a33b06e1b37bca6a6ed641e91892fa40a080
Stéphane Manciot <stephane.manciot@gmail.com>
2024-06-03 18:00:46 +0200

    remove previous sl python sources

--- src/main/resources/python/orchestration/ai/starlake/job/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ['spark_config', 'starlake_job', 'starlake_options', 'starlake_pre_load_strategy']
-
-from .spark_config import StarlakeSparkConfig, StarlakeSparkExecutorConfig
-from .starlake_job import IStarlakeJob
-from .starlake_options import StarlakeOptions
-from .starlake_pre_load_strategy import StarlakePreLoadStrategy
