f274912321acebcda8eb724762d4b1853d56b2de
Scala Steward <scala_steward@virtuslab.com>
2024-07-10 21:34:08 +0000

    Update gcs-connector to hadoop3-2.2.24

--- project/Versions.scala
+++ project/Versions.scala
@@ -20,7 +20,7 @@ object Versions {
   val jsonSchemaValidator = "1.4.0"
   val scopt = "4.1.0"
   val bigquery = "2.41.0"
-  val gcsConnector = "hadoop3-2.2.23"
+  val gcsConnector = "hadoop3-2.2.24"
   val hadoop = "3.3.6"
   val sparkBigqueryWithDependencies = "0.39.1"
   val bigqueryConnector = "hadoop3-1.2.0"
