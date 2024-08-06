a64ea7d6c61e24560bdb17497f406205e625e3ee
Scala Steward <scala_steward@virtuslab.com>
2024-08-06 03:43:04 +0000

    Update google-cloud-logging to 3.20.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -31,7 +31,7 @@ object Versions {
   val kafkaClients = "7.6.1-ce"
   val testContainers = "0.41.4"
   val gcpDataCatalog = "1.52.0"
-  val gcpCloudLogging = "3.19.0"
+  val gcpCloudLogging = "3.20.0"
   val jinja = "2.7.2" // forces dependency override on guava
   val snowflakeJDBC = "3.16.1"
   val snowflakeSpark: String = "2.16.0-spark_3.4"
