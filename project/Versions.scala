d4c28606cd22df3489313921d9ae10c7ea2c64ea
Scala Steward <scala_steward@virtuslab.com>
2024-09-29 00:33:37 +0000

    Update delta-spark to 3.2.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val curator = "2.6.0"
   val spark3d0 = "3.5.2"
-  val deltaSpark3d0 = "3.2.0"
+  val deltaSpark3d0 = "3.2.1"
   val scalatest = "3.2.19"
   val scalacheckForScalatest = "3.2.18.0"
   val sparkXML2d0 = "0.13.0"
