074fa7b4f3879e302fe23c27332b92373b84fa39
Stéphane Manciot <stephane.manciot@gmail.com>
2024-10-11 14:51:25 +0200

    to fix docker prepare

--- scripts/docker-multi-arch-build.sh
+++ scripts/docker-multi-arch-build.sh
@@ -1,3 +1,5 @@
-docker buildx use multi-arch-builder
-docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64  --build-arg SCALA_VERSION=2.13 -t starlakeai/starlake:latest .
+#!/usr/bin/env bash
+
+set -e
+
+./scripts/docker-build.sh -e dev -p
