e1895151ddd2bcfeb1f4c6cf0be6cd36e36aab0d
Stéphane Manciot <stephane.manciot@gmail.com>
2024-10-11 14:45:40 +0200

    update docker build, include docker build and push within  github actions

---
+++ scripts/versions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+while read line; do
+  export $line
+done < .versions
+
+FIRST_LINE=$(head -n 1 version.sbt)
+export SL_VERSION=$(echo "$FIRST_LINE" | sed -E 's/.*version := "([0-9]+\.[0-9]+\.?.*)"/\1/')
+export SL_MAJOR_MINOR_VERSION=$(echo "${SL_VERSION}" | cut -d'.' -f1-2)
+echo "SL_MAJOR_MINOR_VERSION=${SL_MAJOR_MINOR_VERSION}"
+export REGISTRY_IMAGE=starlakeai/starlake
+echo "REGISTRY_IMAGE=${REGISTRY_IMAGE}"
+export REGISTRY_IMAGE_LATEST=${REGISTRY_IMAGE}:${SL_VERSION:-latest}
+echo "REGISTRY_IMAGE_LATEST=${REGISTRY_IMAGE_LATEST}"
