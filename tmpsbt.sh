756be82d25b6e20a3fa5bb30b6ecf1cea7d0dd50
Hayssam Saleh <hayssam@saleh.fr>
2024-10-21 16:06:40 +0200

    rich format rws sqls

--- tmpsbt.sh
+++ tmpsbt.sh
@@ -1,4 +1,4 @@
 sbt ++2.13 assembly
-cp $HOME/git/public/starlake/target/scala-2.13/starlake-spark3_2.13-1.3.0-SNAPSHOT-assembly.jar $HOME/starlake/bin/sl/
-cp $HOME/git/public/starlake/target/scala-2.13/starlake-spark3_2.13-1.3.0-SNAPSHOT-assembly.jar $HOME/git/starlake-api/lib/
-cp $HOME/git/public/starlake/target/scala-2.13/starlake-spark3_2.13-1.3.0-SNAPSHOT-assembly.jar $HOME/git/starlake-api/tmpbuild/starlake/bin/sl/
+cp $HOME/git/public/starlake/target/scala-2.13/starlake-spark3_2.13-1.3.1-SNAPSHOT-assembly.jar $HOME/starlake/bin/sl/
+cp $HOME/git/public/starlake/target/scala-2.13/starlake-spark3_2.13-1.3.1-SNAPSHOT-assembly.jar $HOME/git/starlake-api/lib/
+cp $HOME/git/public/starlake/target/scala-2.13/starlake-spark3_2.13-1.3.1-SNAPSHOT-assembly.jar $HOME/git/starlake-api/tmpbuild/starlake/bin/sl/
