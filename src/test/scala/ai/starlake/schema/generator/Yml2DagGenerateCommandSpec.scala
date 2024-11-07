8fa726a4fca35e042d5304eb4e989b3ae667682b
Hayssam Saleh <hayssam@saleh.fr>
2024-11-07 19:37:22 +0100

    Update dag path

--- src/test/scala/ai/starlake/schema/generator/Yml2DagGenerateCommandSpec.scala
+++ src/test/scala/ai/starlake/schema/generator/Yml2DagGenerateCommandSpec.scala
@@ -85,7 +85,7 @@ class Yml2DagGenerateCommandSpec extends TestHelper {
         )
         val schemaHandler = settings.schemaHandler()
         new DagGenerateJob(schemaHandler).run(Array.empty)
-        val dagPath = new Path(new Path(DatasetArea.dags, "generated/load"), "position.py")
+        val dagPath = new Path(new Path(DatasetArea.dags, "generated"), "position.py")
         settings.storageHandler().exists(dagPath) shouldBe true
         val dagContent = settings.storageHandler().read(dagPath)
         dagContent should include("description='sample dag configuration'")
