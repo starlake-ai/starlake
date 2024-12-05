71fd17d93ff217adb2bd7545b224a3b2df14fa43
Stéphane Manciot <stephane.manciot@gmail.com>
2024-12-05 21:24:46 +0100

    update orchestration framework, update airflow and dagster implementations accordingly

--- src/main/python/starlake-airflow/ai/starlake/airflow/__init__.py
+++ src/main/python/starlake-airflow/ai/starlake/airflow/__init__.py
@@ -2,10 +2,10 @@ __all__ = ['starlake_airflow_job', 'starlake_airflow_options', 'starlake_airflow
 
 from .starlake_airflow_job import StarlakeAirflowJob, DEFAULT_DAG_ARGS, DEFAULT_POOL
 from .starlake_airflow_options import StarlakeAirflowOptions
-from .starlake_airflow_orchestration import StarlakeAirflowOrchestration
+from .starlake_airflow_orchestration import AirflowOrchestration
 
 from ai.starlake.job import StarlakeOrchestrator
 
-from ai.starlake.orchestration import StarlakeOrchestrationFactory
+from ai.starlake.orchestration import OrchestrationFactory
 
-StarlakeOrchestrationFactory.register_orchestration(StarlakeOrchestrator.airflow, StarlakeAirflowOrchestration)
+OrchestrationFactory.register_orchestration(StarlakeOrchestrator.airflow, AirflowOrchestration)
--- src/main/python/starlake-dagster/ai/starlake/dagster/__init__.py
+++ src/main/python/starlake-dagster/ai/starlake/dagster/__init__.py
@@ -1,2 +1,9 @@
 # package ai.starlake.dagster
 from .starlake_dagster_job import StarlakeDagsterJob
+from .starlake_dagster_orchestration import DagsterPipeline, DagsterOrchestration
+
+from ai.starlake.job import StarlakeOrchestrator
+
+from ai.starlake.orchestration import OrchestrationFactory
+
+OrchestrationFactory.register_orchestration(StarlakeOrchestrator.dagster, DagsterOrchestration)
