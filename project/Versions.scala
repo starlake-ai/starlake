37b136f0bba4033597d2a73eefe7056af1c6fd1d
Scala Steward <scala_steward@virtuslab.com>
2024-12-07 17:32:36 +0000

    Update hadoop-client, hadoop-common, ... to 3.4.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -22,7 +22,7 @@ object Versions {
   val bigquery = "2.44.0"
   // val gcsConnector = "3.0.4"
   val gcsConnector = "hadoop3-2.2.25"
-  val hadoop = "3.3.6"
+  val hadoop = "3.4.1"
   val sparkBigqueryWithDependencies = "0.41.0"
   val bigqueryConnector = "hadoop3-1.2.0"
   val h2 = "2.3.232" // Test only
