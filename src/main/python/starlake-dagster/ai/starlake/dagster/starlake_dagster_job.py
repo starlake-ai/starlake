e9f78294ab3524baeb2e91e645a37cea35947e47
Stéphane Manciot <stephane.manciot@gmail.com>
2024-12-08 17:56:06 +0100

    update sl_orchestrator method signature

--- src/main/python/starlake-dagster/ai/starlake/dagster/starlake_dagster_job.py
+++ src/main/python/starlake-dagster/ai/starlake/dagster/starlake_dagster_job.py
@@ -17,7 +17,7 @@ class StarlakeDagsterJob(IStarlakeJob[NodeDefinition, AssetKey], StarlakeOptions
     def __init__(self, filename: str, module_name: str, pre_load_strategy: Union[StarlakePreLoadStrategy, str, None]=None, options: dict=None, **kwargs) -> None:
         super().__init__(filename=filename, module_name=module_name, pre_load_strategy=pre_load_strategy, options=options, **kwargs)
 
-    def sl_orchestrator(self) -> StarlakeOrchestrator:
+    def sl_orchestrator(self) -> Union[StarlakeOrchestrator, str]:
          return StarlakeOrchestrator.dagster
 
     def update_events(self, event: AssetKey, **kwargs) -> Tuple[(str, List[AssetKey])]:
