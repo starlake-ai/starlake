412efcc08887e9857213814c9b6a739f06900ff0
Stéphane Manciot <stephane.manciot@gmail.com>
2024-12-11 17:48:31 +0100

    fix wrong import for dagster

--- src/main/python/starlake-dagster/ai/starlake/dagster/__init__.py
+++ src/main/python/starlake-dagster/ai/starlake/dagster/__init__.py
@@ -1,4 +1,4 @@
-# package ai.starlake.dagster
+__all__ = ["starlake_dagster_job", "starlake_dagster_orchestration"]
 from .starlake_dagster_job import StarlakeDagsterJob, DagsterDataset
 from .starlake_dagster_orchestration import DagsterPipeline, DagsterOrchestration
 
