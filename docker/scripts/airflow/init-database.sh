e2c78bafec48e41d2ed0ba4b90f0941bcc921590
Stéphane Manciot <stephane.manciot@gmail.com>
2025-01-08 11:43:21 +0100

    delete docker compose materials

--- docker/scripts/airflow/init-database.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-    CREATE DATABASE ${AIRFLOW_DB:-airflow};
-    GRANT ALL PRIVILEGES ON DATABASE ${AIRFLOW_DB:-airflow} TO "$POSTGRES_USER";
-EOSQL
