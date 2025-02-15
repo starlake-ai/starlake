ba8e66f59487856a53b0c0790072b79bb7ae2bd1
Stéphane Manciot <stephane.manciot@gmail.com>
2025-02-15 10:28:20 +0100

    init snowflake orchestration

---
+++ src/main/python/starlake-snowflake/ai/starlake/snowflake/__init__.py
@@ -0,0 +1,4 @@
+# package snowflake
+__all__ = ['StarlakeSnowflakeJob', 'StarlakeSnowflakeOrchestrator']
+from .starlake_snowflake_job import StarlakeSnowflakeJob
+from .starlake_snowflake_orchestration import StarlakeSnowflakeOrchestrator
