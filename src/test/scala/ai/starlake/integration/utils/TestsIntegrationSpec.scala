3843a19120a4b09b75b058050e8a43abccbeea1c
Hayssam Saleh <hayssam@saleh.fr>
2025-04-15 18:57:49 +0200

    rename spark sample to the more meaningful any-engine

--- src/test/scala/ai/starlake/integration/utils/TestsIntegrationSpec.scala
+++ src/test/scala/ai/starlake/integration/utils/TestsIntegrationSpec.scala
@@ -2,13 +2,8 @@ package ai.starlake.integration.utils
 
 import ai.starlake.integration.IntegrationTestBase
 import ai.starlake.job.Main
-import better.files.File
 
 class TestsIntegrationSpec extends IntegrationTestBase {
-  override def samplesFolder: File = starlakeDir / "samples"
-
-  override def theSampleFolder: File = samplesFolder / "spark"
-
   "Load Tests" should "succeed" in {
     withEnvs(
       "SL_ROOT" -> theSampleFolder.pathAsString
