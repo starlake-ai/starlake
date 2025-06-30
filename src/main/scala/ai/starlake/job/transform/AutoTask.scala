70f898d2b9eec648aab01f56ee8e7118050da254
Hayssam Saleh <hayssam@saleh.fr>
2025-06-30 17:44:16 +0200

    page number starts at 0

--- src/main/scala/ai/starlake/job/transform/AutoTask.scala
+++ src/main/scala/ai/starlake/job/transform/AutoTask.scala
@@ -454,9 +454,9 @@ abstract class AutoTask(
     ) {
       if (trimmedSql.endsWith(";")) {
         val noDelimiterSql = trimmedSql.dropRight(1)
-        s"$noDelimiterSql LIMIT $limit OFFSET ${pageSize * (pageNumber - 1)}"
+        s"$noDelimiterSql LIMIT $limit OFFSET ${pageSize * pageNumber}"
       } else
-        s"$sql LIMIT $limit OFFSET ${pageSize * (pageNumber - 1)}"
+        s"$sql LIMIT $limit OFFSET ${pageSize * pageNumber}"
     } else
       sql
   }
