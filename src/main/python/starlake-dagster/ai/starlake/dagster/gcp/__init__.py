ec1653812e61df79bd378ef13646bdd6acfa01a0
Stéphane Manciot <stephane.manciot@gmail.com>
2025-10-21 06:41:55 +0200

    remove resources related to sl orchestration - add sl templates as dependencies

--- src/main/python/starlake-dagster/ai/starlake/dagster/gcp/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ['starlake_dagster_cloud_run_job', 'starlake_dagster_dataproc_job']
-
-from .starlake_dagster_cloud_run_job import StarlakeDagsterCloudRunJob
-from .starlake_dagster_dataproc_job import StarlakeDagsterDataprocJob
--- src/main/python/starlake-dagster/ai/starlake/dagster/shell/__init__.py
+++
@@ -1,2 +0,0 @@
-# package ai.starlake.dagster.shell
-from .starlake_dagster_shell_job import StarlakeDagsterShellJob
