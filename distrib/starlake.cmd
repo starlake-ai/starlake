9b481c15d39ebfbb6e8736320a76cd19794ee1b9
Hayssam Saleh <hayssam@saleh.fr>
2025-11-25 14:37:00 +0100

    Update starlake.cmd

--- distrib/starlake.cmd
+++ distrib/starlake.cmd
@@ -2,187 +2,215 @@
 setlocal enabledelayedexpansion
 
 set "SCRIPT_DIR=%~dp0"
+set "SL_SCRIPT_DIR=%SCRIPT_DIR%"
+set "API_BIN_DIR=%SCRIPT_DIR%\bin\api\bin"
 
-if "%SL_ROOT%"=="" (
-    SET "SL_ROOT=%cd%"
+if not defined SL_ROOT (
+    set "SL_ROOT=%cd%"
+    set "SL_ROOT=!SL_ROOT:\=/!"
 )
 
-if "%~1"=="reinstall" (
+if /i "%1" == "reinstall" (
+    if exist "%SCRIPT_DIR%versions.bat" del "%SCRIPT_DIR%versions.bat"
     if exist "%SCRIPT_DIR%bin\spark" rmdir /s /q "%SCRIPT_DIR%bin\spark"
 ) else (
- IF EXIST "%SCRIPT_DIR%versions.cmd" (
-     call "%SCRIPT_DIR%versions.cmd"
- )
-)
-
-
-
-:: Internal variables
-set "SL_ARTIFACT_NAME=starlake-core_%SCALA_VERSION%"
-set "SPARK_DIR_NAME=spark-%SPARK_VERSION%-bin-hadoop%HADOOP_MAJOR_VERSION%"
-set "SPARK_TARGET_FOLDER=%SCRIPT_DIR%bin\spark"
-set "DEPS_EXTRA_LIB_FOLDER=%SCRIPT_DIR%bin\deps"
-set "STARLAKE_EXTRA_LIB_FOLDER=%SCRIPT_DIR%bin\sl"
-if "%SPARK_DRIVER_MEMORY%"=="" set "SPARK_DRIVER_MEMORY=4G"
-set "SL_MAIN=ai.starlake.job.Main"
-set "HADOOP_HOME=%SCRIPT_DIR%bin\hadoop"
-:: set "SL_VALIDATE_ON_LOAD=false"
-if not "%SL_VERSION%"=="" SET SL_JAR_NAME=%SL_ARTIFACT_NAME%-%SL_VERSION%-assembly.jar
-:: End of internal variables
-
-:: Check if Java is installed using JAVA_HOME env variable
-
-if "%JAVA_HOME%"=="" (
-    set "RUNNER=java"
-) else (
-    set "RUNNER=%JAVA_HOME%\bin\java"
-)
-
-IF /i "%language%"=="de" goto languageDE
-
-SET SL_HTTP_HOST=127.0.0.1
-if "%SL_HTTP_HOST%"=="" SET SL_HTTP_HOST=127.0.0.1
-SET SL_SERVE_URI=http://%SL_HTTP_HOST%:%SL_HTTP_PORT%
-
-if "%~1"=="--version" (
-	  echo Starlake %SL_VERSION%
-	  echo Duckdb JDBC driver %DUCKDB_VERSION%
-	  echo BigQuery Spark connector %SPARK_BQ_VERSION%
-	  echo Hadoop for Azure %HADOOP_AZURE_VERSION%
-	  echo Azure Storage %AZURE_STORAGE_VERSION%
-	  echo Spark %SPARK_VERSION%
-	  echo Hadoop %HADOOP_VERSION%
-	  echo Snowflake Spark connector %SPARK_SNOWFLAKE_VERSION%
-	  echo Snowflake JDBC driver %SNOWFLAKE_JDBC_VERSION%
-	  echo Postgres JDBC driver %POSTGRESQL_VERSION%
-	  echo AWS SDK %AWS_JAVA_SDK_VERSION%
-	  echo Hadoop for AWS %HADOOP_AWS_VERSION%
-	  echo Redshift JDBC driver %REDSHIFT_JDBC_VERSION%
-	  echo Redshift Spark connector %SPARK_REDSHIFT_VERSION%
-   ) else (
-    if "%~1"=="reinstall" (
-        call :launch_setup %*
-        echo Installation done. You're ready to enjoy Starlake!
-        echo If any errors happen during installation. Please try to install again or open an issue.
-    ) else (
-        if "%~1"=="install" (
-            call :launch_setup %*
-            echo Installation done. You're ready to enjoy Starlake!
-            echo If any errors happen during installation. Please try to install again or open an issue.
-        ) else (
-            if "%~1"=="serve" (
-                call :launch_starlake %*
-            ) else (
-                if "%SL_HTTP_PORT%"=="" (
-                    call :launch_starlake %*
-                ) else (
-                    FOR %%x IN (validation run transform compile) DO DEL /F /Q %SL_ROOT\%out\%%x.log 2>NUL
-                    curl.exe  "%SL_SERVE_URI%?ROOT=%SL_ROOT%&PARAMS=%*"
-                    FOR %%x IN (validation run transform compile) DO TYPE  %SL_ROOT%\out\%%x.log 2>NUL
-                )
-            )
-        )
+    if exist "%SCRIPT_DIR%versions.bat" (
+        call "%SCRIPT_DIR%versions.bat"
     )
 )
-goto :eof
 
-:launch_setup
-for /f tokens^=2-5^ delims^=.-_+^" %%j in ('"%RUNNER%" -fullversion 2^>^&1') do set "javaVersion=%%j%%k%%l%%m"
+set "SL_ARTIFACT_NAME=starlake-core_%SCALA_VERSION%"
+set "SPARK_DIR_NAME=spark-%SPARK_VERSION%-bin-hadoop%HADOOP_VERSION%"
+set "SPARK_TARGET_FOLDER=%SCRIPT_DIR%bin\spark"
+set "SPARK_EXTRA_LIB_FOLDER=%SCRIPT_DIR%bin"
+set "DEPS_EXTRA_LIB_FOLDER=%SPARK_EXTRA_LIB_FOLDER%\deps"
+set "STARLAKE_EXTRA_LIB_FOLDER=%SPARK_EXTRA_LIB_FOLDER%\sl"
 
-:: Check if Java is installed
-if "%javaVersion%"=="" (
-    echo Java is not installed. Please install Java 11 or above.
-    exit /b 1
+if not defined SPARK_DRIVER_MEMORY set "SPARK_DRIVER_MEMORY=4g"
+set "SL_MAIN=ai.starlake.job.Main"
+if not defined SPARK_MASTER_URL set "SPARK_MASTER_URL=local[*]"
+
+if defined SL_VERSION (
+    set "SL_JAR_NAME=%SL_ARTIFACT_NAME%-%SL_VERSION%-assembly.jar"
 )
 
-:: Check if Java version is less than 11
-if "%javaVersion%" LSS "11000" (
-    echo Java version %javaVersion% is not supported. Please install Java 11 or above.
-    exit /b 1
-
-)
-
-set setup_url=https://raw.githubusercontent.com/starlake-ai/starlake/master/distrib/setup.jar
+set "PROXY="
 if defined https_proxy (
     set "PROXY=%https_proxy%"
 ) else if defined http_proxy (
     set "PROXY=%http_proxy%"
-) else (
-    set "PROXY="
 )
 
-if not "%PROXY%"=="" if defined SL_INSECURE (
-    set "CURL_EXTRA=--insecure"
-) else (
-    set "CURL_EXTRA="
+set "JAVA_ARGS="
+if defined HTTPS_PROXY (
+    echo Using HTTPS_PROXY: %HTTPS_PROXY%
+    set "JAVA_ARGS=%JAVA_ARGS% -Dhttps.proxyHost=%HTTPS_PROXY_HOST% -Dhttps.proxyPort=%HTTPS_PROXY_PORT%"
+    if defined HTTPS_PROXY_USER set "JAVA_ARGS=%JAVA_ARGS% -Dhttps.proxyUser=%HTTPS_PROXY_USER%"
+    if defined HTTPS_PROXY_PASS set "JAVA_ARGS=%JAVA_ARGS% -Dhttps.proxyPassword=%HTTPS_PROXY_PASS%"
+)
+if defined HTTP_PROXY (
+    echo Using HTTP_PROXY: %HTTP_PROXY%
+    set "JAVA_ARGS=%JAVA_ARGS% -Dhttp.proxyHost=%HTTP_PROXY_HOST% -Dhttp.proxyPort=%HTTP_PROXY_PORT%"
+    if defined HTTP_PROXY_USER set "JAVA_ARGS=%JAVA_ARGS% -Dhttp.proxyUser=%HTTP_PROXY_USER%"
+    if defined HTTP_PROXY_PASS set "JAVA_ARGS=%JAVA_ARGS% -Dhttp.proxyPassword=%HTTP_PROXY_PASS%"
 )
 
-curl %CURL_EXTRA%  %PROXY% -s -o %SCRIPT_DIR%setup.jar %setup_url%
+if defined SPARK_DRIVER_OPTIONS (
+    set "SPARK_DRIVER_OPTIONS=%SPARK_DRIVER_OPTIONS% %JAVA_ARGS%"
+) else (
+    set "SPARK_DRIVER_OPTIONS=%JAVA_ARGS%"
+)
 
-"%RUNNER%" -cp %SCRIPT_DIR%setup.jar Setup %SCRIPT_DIR%
+set "JAVA_OPTS=%JAVA_OPTS% %JAVA_ARGS%"
+
+goto :handle_command
+
+:launch_setup
+    set "setup_url=https://raw.githubusercontent.com/starlake-ai/starlake/master/distrib/setup.jar"
+    echo Downloading %setup_url% to %SCRIPT_DIR%setup.jar
+    if defined PROXY (
+        if defined SL_INSECURE (
+            curl --insecure --proxy "%PROXY%" -s -o "%SCRIPT_DIR%setup.jar" "%setup_url%"
+        ) else (
+            curl --proxy "%PROXY%" -s -o "%SCRIPT_DIR%setup.jar" "%setup_url%"
+        )
+    ) else (
+        curl -s -o "%SCRIPT_DIR%setup.jar" "%setup_url%"
+    )
+    if errorlevel 1 (
+        echo Error: Failed to retrieve data from %setup_url%.
+        exit /b 1
+    )
+
+    set "RUNNER="
+    if defined JAVA_HOME (
+        set "RUNNER=%JAVA_HOME%\bin\java.exe"
+    ) else (
+        for %%X in (java.exe) do (set RUNNER=%%~dp$PATH:X)
+        if not defined RUNNER (
+            echo JAVA_HOME is not set and java not in PATH
+            exit /b 1
+        )
+    )
+    "%RUNNER%" -cp "%SCRIPT_DIR%setup.jar" Setup "%SCRIPT_DIR%" "windows"
+
+    if exist "%API_BIN_DIR%" (
+        for %%f in ("%API_BIN_DIR%\local-*") do (
+            rem In Windows, .bat/.cmd files are executable by default.
+            echo Granting execute permission to %%f is not necessary on Windows.
+        )
+    )
 goto :eof
 
-
-
 :launch_starlake
-PATH|FIND /i "%HADOOP_HOME%\bin"    >nul || SET PATH=%path%;%HADOOP_HOME%\bin
-if exist %STARLAKE_EXTRA_LIB_FOLDER%\%SL_JAR_NAME% (
-    @REM Transform windows path to unix path for java
-    set SL_ROOT=!SL_ROOT:\=/!
-    set SCRIPT_DIR=%SCRIPT_DIR:\=/%
-    set UNIX_SPARK_TARGET_FOLDER=%SPARK_TARGET_FOLDER:\=/%
-    set UNIX_DEPS_EXTRA_LIB_FOLDER=%DEPS_EXTRA_LIB_FOLDER:\=/%
-    set UNIX_STARLAKE_EXTRA_LIB_FOLDER=%STARLAKE_EXTRA_LIB_FOLDER:\=/%
-    echo.
-    echo Launching starlake.
-    echo - HADOOP_HOME=%HADOOP_HOME%
-    echo - JAVA_HOME=%JAVA_HOME%
-    echo - SL_ROOT=%SL_ROOT%
-    echo - SL_ENV=%SL_ENV%
-    echo - SL_ROOT=%SL_ROOT%
-
-
-    if "%SL_DEBUG%" == "" (
-        set SPARK_DRIVER_OPTIONS=-Dlog4j.configurationFile=file:///%SCRIPT_DIR%bin/spark/conf/log4j2.properties
-        set SPARK_OPTIONS=-Dlog4j.configurationFile="%SPARK_TARGET_FOLDER%\conf\log4j2.properties"
-    ) else (
-        set SPARK_DRIVER_OPTIONS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -Dlog4j.configurationFile=file:///%SPARK_DIR%/conf/log4j2.properties
-        set SPARK_OPTIONS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -Dlog4j.configurationFile="%SPARK_TARGET_FOLDER%\conf\log4j2.properties"
+    if not exist "%STARLAKE_EXTRA_LIB_FOLDER%\%SL_JAR_NAME%" (
+        echo Starlake jar %SL_JAR_NAME% does not exist. Please install it.
+        exit /b 1
     )
 
-    if "%SL_RUN_MODE%" == "main" (
-        set JAVA_OPTIONS=^
-            --add-opens=java.base/java.lang=ALL-UNNAMED ^
-            --add-opens=java.base/java.lang.invoke=ALL-UNNAMED ^
-            --add-opens=java.base/java.lang.reflect=ALL-UNNAMED ^
-            --add-opens=java.base/java.io=ALL-UNNAMED ^
-            --add-opens=java.base/java.net=ALL-UNNAMED ^
-            --add-opens=java.base/java.nio=ALL-UNNAMED ^
-            --add-opens=java.base/java.util=ALL-UNNAMED ^
-            --add-opens=java.base/java.util.concurrent=ALL-UNNAMED ^
-            --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED ^
-            --add-opens=java.base/sun.nio.ch=ALL-UNNAMED ^
-            --add-opens=java.base/sun.nio.cs=ALL-UNNAMED ^
-            --add-opens=java.base/sun.security.action=ALL-UNNAMED ^
-            --add-opens=java.base/sun.util.calendar=ALL-UNNAMED ^
-            --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED
+    if defined SL_ENV (
+        echo - SL_ENV=%SL_ENV%
+    )
 
-        rem Add any additional options you need for your Java application here
-        set JAVA_OPTIONS=!JAVA_OPTS! !JAVA_OPTIONS! !SPARK_OPTIONS!
+    if defined SL_DEBUG (
+        set "SPARK_DRIVER_OPTIONS=%SPARK_DRIVER_OPTIONS% -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005"
+    )
 
+    set "SL_RUN_MODE="
+    if /i "%1" == "import" set SL_RUN_MODE=main
+    if /i "%1" == "xls2yml" set SL_RUN_MODE=main
+    if /i "%1" == "yml2xls" set SL_RUN_MODE=main
+
+    if /i "%SL_RUN_MODE%" == "main" (
+        set "SL_ROOT=!SL_ROOT!"
+        java --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.nio.cs=ALL-UNNAMED --add-opens=java.base/sun.security.action=ALL-UNNAMED --add-opens=java.base/sun.util.calendar=ALL-UNNAMED --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED -Dlog4j.configurationFile="%SPARK_TARGET_FOLDER%/conf/log4j2.properties" -cp "%SPARK_TARGET_FOLDER%\jars\*;%DEPS_EXTRA_LIB_FOLDER%\*;%STARLAKE_EXTRA_LIB_FOLDER%\%SL_JAR_NAME%" %SL_MAIN% %*
     ) else (
-        set EXTRA_CLASSPATH=%STARLAKE_EXTRA_LIB_FOLDER%\%SL_JAR_NAME%
-        set extra_jars=%STARLAKE_EXTRA_LIB_FOLDER%\%SL_JAR_NAME%
+        set "extra_classpath=%STARLAKE_EXTRA_LIB_FOLDER%\%SL_JAR_NAME%"
+        set "extra_jars=%STARLAKE_EXTRA_LIB_FOLDER%\%SL_JAR_NAME%"
+        set "SPARK_SUBMIT=%SPARK_TARGET_FOLDER%\bin\spark-submit.cmd"
 
-        for %%F in ("%DEPS_EXTRA_LIB_FOLDER%\"*.jar) do (
-            set "EXTRA_CLASSPATH=!EXTRA_CLASSPATH!;%%F"
-            set "EXTRA_JARS=!EXTRA_JARS!,%%F"
+        set "deps_jars="
+        for %%f in ("%DEPS_EXTRA_LIB_FOLDER%\*.jar") do (
+            if defined deps_jars (
+                call set "deps_jars=%%deps_jars%%,%%f"
+            ) else (
+                set "deps_jars=%%f"
+            )
         )
-        set SPARK_SUBMIT=%SPARK_TARGET_FOLDER%\bin\spark-submit.cmd
-        @REM spark-submit cmd handles windows path
-        !SPARK_SUBMIT! %SPARK_EXTRA_PACKAGES% --driver-java-options "%JAVA_OPTS% %SPARK_DRIVER_OPTIONS%" %SPARK_CONF_OPTIONS% --driver-class-path "!EXTRA_CLASSPATH!" --class %SL_MAIN% --jars "!EXTRA_JARS!" "%STARLAKE_EXTRA_LIB_FOLDER%\%SL_JAR_NAME%" %*
+
+        if /i "%SPARK_MASTER_URL:~0,5%" == "local" (
+            if defined deps_jars (
+                set "extra_classpath=%extra_classpath%;%deps_jars:,=;%"
+            )
+            set "SPARK_LOCAL_HOSTNAME=127.0.0.1"
+            set "SPARK_HOME=%SCRIPT_DIR%bin\spark"
+            set "SL_ROOT=!SL_ROOT!"
+            call "%SPARK_SUBMIT%" %SPARK_EXTRA_PACKAGES% --driver-java-options "%SPARK_DRIVER_OPTIONS%" %SPARK_CONF_OPTIONS% --driver-class-path "%extra_classpath%" --class "%SL_MAIN%" --master "%SPARK_MASTER_URL%" "%SPARK_TARGET_FOLDER%\README.md" %*
+        ) else (
+            if defined deps_jars (
+                set "extra_classpath=%deps_jars:,=;%"
+                set "extra_jars=%extra_jars%,%deps_jars%"
+            )
+            set "SPARK_HOME=%SCRIPT_DIR%bin\spark"
+            set "SL_ROOT=!SL_ROOT!"
+            call "%SPARK_SUBMIT%" %SPARK_EXTRA_PACKAGES% %SPARK_CONF_OPTIONS% --driver-java-options "%SPARK_DRIVER_OPTIONS%" --driver-class-path "%extra_classpath%" --class "%SL_MAIN%" --master "%SPARK_MASTER_URL%" --jars "%extra_jars%" "%STARLAKE_EXTRA_LIB_FOLDER%\%SL_JAR_NAME%" %*
+        )
     )
-) else (
-    echo Starlake jar %SL_JAR_NAME% do not exists. Please install it.
-    EXIT /B 1
-)
+goto :eof
+
+:handle_command
+    if /i "%1" == "--version" goto :version_command
+    if /i "%1" == "version" goto :version_command
+    if /i "%1" == "install" goto :install_command
+    if /i "%1" == "reinstall" goto :install_command
+    if /i "%1" == "serve" goto :serve_command
+    goto :default_command
+
+:version_command
+    echo Starlake %SL_VERSION%
+    echo Duckdb JDBC driver %DUCKDB_VERSION%
+    echo BigQuery Spark connector %SPARK_BQ_VERSION%
+    echo Hadoop for Azure %HADOOP_AZURE_VERSION%
+    echo Azure Storage %AZURE_STORAGE_VERSION%
+    echo Spark %SPARK_VERSION%
+    echo Hadoop %HADOOP_VERSION%
+    echo Snowflake Spark connector %SPARK_SNOWFLAKE_VERSION%
+    echo Snowflake JDBC driver %SNOWFLAKE_JDBC_VERSION%
+    echo Postgres JDBC driver %POSTGRESQL_VERSION%
+    echo AWS SDK %AWS_JAVA_SDK_VERSION%
+    echo Hadoop for AWS %HADOOP_AWS_VERSION%
+    echo Redshift JDBC driver %REDSHIFT_JDBC_VERSION%
+    echo Redshift Spark connector %SPARK_REDSHIFT_VERSION%
+    goto :eof
+
+:install_command
+    call :launch_setup
+    echo.
+    echo Installation done. You're ready to enjoy Starlake!
+    echo If any errors happen during installation. Please try to install again or open an issue.
+    goto :eof
+
+:serve_command
+    if defined SL_API_DEBUG (
+        set "JAVA_OPTS=%JAVA_OPTS% -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005"
+    )
+    call "%SCRIPT_DIR%bin\api\bin\local-run-api.bat" "%SCRIPT_DIR%" dummy
+    goto :eof
+
+:default_command
+    if not defined SL_HTTP_PORT (
+        call :launch_starlake %*
+    ) else (
+        if not defined SL_HTTP_HOST set "SL_HTTP_HOST=127.0.0.1"
+        set "SL_SERVE_URI=http://%SL_HTTP_HOST%:%SL_HTTP_PORT%"
+        for %%v in (validation run transform compile) do (
+            set "log=!SL_ROOT!\out\%%v.log"
+            if exist "!log!" del "!log!"
+        )
+        curl "%SL_SERVE_URI%?ROOT=!SL_ROOT!&PARAMS=%*"
+        for %%v in (validation run transform compile) do (
+            set "log=!SL_ROOT!\out\%%v.log"
+            if exist "!log!" type "!log!"
+        )
+    )
+    goto :eof
