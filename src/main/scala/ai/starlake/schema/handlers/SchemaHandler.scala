d1a53b482db822239d7c8c1baff13c590b20a4e5
Hayssam Saleh <hayssam@saleh.fr>
2025-12-04 10:45:38 +0100

    Handle dynamic fields

--- src/main/scala/ai/starlake/schema/handlers/SchemaHandler.scala
+++ src/main/scala/ai/starlake/schema/handlers/SchemaHandler.scala
@@ -2480,6 +2480,27 @@ class SchemaHandler(storage: StorageHandler, cliEnv: Map[String, String] = Map.e
     }
   }
 
+  private def updateTypesIfNotDefined(attr: TableAttribute): TableAttribute = {
+    if (attr.`type`.isEmpty) {
+      attr.copy(`type` = "string")
+    } else if (attr.`type`.contains('(')) {
+      val components = attr.`type`.split('(')
+      val typeName = components(0)
+      val zone = components(1).trim.dropRight(1).trim
+      attr.copy(`type` = typeName)
+      /*
+      this
+        .types()
+        .find(t => t.name == typeName)
+        .map { typeInfo =>
+          attr.copy(`type` = typeInfo.name)
+        }
+        .getOrElse {
+          attr.copy(`type` = typeName)
+        }*/
+    } else
+      attr
+  }
   def syncPreviewSqlWithDb(
     taskFullName: String,
     query: Option[String],
@@ -2488,7 +2509,10 @@ class SchemaHandler(storage: StorageHandler, cliEnv: Map[String, String] = Map.e
     settings.schemaHandler().taskByFullName(taskFullName) match {
       case Success(taskInfo) =>
         val list: List[(TableAttribute, AttributeStatus)] =
-          taskInfo.diffSqlAttributesWithSQL(query, accessToken)
+          taskInfo.diffSqlAttributesWithSQL(query, accessToken).map { it =>
+            (updateTypesIfNotDefined(it._1), it._2)
+          }
+
         logger.debug(
           s"Diff SQL attributes with YAML for task $taskFullName: ${list.length} attributes"
         )
